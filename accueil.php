a14997f495eeebe7148cd1e98089e4c48dd2831b
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2007-03-08 17:35:34 +0000

    Modification bidon pour tester le $Id$
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@69 e073496e-d129-0410-8f7b-841bfd467322

--- accueil.php
+++ accueil.php
@@ -21,6 +21,9 @@ $starttime = microtime();
  * along with GEPI; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+
+// MODIF BIDON POUR TESTER $Id$
+
 // Begin standart header
 $titre_page = "Accueil GEPI";
 $affiche_connexion = 'yes';
