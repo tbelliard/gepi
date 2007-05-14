16619d215980726a8950df90eec194b871760e97
tbelliard <tbelliard@e073496e-d129-0410-8f7b-841bfd467322>
2007-05-14 15:25:48 +0000

    Amélioration ergonomie et corrections de bugs
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@423 e073496e-d129-0410-8f7b-841bfd467322

--- absences/index.php
+++ absences/index.php
@@ -1,210 +1,104 @@
 <?php
 
 /*
-
- * Last modification  : 12/05/2005
-
+ * $Id$
  *
-
- * Copyright 2001, 2005 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun
-
+ * Copyright 2001, 2007 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun
  *
-
  * This file is part of GEPI.
-
  *
-
  * GEPI is free software; you can redistribute it and/or modify
-
  * it under the terms of the GNU General Public License as published by
-
  * the Free Software Foundation; either version 2 of the License, or
-
  * (at your option) any later version.
-
  *
-
  * GEPI is distributed in the hope that it will be useful,
-
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-
  * GNU General Public License for more details.
-
  *
-
  * You should have received a copy of the GNU General Public License
-
  * along with GEPI; if not, write to the Free Software
-
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
  */
-
-
 
 // Initialisations files
 require_once("../lib/initialisations.inc.php");
 
 
 // Resume session
-
 $resultat_session = resumeSession();
-
 if ($resultat_session == 'c') {
-
     header("Location: ../utilisateurs/mon_compte.php?change_mdp=yes");
-
     die();
-
 } else if ($resultat_session == '0') {
-
     header("Location: ../logout.php?auto=1");
-
     die();
-
 };
 
 
-
-
 if (!checkAccess()) {
-
     header("Location: ../logout.php?auto=1");
-
     die();
-
 }
 
 // Initialisation
-
 $id_classe = isset($_POST['id_classe']) ? $_POST['id_classe'] : (isset($_GET['id_classe']) ? $_GET['id_classe'] : NULL);
 
-
-
-
-
 include "../lib/periodes.inc.php";
-
 //**************** EN-TETE *****************
-
 $titre_page = "Saisie des absences";
-
 require_once("../lib/header.inc");
-
 //**************** FIN EN-TETE *****************
 
-?>
-
-<p class=bold>|<a href='../accueil.php'>Accueil</a>|
-
-<?php
-
 if (!isset($id_classe)) {
-
-
-
+	echo "<p class=bold><a href='../accueil.php'><img src='../images/icons/back.png' alt='Retour' class='back_link'/> Retour</a>";
     if ($_SESSION['statut']=="cpe") {
-
         $calldata = mysql_query("SELECT DISTINCT c.* FROM classes c, j_eleves_cpe e, j_eleves_classes jc WHERE (e.cpe_login = '".$_SESSION['login']."' AND jc.login = e.e_login AND c.id = jc.id_classe)  ORDER BY classe");
-
     } else {
-
         $calldata = mysql_query("SELECT DISTINCT c.* FROM classes c, periodes p WHERE p.id_classe = c.id  ORDER BY classe");
-
     }
-
-
 
     $nombreligne = mysql_num_rows($calldata);
-
     echo "</p>\n<p>Total : $nombreligne classes - ";
-
     echo "Cliquez sur la classe pour laquelle vous souhaitez saisir les absences :</p>\n";
-
     echo "<p>Remarque : s'affichent toutes les classes pour lesquelles vous êtes responsable du suivi d'un moins un élève de la classe.</p>";
-
     $i = 0;
-
     while ($i < $nombreligne){
-
         $id_classe = mysql_result($calldata, $i, "id");
-
         $classe_liste = mysql_result($calldata, $i, "classe");
-
         echo "<br /><a href='index.php?id_classe=$id_classe'>$classe_liste</a>\n";
-
         $i++;
 
     }
-
     echo "<br />\n";
-
-
-
 } else {
-
     // On choisit la période :
-
-    echo "<a href=\"index.php\">Choisir une autre classe</a>| ";
+	echo "<p class=bold><a href='index.php'><img src='../images/icons/back.png' alt='Retour' class='back_link'/> Choisir une autre classe</a></p>\n";
 
     $call_classe = mysql_query("SELECT classe FROM classes WHERE id = '$id_classe'");
-
     $classe = mysql_result($call_classe, "0", "classe");
-
-    echo "Classe de ".$classe." |</p>\n";
-
-
-
+    echo "<h2>Classe de ".$classe."</h2>\n";
     echo "<p><b>Saisie manuelle - Choisissez la période : </b></p><ul>";
-
     $i="1";
-
     while ($i < $nb_periode) {
-
         if ($ver_periode[$i] == "N") {
-
             echo "<li><a href='saisie_absences.php?id_classe=$id_classe&amp;periode_num=$i'>".ucfirst($nom_periode[$i])."</a></li>";
-
         } else {
-
             echo "<li>".ucfirst($nom_periode[$i])." (".$gepiClosedPeriodLabel.")</li>";
-
         }
-
     $i++;
-
     }
-
     echo "</ul>";
-
     $i="1";
-
     // On propose l'importation à partir d'un fichier GEP
-
     while ($i < $nb_periode) {
-
         if ($ver_periode[$i] == "N") {
-
             echo "<p class='bold'>".ucfirst($nom_periode[$i])." - Importation à partir du fichier F_EABS.DBF de la base GEP (fichier F_NOMA.DBF également requis) :</p>";
-
             echo "<ul><li><a href='import_absences_gep.php?id_classe=$id_classe&amp;periode_num=$i'>Importer les absences à partir du fichier F_EABS.DBF</a></li></ul>";
-
         }
-
         $i++;
-
     }
-
 }
-
+require "../lib/footer.inc.php";
 ?>
-
-
-
-<!--/table-->
-
-</body>
-
-</html>
--- absences/saisie_absences.php
+++ absences/saisie_absences.php
@@ -1,48 +1,26 @@
 <?php
 
 /*
-
- * Last modification  : 21/03/2005
-
+ * $Id$
  *
-
- * Copyright 2001, 2005 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun
-
+ * Copyright 2001, 2007 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun
  *
-
  * This file is part of GEPI.
-
  *
-
  * GEPI is free software; you can redistribute it and/or modify
-
  * it under the terms of the GNU General Public License as published by
-
  * the Free Software Foundation; either version 2 of the License, or
-
  * (at your option) any later version.
-
  *
-
  * GEPI is distributed in the hope that it will be useful,
-
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-
  * GNU General Public License for more details.
-
  *
-
  * You should have received a copy of the GNU General Public License
-
  * along with GEPI; if not, write to the Free Software
-
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
  */
-
-
 
 // Initialisations files
 require_once("../lib/initialisations.inc.php");
@@ -55,283 +33,142 @@ extract($_POST, EXTR_OVERWRITE);
 // Resume session
 
 $resultat_session = resumeSession();
-
 if ($resultat_session == 'c') {
-
     header("Location: ../utilisateurs/mon_compte.php?change_mdp=yes");
-
     die();
-
 } else if ($resultat_session == '0') {
-
     header("Location: ../logout.php?auto=1");
-
     die();
-
 };
 
-
-
-
 if (!checkAccess()) {
-
     header("Location: ../logout.php?auto=1");
-
     die();
-
 }
 
 include "../lib/periodes.inc.php";
 
-
-
 if (isset($_POST['is_posted']) and $_POST['is_posted'] == "yes") {
-
     if ($_SESSION['statut'] == "cpe") {
-
         $quels_eleves = mysql_query("SELECT e.login FROM eleves e, j_eleves_classes c, j_eleves_cpe j WHERE (c.id_classe='$id_classe' AND j.e_login = c.login AND e.login = j.e_login AND j.cpe_login = '".$_SESSION['login'] . "' AND c.periode = '$periode_num')");
-
     } else {
-
         $quels_eleves = mysql_query("SELECT e.login FROM eleves e, j_eleves_classes c WHERE ( c.id_classe='$id_classe' AND c.login = e.login AND c.periode='$periode_num')");
-
 }
-
-
-
 
 
     $quels_eleves = mysql_query("SELECT e.login FROM eleves e, j_eleves_classes c WHERE (c.id_classe='$id_classe' AND e.login = c.login AND c.periode='$periode_num')");
-
     $lignes = mysql_num_rows($quels_eleves);
-
     $j = '0';
-
     while($j < $lignes) {
-
         $reg_eleve_login = mysql_result($quels_eleves, $j, "login");
-
         $nom_log_nb_abs = $reg_eleve_login."_nb_abs";
-
-
 
         $nb_absences = $_POST[$nom_log_nb_abs];
 
-
-
         $nom_log_nb_nj = $reg_eleve_login."_nb_nj";
-
         $nb_nj = $_POST[$nom_log_nb_nj];
 
-
-
         $nom_log_nb_retard = $reg_eleve_login."_nb_retard";
-
         $nb_retard = $_POST[$nom_log_nb_retard];
 
-
-
         $nom_log_ap = $reg_eleve_login."_ap";
-
         $ap = $_POST[$nom_log_ap];
 
-
-        if (!(ereg ("^[0-9]{1,}$", $nb_absences))) {
-
+       if (!(ereg ("^[0-9]{1,}$", $nb_absences))) {
             $nb_absences = '';
-
         }
-
         if (!(ereg ("^[0-9]{1,}$", $nb_nj))) {
-
             $nb_nj = '';
-
         }
-
         if (!(ereg ("^[0-9]{1,}$", $nb_retard))) {
-
             $nb_retard = '';
-
         }
-
-
 
         $test_eleve_nb_absences_query = mysql_query("SELECT * FROM absences WHERE (login='$reg_eleve_login' AND periode='$periode_num')");
-
         $test_nb = mysql_num_rows($test_eleve_nb_absences_query);
-
         if ($test_nb != "0") {
-
             $register = mysql_query("UPDATE absences SET nb_absences='$nb_absences', non_justifie='$nb_nj', nb_retards='$nb_retard', appreciation='$ap' WHERE (login='$reg_eleve_login' AND periode='$periode_num')");
-
         } else {
-
             $register = mysql_query("INSERT INTO absences SET login='$reg_eleve_login', periode='$periode_num',nb_absences='$nb_absences',non_justifie='$nb_nj', nb_retards='$nb_retard',appreciation='$ap'");
-
             }
-
     if (!$register) {
-
             $msg = "Erreur lors de l'enregistrement des données";
-
         }
-
     $j++;
-
     }
-
     $affiche_message = 'yes';
-
 }
-
 $themessage  = 'Des champs ont été modifiés. Voulez-vous vraiment quitter sans enregistrer ?';
-
 $message_enregistrement = 'Les modifications ont été enregistrées !';
 
 //**************** EN-TETE *****************
-
 $titre_page = "Saisie des absences";
-
 require_once("../lib/header.inc");
-
 //**************** FIN EN-TETE *****************
-
 ?>
-
 <script type="text/javascript" language="javascript">
-
 change = 'no';
-
 </script>
 
-
-
 <form enctype="multipart/form-data" action="saisie_absences.php" method=post>
-
-<p class=bold>|<a href="index.php" onclick="return confirm_abandon (this, change, '<?php echo $themessage; ?>')">Choisir une autre classe</a>|
-
-<a href="index.php?id_classe=<?php echo $id_classe; ?>" onclick="return confirm_abandon (this, change, '<?php echo $themessage; ?>')">Choisir une autre période</a>|<input type=submit value=Enregistrer></p>
-
-
-
+<p class=bold>
+<a href="index.php?id_classe=<?php echo $id_classe; ?>" onclick="return confirm_abandon (this, change, '<?php echo $themessage; ?>')"><img src='../images/icons/back.png' alt='Retour' class='back_link'/> Choisir une autre période</a> | 
+<a href="index.php" onclick="return confirm_abandon (this, change, '<?php echo $themessage; ?>')">Choisir une autre classe</a> | <input type=submit value=Enregistrer></p>
 
 
 <?php
-
 $call_classe = mysql_query("SELECT classe FROM classes WHERE id = '$id_classe'");
-
 $classe = mysql_result($call_classe, "0", "classe");
-
 ?>
-
 <p><b>Classe de <?php echo "$classe"; ?> - Saisie des absences : <?php $temp = strtolower($nom_periode[$periode_num]); echo "$temp"; ?></b>
-
 <br />
-
 <table border=1 cellspacing=2 cellpadding=5>
-
-
-
 <tr>
-
     <td><b>Nom Prénom</b></td>
-
     <td><b>Nb. total de 1/2 journées d'absence</b></td>
-
     <td><b>Nb. absences non justifiées</b></td>
-
     <td><b>Nb. de retard</b></td>
-
     <td><b>Observations</b></td>
-
 </tr>
-
-
-
 <?php
-
-
-
 if ($_SESSION['statut'] == "cpe") {
-
         $appel_donnees_eleves = mysql_query("SELECT e.* FROM eleves e, j_eleves_classes c, j_eleves_cpe j WHERE (c.id_classe='$id_classe' AND j.e_login = c.login AND e.login = j.e_login AND j.cpe_login = '".$_SESSION['login'] . "' AND c.periode = '$periode_num') order by e.nom, e.prenom");
-
     } else {
-
         $appel_donnees_eleves = mysql_query("SELECT e.* FROM eleves e, j_eleves_classes c WHERE ( c.id_classe='$id_classe' AND c.login = e.login AND c.periode='$periode_num') order by e.nom, e.prenom");
-
 }
-
-
 
 $nombre_lignes = mysql_num_rows($appel_donnees_eleves);
-
 $i = '0';
-
 $num_id=10;
-
 while($i < $nombre_lignes) {
-
     $current_eleve_login = mysql_result($appel_donnees_eleves, $i, "login");
-
     $current_eleve_absences_query = mysql_query("SELECT * FROM  absences WHERE (login='$current_eleve_login' AND periode='$periode_num')");
-
     $current_eleve_nb_absences = @mysql_result($current_eleve_absences_query, 0, "nb_absences");
-
     $current_eleve_nb_nj = @mysql_result($current_eleve_absences_query, 0, "non_justifie");
-
     $current_eleve_nb_retards = @mysql_result($current_eleve_absences_query, 0, "nb_retards");
-
     $current_eleve_ap_absences = @mysql_result($current_eleve_absences_query, 0, "appreciation");
-
     $current_eleve_nom = mysql_result($appel_donnees_eleves, $i, "nom");
-
     $current_eleve_prenom = mysql_result($appel_donnees_eleves, $i, "prenom");
-
     $current_eleve_login_nb = $current_eleve_login."_nb_abs";
-
     $current_eleve_login_nj = $current_eleve_login."_nb_nj";
-
     $current_eleve_login_retard = $current_eleve_login."_nb_retard";
-
     $current_eleve_login_ap = $current_eleve_login."_ap";
-
     echo "<tr><td>$current_eleve_nom $current_eleve_prenom</td>\n";
-
-    echo "<td><input id=\"".$num_id."\" onKeyDown=\"clavier(this.id,event);\" type=text size=4 name=$current_eleve_login_nb value=\"".$current_eleve_nb_absences."\" onchange=\"changement()\"></td>\n";
-
-    echo "<td><input id=\"1".$num_id."\" onKeyDown=\"clavier(this.id,event);\" type=text size=4 name=$current_eleve_login_nj value=\"".$current_eleve_nb_nj."\" onchange=\"changement()\"></td>\n";
-
-    echo "<td><input id=\"2".$num_id."\" onKeyDown=\"clavier(this.id,event);\" type=text size=4 name=$current_eleve_login_retard value=\"".$current_eleve_nb_retards."\" onchange=\"changement()\"></td>\n";
-
+    echo "<td><input id=\"".$num_id."\" onKeyDown=\"clavier(this.id,event);\" type=text size=4 name=$current_eleve_login_nb value=\"".$current_eleve_nb_absences."\" onchange=\"changement()\" /></td>\n";
+    echo "<td><input id=\"1".$num_id."\" onKeyDown=\"clavier(this.id,event);\" type=text size=4 name=$current_eleve_login_nj value=\"".$current_eleve_nb_nj."\" onchange=\"changement()\" /></td>\n";
+    echo "<td><input id=\"2".$num_id."\" onKeyDown=\"clavier(this.id,event);\" type=text size=4 name=$current_eleve_login_retard value=\"".$current_eleve_nb_retards."\" onchange=\"changement()\" /></td>\n";
     echo "<td><textarea id=\"3".$num_id."\" onKeyDown=\"clavier(this.id,event);\" onchange=\"changement()\" name=$current_eleve_login_ap rows=2 cols=50  wrap=\"virtual\">$current_eleve_ap_absences</textarea></td></tr>\n";
-
 $i++;
-
 $num_id++;
-
 }
 
-
-
 ?>
-
 </table>
-
-<input type=hidden name=is_posted value="yes">
-
-<input type=hidden name=id_classe value=<?php echo "$id_classe";?>>
-
-<input type=hidden name=periode_num value=<?php echo "$periode_num";?>>
-
-<center><div id="fixe"><input type=submit value=Enregistrer></div></center>
-
+<input type="hidden" name="is_posted" value="yes" />
+<input type="hidden" name="id_classe" value=<?php echo "$id_classe";?> />
+<input type="hidden" name="periode_num" value=<?php echo "$periode_num";?> />
+<center><div id="fixe"><input type="submit" value="Enregistrer" /></div></center>
 </form>
 
-
-
 </p>
-
-</body>
-
-</html>
+<?php require "../lib/footer.inc.php";?>
