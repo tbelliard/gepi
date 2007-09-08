b89004fdbf531e8d4fdc4c46e68fb3081358501f
jjocal <jjocal@e073496e-d129-0410-8f7b-841bfd467322>
2007-09-08 22:43:32 +0000

    Modif droits et Insertion dans la table edt_setting pour les emplois du temps
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@664 e073496e-d129-0410-8f7b-841bfd467322

--- utilitaires/maj.php
+++ utilitaires/maj.php
@@ -496,8 +496,9 @@ if (isset ($_POST['maj'])) {
 
 	$tab_req[] = "INSERT INTO droits VALUES ('/mod_absences/lib/graph_double_ligne_fiche.php', 'V', 'V', 'V', 'F', 'F', 'F', 'V', 'Graphique de la fiche élève', '1');";
 
-	$tab_req[] = "INSERT INTO droits VALUES ('/edt_organisation/index_edt.php', 'V', 'V', 'V', 'V', 'F', 'F', 'F', 'Gestion de l emploi du temps', '');";
-	$tab_req[] = "INSERT INTO droits VALUES ('/edt_organisation/edt_initialiser.php', 'V', 'F', 'F', 'F', 'F', 'F', 'F', 'Initialisation de l emploi du temps', '');";
+	$tab_req[] = "INSERT INTO droits VALUES ('/edt_organisation/index_edt.php', 'V', 'V', 'V', 'V', 'F', 'F', 'F', 'Gestion des emplois du temps', '');";
+	$tab_req[] = "INSERT INTO droits VALUES ('/edt_organisation/edt_initialiser.php', 'V', 'F', 'F', 'F', 'F', 'F', 'F', 'Initialisation des emplois du temps', '');";
+	$tab_req[] = "INSERT INTO droits VALUES ('/edt_organisation/effacer_cours.php', 'V', 'F', 'F', 'F', 'F', 'F', 'F', 'Effacer un cours des emplois du temps', '');";
 
 	//$tab_req[] = "";
 
