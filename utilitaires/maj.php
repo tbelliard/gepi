1c8cd0b03c07b5cd00dc00c60b58c4a4053fa2f0
jjocal <jjocal@e073496e-d129-0410-8f7b-841bfd467322>
2007-09-09 19:45:54 +0000

    Ajout de droits pour le module EdT.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@676 e073496e-d129-0410-8f7b-841bfd467322

--- utilitaires/maj.php
+++ utilitaires/maj.php
@@ -501,6 +501,9 @@ if (isset ($_POST['maj'])) {
 	$tab_req[] = "INSERT INTO droits VALUES ('/edt_organisation/effacer_cours.php', 'V', 'F', 'F', 'F', 'F', 'F', 'F', 'Effacer un cours des emplois du temps', '');";
 	$tab_req[] = "INSERT INTO droits VALUES ('/edt_organisation/edt_calendrier.php', 'V', 'F', 'F', 'F', 'F', 'F', 'F', 'Initialisation du calendrier', '');";
 	$tab_req[] = "INSERT INTO droits VALUES ('/edt_organisation/ajouter_salle.php', 'V', 'F', 'F', 'F', 'F', 'F', 'F', 'Gestion des salles', '');";
+	$tab_req[] = "INSERT INTO droits VALUES ('/edt_organisation/edt_parametrer.php', 'V', 'F', 'F', 'F', 'F', 'F', 'F', 'Gérer les paramètres EdT', '');";
+	$tab_req[] = "INSERT INTO droits VALUES ('/edt_organisation/voir_groupe.php', 'V', 'F', 'F', 'F', 'F', 'F', 'F', 'Voir les groupes de Gepi', '');";
+	$tab_req[] = "INSERT INTO droits VALUES ('/edt_organisation/modif_edt_tempo.php', 'V', 'F', 'F', 'F', 'F', 'F', 'F', 'Modification temporaire des EdT', '');";
 
 	//$tab_req[] = "";
 
