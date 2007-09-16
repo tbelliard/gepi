f9b4ac34b591beb4ba8c6e5565f4498324e9f194
jjocal <jjocal@e073496e-d129-0410-8f7b-841bfd467322>
2007-09-16 14:08:31 +0000

    Correctif de bug avec l'affichage des pauses et modif de style pour valider le css.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@709 e073496e-d129-0410-8f7b-841bfd467322

--- edt_organisation/edt_initialiser.php
+++ edt_organisation/edt_initialiser.php
@@ -183,7 +183,7 @@ echo '
 echo "\n";
 	// On propose aussi le choix de l'horaire
 
-	$req_heure = mysql_query("SELECT id_definie_periode, nom_definie_periode, heuredebut_definie_periode, heurefin_definie_periode FROM absences_creneaux ORDER BY heuredebut_definie_periode");
+	$req_heure = mysql_query("SELECT id_definie_periode, nom_definie_periode, heuredebut_definie_periode, heurefin_definie_periode FROM absences_creneaux WHERE type_creneaux != 'pause' ORDER BY heuredebut_definie_periode");
 	$rep_heure = mysql_num_rows($req_heure);
 	$tab_select_heure = array();
 
