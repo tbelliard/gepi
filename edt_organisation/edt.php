3257b86091b0e8cde7697fca4bfe6a898bc3a699
jjocal <jjocal@e073496e-d129-0410-8f7b-841bfd467322>
2007-11-02 15:10:40 +0000

    Amélioration de l'affichage des checked.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@967 e073496e-d129-0410-8f7b-841bfd467322

--- edt_organisation/edt.php
+++ edt_organisation/edt.php
@@ -64,13 +64,14 @@ $message = "";
 
 	// On effectue la requête
 	if ($modif_setting == "ok") {
-		$modif = mysql_query($requete) OR DIE('Erreur dans la requête : '.mysql_error());
+		$modif = mysql_query($requete) OR DIE('La modification n\'a pas été enregistrée : '.mysql_error());
 		$message .= "<p class=\"red\">La modification a bien été enregistrée !</p>";
 	}
 
 	// Petite fonction pour déterminer le checked="checked"
-	function eval_checked($grrSettings, $yn){
-		if ($grrSettings == $yn) {
+	function eval_checked($Settings, $yn){
+	$req_setting = mysql_fetch_array(mysql_query("SELECT value FROM setting WHERE name = '".$Settings."'")) OR DIE ('Erreur requête eval_setting () : '.mysql_error());
+		if ($req_setting["value"] == $yn) {
 			$aff_check = ' checked="checked"';
 		}else {
 			$aff_check = '';
@@ -95,9 +96,9 @@ $message = "";
  Lorsque le module est désactivé, personne n'a accès au module et
  la consultation des emplois du temps est impossible.</i><br />
 <br />
-		<input name="activ_tous" value="y" type="radio"<?php echo eval_checked($grrSettings["autorise_edt_tous"], "y"); ?> onclick='document.autorise_edt.submit();' />
+		<input name="activ_tous" value="y" type="radio"<?php echo eval_checked("autorise_edt_tous", "y"); ?> onclick='document.autorise_edt.submit();' />
 &nbsp;Activer les emplois du temps pour tous les utilisateurs<br />
-		<input name="activ_tous" value="n" type="radio"<?php echo eval_checked($grrSettings["autorise_edt_tous"], "n"); ?> onclick='document.autorise_edt.submit();' />
+		<input name="activ_tous" value="n" type="radio"<?php echo eval_checked("autorise_edt_tous", "n"); ?> onclick='document.autorise_edt.submit();' />
 &nbsp;Désactiver les emplois du temps pour tous les utilisateurs<br />
 
 	</form>
@@ -109,9 +110,9 @@ $message = "";
 <i>Si vous avez d&eacute;sactiver l'acc&egrave;s g&eacute;n&eacute;ral aux emplois du temps,
  vous pouvez autoriser les comptes administrateurs &agrave; y avoir acc&egrave;s.</i><br />
 <br />
-		<input name="activ_ad" value="y" type="radio"<?php echo eval_checked($grrSettings["autorise_edt_admin"], "y"); ?> onclick='document.autorise_admin.submit();' />
+		<input name="activ_ad" value="y" type="radio"<?php echo eval_checked("autorise_edt_admin", "y"); ?> onclick='document.autorise_admin.submit();' />
 &nbsp;Activer les emplois du temps pour les administrateurs<br />
-		<input name="activ_ad" value="n" type="radio"<?php echo eval_checked($grrSettings["autorise_edt_admin"], "n"); ?> onclick='document.autorise_admin.submit();' />
+		<input name="activ_ad" value="n" type="radio"<?php echo eval_checked("autorise_edt_admin", "n"); ?> onclick='document.autorise_admin.submit();' />
 &nbsp;D&eacute;sactiver les emplois du temps pour les administrateurs<br />
 
 	</form>
@@ -124,9 +125,9 @@ $message = "";
 <i>Si vous souhaitez rendre accessible leur emploi du temps aux &eacute;l&egrave;ves et
 &agrave; leurs responsables, il faut imp&eacute;rativement l'autoriser ici.</i><br />
 <br />
-		<input name="activ_ele" value="y" type="radio"<?php echo eval_checked($grrSettings["autorise_edt_eleve"], "y"); ?> onclick='document.autorise_ele.submit();' />
+		<input name="activ_ele" value="y" type="radio"<?php echo eval_checked("autorise_edt_eleve", "y"); ?> onclick='document.autorise_ele.submit();' />
 &nbsp;Activer les emplois du temps pour les &eacute;l&egrave;ves et leurs responsables<br />
-		<input name="activ_ele" value="n" type="radio"<?php echo eval_checked($grrSettings["autorise_edt_eleve"], "n"); ?> onclick='document.autorise_ele.submit();' />
+		<input name="activ_ele" value="n" type="radio"<?php echo eval_checked("autorise_edt_eleve", "n"); ?> onclick='document.autorise_ele.submit();' />
 &nbsp;D&eacute;sactiver les emplois du temps pour les &eacute;l&egrave;ves et leurs responsables<br />
 	</form>
 <br /><br />
