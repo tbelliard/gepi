c8346c351aca5fb987b065ced0a2d8f11da6e5eb
jjocal <jjocal@e073496e-d129-0410-8f7b-841bfd467322>
2007-12-08 16:45:08 +0000

    Réorganisation du code js pour qu'il soit plus clair.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@1227 e073496e-d129-0410-8f7b-841bfd467322

--- saisie/scripts/js_saisie.js
+++ saisie/scripts/js_saisie.js
@@ -1,17 +1,17 @@
 /**
  * Fonction qui récupère l'appréciation d'un textarea précis pour le sauvegarder
- * @access public
- * @return void
  **/
 function ajaxAppreciations(eleveperiode, enseignement, textId){
 	var essai = $(textId);
+	// On récupère le contenu du textarea dont l'id est textId
 	var contenu = $F(textId);
+	// On définit le nom du fichier qui va traiter la requête
 	var url = "ajax_appreciations.php";
 	o_options = new Object();
-
-	// beaucoup de choses restent à revoir
-
 	o_options = {postBody: 'var1='+eleveperiode+'&var2='+enseignement+'&var3='+contenu};
+	// On construit la requête ajax
 	var laRequete = new Ajax.Request(url,o_options);
+	// Il faudra envisager d'utiliser Ajax.Updater pour renvoyer une phrase de confirmation
+	//  ou alors résupérer un retour par Ajax.Request avec onSuccess
 	//alert(enseignement+' \n'+eleveperiode+' \n'+textId+' \n Essai = ' +essai+' \nContenu = '+contenu);
 }
