226f953a8ba592ee72b8e4531026fe6253ec22cd
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2007-12-13 08:45:46 +0000

    Modifications de présentation. Ajout d'une NOTE de bas de page. Ajout du lien vers l'import Sconet par classe par classe.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@1249 e073496e-d129-0410-8f7b-841bfd467322

--- absences/index.php
+++ absences/index.php
@@ -53,7 +53,7 @@ require_once("../lib/header.inc");
 
 if (!isset($id_classe)) {
 	echo "<p class=bold><a href='../accueil.php'><img src='../images/icons/back.png' alt='Retour' class='back_link'/> Retour</a>";
-	echo " | <a href='import_absences_sconet.php'>Importer les absences de Sconet</a>\n";
+	echo " | <a href='import_absences_sconet.php'>Importer les absences de Sconet par lots</a>\n";
 	echo "</p>\n";
 
 	if ($_SESSION['statut']=="cpe") {
@@ -98,7 +98,7 @@ if (!isset($id_classe)) {
 	// On choisit la période :
 	echo "<p class=bold><a href='index.php'><img src='../images/icons/back.png' alt='Retour' class='back_link'/> Choisir une autre classe</a>";
 
-	echo " | <a href='import_absences_sconet.php'>Importer les absences de Sconet</a>\n";
+	echo " | <a href='import_absences_sconet.php'>Importer les absences de Sconet par lots</a>\n";
 
 	echo "</p>\n";
 
@@ -111,39 +111,50 @@ if (!isset($id_classe)) {
 	echo "<table class='boireaus' cellpadding='3'>\n";
 
 	// si le module de gestion des absences est activé alors on ajout un colspan de 2 pour l'entêt d'importation
-	$colspan = '1';
+	$colspan = '2';
 	if ( getSettingValue("active_module_absence") === 'y' ) {
-		$colspan = '2';
+		$colspan = '3';
 	}
 
-	echo "<tr><th>Période</th><th>Saisir</th><th>Consulter</th><th colspan='$colspan'>Importer les absences</th></tr>\n";
+	echo "<tr><th>Période</th><th style='width:6em;'>Saisir</th><th style='width:6em;'>Consulter</th><th colspan='$colspan'>Importer les absences</th></tr>\n";
 	$alt=1;
 	while ($i < $nb_periode) {
 		$alt=$alt*(-1);
 		echo "<tr class='lig$alt'>\n";
 		echo "<th>".ucfirst($nom_periode[$i])."</th>\n";
 		if ($ver_periode[$i] == "N") {
-			echo "<td><a href='saisie_absences.php?id_classe=$id_classe&amp;periode_num=$i'><img src='../images/edit16.png' width='16' height='16' alt='Saisir les absences, retards,...' title='Saisir les absences, retards,...' /></a></td>\n";
+			echo "<td><a href='saisie_absences.php?id_classe=$id_classe&amp;periode_num=$i'><img src='../images/edit16.png' width='16' height='16' alt='Saisir' title='Saisir' /></a></td>\n";
 		} else {
-			echo "<td style='color:red;'>".$gepiClosedPeriodLabel."</td>\n";
+			echo "<td style='color:red;'><img src='../images/disabled.png' width='20' height='20' alt='".$gepiClosedPeriodLabel."' title='".$gepiClosedPeriodLabel."' /></td>\n";
 		}
-		echo "<td><a href='consulter_absences.php?id_classe=$id_classe&amp;periode_num=$i'><img src='../images/icons/chercher.png' width='16' height='16' alt='Saisir les absences, retards,...' title='Consulter les absences, retards,...' /></a></td>\n";
+		echo "<td><a href='consulter_absences.php?id_classe=$id_classe&amp;periode_num=$i'><img src='../images/icons/chercher.png' width='16' height='16' alt='Consulter' title='Consulter' /></a></td>\n";
 
 		if ($ver_periode[$i] == "N") {
-			echo "<td><a href='import_absences_gep.php?id_classe=$id_classe&amp;periode_num=$i'>de GEP</a></td>\n";
+			echo "<td style='width:5em;'><a href='import_absences_gep.php?id_classe=$id_classe&amp;periode_num=$i'>de GEP</a></td>\n";
 		} else {
-			echo "<td style='color:red;'>".$gepiClosedPeriodLabel."</td>\n";
+			echo "<td style='color:red;' colspan='$colspan'><img src='../images/disabled.png' width='20' height='20' alt='".$gepiClosedPeriodLabel."' title='".$gepiClosedPeriodLabel."' /></td>\n";
 		}
 
 	    // si le module de gestion des absences de gepi est activé alors on propose l'importation des absences de ce module
 	    if ( getSettingValue("active_module_absence") === 'y' ) {
 			if ($ver_periode[$i] == "N") {
-				echo "<td><a href='import_absences_gepi.php?id_classe=$id_classe&amp;periode_num=$i'>de GEPI</a></td>\n";
-			} else {
+				echo "<td style='width:5em;'><a href='import_absences_gepi.php?id_classe=$id_classe&amp;periode_num=$i'>de GEPI</a></td>\n";
+			}
+			/*
+			else {
 				echo "<td style='color:red;'>".$gepiClosedPeriodLabel."</td>\n";
 			}
+			*/
 	    }
 
+		if ($ver_periode[$i] == "N") {
+			echo "<td style='width:5em;'><a href='import_absences_sconet.php?id_classe=$id_classe&amp;num_periode=$i'>de Sconet</a></td>\n";
+		}
+		/*
+		else {
+			echo "<td style='color:red;'>".$gepiClosedPeriodLabel."</td>\n";
+		}
+		*/
 		$i++;
 	}
 	echo "</table>\n";
@@ -151,7 +162,11 @@ if (!isset($id_classe)) {
 
 	echo "<p><br /></p>\n";
 
-	echo "<p><i>NOTE:</i> Pour l'importation des absences depuis GEP, les fichiers F_EABS.DBF et F_NOMA.DBF de la base GEP sont requis.</p>\n";
+	echo "<p><i>NOTES:</i></p>\n";
+	echo "<ul>\n";
+	echo "<li><p>Pour l'importation des absences depuis GEP, les fichiers F_EABS.DBF et F_NOMA.DBF de la base GEP sont requis.</p></li>\n";
+	echo "<li><p>Pour l'importation des absences depuis Sconet, le fichier ExportAbsences.xml de Sconet est requis.</p></li>\n";
+	echo "</ul>\n";
 
 	/*
 	$i="1";
