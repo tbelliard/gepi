34a2869c38fc2a6b8511febff2e58c82422a37da
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2007-12-20 08:25:05 +0000

    Ajout d'un test sur le champ adresse mail de l'administrateur pour ne pas tenter d'envoyer un mail si ce champ est vide.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@1278 e073496e-d129-0410-8f7b-841bfd467322

--- gestion/contacter_admin.php
+++ gestion/contacter_admin.php
@@ -54,67 +54,82 @@ switch($action)
 {
 //envoi du message
 case "envoi":
-//N.B. pour peaufiner, mettre un script de vérification de l'adresse email et du contenu du message !
+	//N.B. pour peaufiner, mettre un script de vérification de l'adresse email et du contenu du message !
 
-$corps_message=$message;
+	$corps_message=$message;
 
-$message = "Demandeur : ".$nama."\n";
-$message .= "Statut : ".$_SESSION['statut']."\n";
-// ===============
-// Modif 20070927:
-//$message .= "Etablissement : ".getSettingValue("gepiSchoolName")."\n".unslashes($message);
-$message .= "Etablissement : ".getSettingValue("gepiSchoolName")."\n";
-// ===============
+	$message = "Demandeur : ".$nama."\n";
+	$message .= "Statut : ".$_SESSION['statut']."\n";
+	// ===============
+	// Modif 20070927:
+	//$message .= "Etablissement : ".getSettingValue("gepiSchoolName")."\n".unslashes($message);
+	$message .= "Etablissement : ".getSettingValue("gepiSchoolName")."\n";
+	// ===============
 
-$message.="\n".$corps_message."\n";
+	$message.="\n".$corps_message."\n";
 
-if ($_SESSION['statut'] != "responsable" AND $_SESSION['statut'] != "eleve") {
-	$message .= "\n\nMode de réponse : ".($email_reponse =="" ? "dans le casier =>$casier" :"par email.");
-} else {
-	$message .= "\n\nMode de réponse : par email (si spécifié)";
-}
+	if ($_SESSION['statut'] != "responsable" AND $_SESSION['statut'] != "eleve") {
+		$message .= "\n\nMode de réponse : ".($email_reponse =="" ? "dans le casier =>$casier" :"par email.");
+	} else {
+		$message .= "\n\nMode de réponse : par email (si spécifié)";
+	}
 
-// ===============
-// Ajout 20070927:
-unslashes($message);
-// ===============
+	// ===============
+	// Ajout 20070927:
+	unslashes($message);
+	// ===============
 
-$envoi = mail(getSettingValue("gepiAdminAdress"),
-    "Demande d'aide dans GEPI",
-    $message,
-   "From: ".($email_reponse != "" ? "$nama <$email_reponse>" : getSettingValue("gepiAdminAdress"))."\r\n"
-   .($email_reponse != "" ? "Reply-To: $nama <$email_reponse>\r\n" :"")
-   ."X-Mailer: PHP/" . phpversion());
-if ($envoi) {
-    echo "<br><br><br><P style=\"text-align: center\">Votre message été envoyé,vous recevrez rapidement<br>une réponse dans votre ".($email_reponse =="" ? "casier" :"boîte aux lettres électronique").", veuillez ".($email_reponse =="" ? "le" :"la")." consulter régulièrement.<br><br><br><a href=\"javascript:self.close();\">Fermer</a></p>";
-} else {
-    echo "<br><br><br><P style=\"text-align: center\"><font color=\"red\">ATTENTION : impossible d'envoyer le message, contactez l'administrateur pour lui signaler l'erreur ci-dessus.</font>            </p>";
-}
-break;
+	/*
+	$envoi = mail(getSettingValue("gepiAdminAdress"),
+		"Demande d'aide dans GEPI",
+		$message,
+	"From: ".($email_reponse != "" ? "$nama <$email_reponse>" : getSettingValue("gepiAdminAdress"))."\r\n"
+	.($email_reponse != "" ? "Reply-To: $nama <$email_reponse>\r\n" :"")
+	."X-Mailer: PHP/" . phpversion());
+	*/
+	$gepiAdminAdress=getSettingValue("gepiAdminAdress");
+	if($gepiAdminAdress==""){
+		echo "<p><span style='color:red;>ERREUR</span>: L'adresse mail de l'administrateur n'est pas renseignée.</p>\n";
+		require("../lib/footer.inc.php");
+		die();
+	}
+	$envoi = mail($gepiAdminAdress,
+		"Demande d'aide dans GEPI",
+		$message,
+	"From: ".($email_reponse != "" ? "$nama <$email_reponse>" : $gepiAdminAdress)."\r\n"
+	.($email_reponse != "" ? "Reply-To: $nama <$email_reponse>\r\n" :"")
+	."X-Mailer: PHP/" . phpversion());
+
+	if ($envoi) {
+		echo "<br><br><br><P style=\"text-align: center\">Votre message été envoyé,vous recevrez rapidement<br>une réponse dans votre ".($email_reponse =="" ? "casier" :"boîte aux lettres électronique").", veuillez ".($email_reponse =="" ? "le" :"la")." consulter régulièrement.<br><br><br><a href=\"javascript:self.close();\">Fermer</a></p>";
+	} else {
+		echo "<br><br><br><P style=\"text-align: center\"><font color=\"red\">ATTENTION : impossible d'envoyer le message, contactez l'administrateur pour lui signaler l'erreur ci-dessus.</font>            </p>";
+	}
+	break;
 default://formulaire d'envoi
-echo "<table cellpadding='5'>";
-echo "<tr><td>Message posté par :</td><td><b>".$_SESSION['prenom'] . " " . $_SESSION['nom']."</b></td></tr>";
-echo "<tr><td>Nom et prénom de l'administrateur : </td><td><b>".getSettingValue("gepiAdminNom")." ".getSettingValue("gepiAdminPrenom")."</b></td></tr>";
+	echo "<table cellpadding='5'>";
+	echo "<tr><td>Message posté par :</td><td><b>".$_SESSION['prenom'] . " " . $_SESSION['nom']."</b></td></tr>";
+	echo "<tr><td>Nom et prénom de l'administrateur : </td><td><b>".getSettingValue("gepiAdminNom")." ".getSettingValue("gepiAdminPrenom")."</b></td></tr>";
 
-echo "<tr><td>Nom de l'établissement : </td><td><b>".getSettingValue("gepiSchoolName")."</b></td></tr>";
-echo "<tr><td colspan=2>Utilisez l'adresse <b><a href=\"mailto:" . getSettingValue("gepiAdminAdress") . "\">".getSettingValue("gepiAdminAdress")."</a></b> ou bien rédigez votre message ci-dessous : </td><td></tr>";
-echo "</table>";
-?>
-<form action="contacter_admin.php" method="post" name="doc">
-<input type="hidden" name="nama" value="<?php echo $_SESSION['prenom']." ".$_SESSION['nom']; ?>" />
-<input type="hidden" name="action" value="envoi" />
-<textarea name="message" cols="50" rows="5">Contenu du message : </textarea><br />
-E-mail pour la réponse : <?php if ($_SESSION['statut'] != "responsable" AND $_SESSION['statut'] != "eleve") echo "(facultatif, une réponse vous sera adressée dans votre casier si vous ne précisez pas d'e-mail)";?><br/>
-<input type="text" name="email_reponse" size="40" maxlength="256" /><br/>
-<?php if ($_SESSION['statut'] != "responsable" AND $_SESSION['statut'] != "eleve") { ?>
-Ou numéro de votre casier en salle des professeurs pour la réponse :
-<input type="text" name="casier" size="40" maxlength="256" value="Casier N°" /><br/><br/>
-<?php } ?>
-<input type="submit" value="Envoyer le message" />
+	echo "<tr><td>Nom de l'établissement : </td><td><b>".getSettingValue("gepiSchoolName")."</b></td></tr>";
+	echo "<tr><td colspan=2>Utilisez l'adresse <b><a href=\"mailto:" . getSettingValue("gepiAdminAdress") . "\">".getSettingValue("gepiAdminAdress")."</a></b> ou bien rédigez votre message ci-dessous : </td><td></tr>";
+	echo "</table>";
+	?>
+	<form action="contacter_admin.php" method="post" name="doc">
+	<input type="hidden" name="nama" value="<?php echo $_SESSION['prenom']." ".$_SESSION['nom']; ?>" />
+	<input type="hidden" name="action" value="envoi" />
+	<textarea name="message" cols="50" rows="5">Contenu du message : </textarea><br />
+	E-mail pour la réponse : <?php if ($_SESSION['statut'] != "responsable" AND $_SESSION['statut'] != "eleve") echo "(facultatif, une réponse vous sera adressée dans votre casier si vous ne précisez pas d'e-mail)";?><br/>
+	<input type="text" name="email_reponse" size="40" maxlength="256" /><br/>
+	<?php if ($_SESSION['statut'] != "responsable" AND $_SESSION['statut'] != "eleve") { ?>
+	Ou numéro de votre casier en salle des professeurs pour la réponse :
+	<input type="text" name="casier" size="40" maxlength="256" value="Casier N°" /><br/><br/>
+	<?php } ?>
+	<input type="submit" value="Envoyer le message" />
 
-</form>
-<?php
-break;
+	</form>
+	<?php
+	break;
 }
 require("../lib/footer.inc.php");
 ?>
