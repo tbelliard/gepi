b265735667035c8929443a1c8dfc58278ab39592
jjocal <jjocal@e073496e-d129-0410-8f7b-841bfd467322>
2008-02-11 13:59:06 +0000

    Réorganisation du code et indentation correcte.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@1488 e073496e-d129-0410-8f7b-841bfd467322

--- class_php/gepi_pdf.class.php
+++ class_php/gepi_pdf.class.php
@@ -4,7 +4,24 @@
  *
  *
  * @version $Id$
- * @copyright 2008
+ *
+ * Copyright 2001, 2007 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun, Stéphane Boireau, Christian Chapel
+ *
+ * This file is part of GEPI.
+ *
+ * GEPI is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GEPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GEPI; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 class bul_PDF extends FPDF_MULTICELLTAG
@@ -17,197 +34,191 @@ class bul_PDF extends FPDF_MULTICELLTAG
 *
 * This function is provided by TUFaT.com
 */
-function drawTextBox($strText, $w, $h, $align='L', $valign='T', $border=1)
-{
-	$xi=$this->GetX();
-	$yi=$this->GetY();
+	function drawTextBox($strText, $w, $h, $align='L', $valign='T', $border=1) {
+		$xi=$this->GetX();
+		$yi=$this->GetY();
 
-	$hrow=$this->FontSize;
-	$textrows=$this->drawRows($w,$hrow,$strText,0,$align,0,0,0);
-	$maxrows=floor($h/$this->FontSize);
-	$rows=min($textrows,$maxrows);
+		$hrow=$this->FontSize;
+		$textrows=$this->drawRows($w,$hrow,$strText,0,$align,0,0,0);
+		$maxrows=floor($h/$this->FontSize);
+		$rows=min($textrows,$maxrows);
 
-	if ($border==1)
-		$this->Rect($xi,$yi,$w,$h,'D');
-	if ($border==2)
-		$this->Rect($xi,$yi,$w,$h,'DF');
+		if ($border==1)
+			$this->Rect($xi,$yi,$w,$h,'D');
+		if ($border==2)
+			$this->Rect($xi,$yi,$w,$h,'DF');
 
-	$dy=0;
-	if (strtoupper($valign)=='M')
-		$dy=($h-$rows*$this->FontSize)/2;
-	if (strtoupper($valign)=='B')
-		$dy=$h-$rows*$this->FontSize;
+		$dy=0;
+		if (strtoupper($valign)=='M')
+			$dy=($h-$rows*$this->FontSize)/2;
+		if (strtoupper($valign)=='B')
+			$dy=$h-$rows*$this->FontSize;
 
-	$this->SetY($yi+$dy);
-	$this->SetX($xi);
+		$this->SetY($yi+$dy);
+		$this->SetX($xi);
 
-	$this->drawRows($w,$hrow,$strText,0,$align,0,$rows,1);
+		$this->drawRows($w,$hrow,$strText,0,$align,0,$rows,1);
 
-}
+	} //function drawTextBox
 
-function drawRows($w,$h,$txt,$border=0,$align='J',$fill=0,$maxline=0,$prn=0)
-{
-	$cw=&$this->CurrentFont['cw'];
-	if($w==0)
-		$w=$this->w-$this->rMargin-$this->x;
-	$wmax=($w-2*$this->cMargin)*1000/$this->FontSize;
-	$s=str_replace("\r",'',$txt);
-	$nb=strlen($s);
-	if($nb>0 and $s[$nb-1]=="\n")
-		$nb--;
-	$b=0;
-	if($border)
-	{
-		if($border==1)
-		{
-			$border='LTRB';
-			$b='LRT';
-			$b2='LR';
-		}
-		else
-		{
-			$b2='';
-			if(is_int(strpos($border,'L')))
-				$b2.='L';
-			if(is_int(strpos($border,'R')))
-				$b2.='R';
-			$b=is_int(strpos($border,'T')) ? $b2.'T' : $b2;
-		}
-	}
-	$sep=-1;
-	$i=0;
-	$j=0;
-	$l=0;
-	$ns=0;
-	$nl=1;
-	while($i<$nb)
-	{
-		//Get next character
-		$c=$s[$i];
-		if($c=="\n")
-		{
-			//Explicit line break
-			if($this->ws>0)
+	function drawRows($w,$h,$txt,$border=0,$align='J',$fill=0,$maxline=0,$prn=0) {
+
+		$cw=&$this->CurrentFont['cw'];
+		if($w==0)
+			$w=$this->w-$this->rMargin-$this->x;
+		$wmax=($w-2*$this->cMargin)*1000/$this->FontSize;
+		$s=str_replace("\r",'',$txt);
+		$nb=strlen($s);
+		if($nb>0 and $s[$nb-1]=="\n")
+			$nb--;
+		$b=0;
+		if($border){
+			if($border==1)
 			{
-				$this->ws=0;
-				if ($prn==1) $this->_out('0 Tw');
+				$border='LTRB';
+				$b='LRT';
+				$b2='LR';
+			}else{
+				$b2='';
+				if(is_int(strpos($border,'L')))
+					$b2.='L';
+				if(is_int(strpos($border,'R')))
+					$b2.='R';
+				$b=is_int(strpos($border,'T')) ? $b2.'T' : $b2;
 			}
-			if ($prn==1) {
-				$this->Cell($w,$h,substr($s,$j,$i-$j),$b,2,$align,$fill);
-			}
-			$i++;
-			$sep=-1;
-			$j=$i;
-			$l=0;
-			$ns=0;
-			$nl++;
-			if($border and $nl==2)
-				$b=$b2;
-			if ( $maxline && $nl > $maxline )
-				return substr($s,$i);
-			continue;
 		}
-		if($c==' ')
-		{
-			$sep=$i;
-			$ls=$l;
-			$ns++;
-		}
-		$l+=$cw[$c];
-		if($l>$wmax)
-		{
-			//Automatic line break
-			if($sep==-1)
-			{
-				if($i==$j)
-					$i++;
+		$sep=-1;
+		$i=0;
+		$j=0;
+		$l=0;
+		$ns=0;
+		$nl=1;
+		while($i<$nb){
+
+			//Get next character
+			$c=$s[$i];
+			if($c=="\n"){
+				//Explicit line break
 				if($this->ws>0)
 				{
 					$this->ws=0;
-					if ($prn==1) $this->_out('0 Tw');
+					if ($prn==1)
+						$this->_out('0 Tw');
 				}
 				if ($prn==1) {
 					$this->Cell($w,$h,substr($s,$j,$i-$j),$b,2,$align,$fill);
 				}
+				$i++;
+				$sep=-1;
+				$j=$i;
+				$l=0;
+				$ns=0;
+				$nl++;
+				if($border and $nl==2)
+					$b=$b2;
+				if ( $maxline && $nl > $maxline )
+					return substr($s,$i);
+				continue;
+			}
+			if($c==' ')
+			{
+				$sep=$i;
+				$ls=$l;
+				$ns++;
+			}
+			$l+=$cw[$c];
+			if($l>$wmax)
+			{
+				//Automatic line break
+				if($sep==-1)
+				{
+					if($i==$j)
+						$i++;
+					if($this->ws>0)
+					{
+						$this->ws=0;
+						if ($prn==1) $this->_out('0 Tw');
+					}
+					if ($prn==1) {
+						$this->Cell($w,$h,substr($s,$j,$i-$j),$b,2,$align,$fill);
+					}
+				}else{
+					if($align=='J')
+					{
+						$this->ws=($ns>1) ? ($wmax-$ls)/1000*$this->FontSize/($ns-1) : 0;
+						if ($prn==1) $this->_out(sprintf('%.3f Tw',$this->ws*$this->k));
+					}
+					if ($prn==1){
+						$this->Cell($w,$h,substr($s,$j,$sep-$j),$b,2,$align,$fill);
+					}
+					$i=$sep+1;
+				}
+				$sep=-1;
+				$j=$i;
+				$l=0;
+				$ns=0;
+				$nl++;
+				if($border and $nl==2)
+					$b=$b2;
+				if ( $maxline && $nl > $maxline )
+					return substr($s,$i);
 			}
 			else
-			{
-				if($align=='J')
-				{
-					$this->ws=($ns>1) ? ($wmax-$ls)/1000*$this->FontSize/($ns-1) : 0;
-					if ($prn==1) $this->_out(sprintf('%.3f Tw',$this->ws*$this->k));
-				}
-				if ($prn==1){
-					$this->Cell($w,$h,substr($s,$j,$sep-$j),$b,2,$align,$fill);
-				}
-				$i=$sep+1;
-			}
-			$sep=-1;
-			$j=$i;
-			$l=0;
-			$ns=0;
-			$nl++;
-			if($border and $nl==2)
-				$b=$b2;
-			if ( $maxline && $nl > $maxline )
-				return substr($s,$i);
+				$i++;
 		}
+		//Last chunk
+		if($this->ws>0)
+		{
+			$this->ws=0;
+			if ($prn==1) $this->_out('0 Tw');
+		}
+		if($border and is_int(strpos($border,'B')))
+			$b.='B';
+		if ($prn==1) {
+			$this->Cell($w,$h,substr($s,$j,$i-$j),$b,2,$align,$fill);
+		}
+		$this->x=$this->lMargin;
+		return $nl;
+	} //function drawRows
+
+	function TextWithDirection($x,$y,$txt,$direction='R'){
+
+		$txt=str_replace(')','\\)',str_replace('(','\\(',str_replace('\\','\\\\',$txt)));
+		if ($direction=='R')
+			$s=sprintf('BT %.2f %.2f %.2f %.2f %.2f %.2f Tm (%s) Tj ET',1,0,0,1,$x*$this->k,($this->h-$y)*$this->k,$txt);
+		elseif ($direction=='L')
+			$s=sprintf('BT %.2f %.2f %.2f %.2f %.2f %.2f Tm (%s) Tj ET',-1,0,0,-1,$x*$this->k,($this->h-$y)*$this->k,$txt);
+		elseif ($direction=='U')
+			$s=sprintf('BT %.2f %.2f %.2f %.2f %.2f %.2f Tm (%s) Tj ET',0,1,-1,0,$x*$this->k,($this->h-$y)*$this->k,$txt);
+		elseif ($direction=='D')
+			$s=sprintf('BT %.2f %.2f %.2f %.2f %.2f %.2f Tm (%s) Tj ET',0,-1,1,0,$x*$this->k,($this->h-$y)*$this->k,$txt);
 		else
-			$i++;
-	}
-	//Last chunk
-	if($this->ws>0)
-	{
-		$this->ws=0;
-		if ($prn==1) $this->_out('0 Tw');
-	}
-	if($border and is_int(strpos($border,'B')))
-		$b.='B';
-	if ($prn==1) {
-		$this->Cell($w,$h,substr($s,$j,$i-$j),$b,2,$align,$fill);
-	}
-	$this->x=$this->lMargin;
-	return $nl;
-}
+			$s=sprintf('BT %.2f %.2f Td (%s) Tj ET',$x*$this->k,($this->h-$y)*$this->k,$txt);
+		if ($this->ColorFlag)
+			$s='q '.$this->TextColor.' '.$s.' Q';
+		$this->_out($s);
+	} // function TextWithDirection
 
-function TextWithDirection($x,$y,$txt,$direction='R')
-{
-	$txt=str_replace(')','\\)',str_replace('(','\\(',str_replace('\\','\\\\',$txt)));
-	if ($direction=='R')
-		$s=sprintf('BT %.2f %.2f %.2f %.2f %.2f %.2f Tm (%s) Tj ET',1,0,0,1,$x*$this->k,($this->h-$y)*$this->k,$txt);
-	elseif ($direction=='L')
-		$s=sprintf('BT %.2f %.2f %.2f %.2f %.2f %.2f Tm (%s) Tj ET',-1,0,0,-1,$x*$this->k,($this->h-$y)*$this->k,$txt);
-	elseif ($direction=='U')
-		$s=sprintf('BT %.2f %.2f %.2f %.2f %.2f %.2f Tm (%s) Tj ET',0,1,-1,0,$x*$this->k,($this->h-$y)*$this->k,$txt);
-	elseif ($direction=='D')
-		$s=sprintf('BT %.2f %.2f %.2f %.2f %.2f %.2f Tm (%s) Tj ET',0,-1,1,0,$x*$this->k,($this->h-$y)*$this->k,$txt);
-	else
-		$s=sprintf('BT %.2f %.2f Td (%s) Tj ET',$x*$this->k,($this->h-$y)*$this->k,$txt);
-	if ($this->ColorFlag)
-		$s='q '.$this->TextColor.' '.$s.' Q';
-	$this->_out($s);
-}
+	function TextWithRotation($x,$y,$txt,$txt_angle,$font_angle=0){
 
-function TextWithRotation($x,$y,$txt,$txt_angle,$font_angle=0)
-{
-	$txt=str_replace(')','\\)',str_replace('(','\\(',str_replace('\\','\\\\',$txt)));
+		$txt=str_replace(')','\\)',str_replace('(','\\(',str_replace('\\','\\\\',$txt)));
 
-	$font_angle+=90+$txt_angle;
-	$txt_angle*=M_PI/180;
-	$font_angle*=M_PI/180;
+		$font_angle+=90+$txt_angle;
+		$txt_angle*=M_PI/180;
+		$font_angle*=M_PI/180;
 
-	$txt_dx=cos($txt_angle);
-	$txt_dy=sin($txt_angle);
-	$font_dx=cos($font_angle);
-	$font_dy=sin($font_angle);
+		$txt_dx=cos($txt_angle);
+		$txt_dy=sin($txt_angle);
+		$font_dx=cos($font_angle);
+		$font_dy=sin($font_angle);
 
-	$s=sprintf('BT %.2f %.2f %.2f %.2f %.2f %.2f Tm (%s) Tj ET',
+		$s=sprintf('BT %.2f %.2f %.2f %.2f %.2f %.2f Tm (%s) Tj ET',
 			$txt_dx,$txt_dy,$font_dx,$font_dy,
 			$x*$this->k,($this->h-$y)*$this->k,$txt);
-	if ($this->ColorFlag)
-		$s='q '.$this->TextColor.' '.$s.' Q';
-	$this->_out($s);
-}
+		if ($this->ColorFlag)
+			$s='q '.$this->TextColor.' '.$s.' Q';
+		$this->_out($s);
+	} // function TextWithRotation
 
-}
+} // class bul_PDF
 ?>
