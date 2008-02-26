2de84408eae9e6475fa2a887a6cbbda7ed124a12
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2008-02-26 18:39:00 +0000

    Ajout du vidage des tables matieres_appreciations_grp et matieres_appreciations_tempo. Ajout de $Id$
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@1543 e073496e-d129-0410-8f7b-841bfd467322

--- gestion/efface_base.php
+++ gestion/efface_base.php
@@ -1,7 +1,7 @@
 <?php
 @set_time_limit(0);
 /*
- * Last modification  : 09/03/2005
+ * $Id$
  *
  * Copyright 2001, 2005 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun
  *
@@ -125,6 +125,8 @@ if (isset($_POST['is_posted']) and ($_POST['is_posted'] == 2)) {
    //"matieres",
    "matieres_appreciations",
    "matieres_notes",
+	"matieres_appreciations_grp",
+	"matieres_appreciations_tempo",
    //"periodes",
    "tempo2",
    "temp_gep_import",
