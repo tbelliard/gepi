42d0c3c6f24a36edfa18958052a31871616c2e8d
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2008-04-22 17:04:07 +0000

    Ajout de label.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@1743 e073496e-d129-0410-8f7b-841bfd467322

--- prepa_conseil/index2.php
+++ prepa_conseil/index2.php
@@ -1,41 +1,41 @@
 <?php
 /*
- * $Id$
- *
- * Copyright 2001, 2007 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun
- *
- * This file is part of GEPI.
- *
- * GEPI is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * GEPI is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GEPI; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
+* $Id$
+*
+* Copyright 2001, 2007 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun
+*
+* This file is part of GEPI.
+*
+* GEPI is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* GEPI is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with GEPI; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
 // Initialisations files
 require_once("../lib/initialisations.inc.php");
 // Resume session
 $resultat_session = resumeSession();
 if ($resultat_session == 'c') {
-    header("Location: ../utilisateurs/mon_compte.php?change_mdp=yes");
-    die();
+	header("Location: ../utilisateurs/mon_compte.php?change_mdp=yes");
+	die();
 } else if ($resultat_session == '0') {
-    header("Location: ../logout.php?auto=1");
-    die();
+	header("Location: ../logout.php?auto=1");
+	die();
 };
 
 if (!checkAccess()) {
-    header("Location: ../logout.php?auto=1");
-    die();
+	header("Location: ../logout.php?auto=1");
+	die();
 }
 
 $id_classe = isset($_POST['id_classe']) ? $_POST['id_classe'] : (isset($_GET['id_classe']) ? $_GET['id_classe'] : NULL);
@@ -68,8 +68,8 @@ require_once("../lib/header.inc");
 <p class=bold><a href='../accueil.php'><img src='../images/icons/back.png' alt='Retour' class='back_link'/> Accueil</a>
 <?php
 if (isset($id_classe)) {
-    $current_eleve_classe = sql_query1("SELECT classe FROM classes WHERE id='$id_classe'");
-    echo " | <a href=\"index2.php\">Choisir une autre classe</a>";
+	$current_eleve_classe = sql_query1("SELECT classe FROM classes WHERE id='$id_classe'");
+	echo " | <a href=\"index2.php\">Choisir une autre classe</a>";
 
 
 	// ===========================================
@@ -120,22 +120,29 @@ if (isset($id_classe)) {
 	// ===========================================
 
 
-	echo " | Classe : ".$current_eleve_classe."</p>";
-    echo "<form target=\"_blank\" name=\"visu_toutes_notes\" method=\"post\" action=\"visu_toutes_notes.php\">\n";
-    echo "<table border=\"1\" cellspacing=\"1\" cellpadding=\"10\"><tr>";
-    echo "<td valign=\"top\"><b>Choisissez&nbsp;la&nbsp;période&nbsp;:&nbsp;</b><br />\n";
-    include "../lib/periodes.inc.php";
-    $i="1";
-    while ($i < $nb_periode) {
-        echo "<br />\n<input type=\"radio\" name=\"num_periode\" value=\"$i\" ";
-        if ($i == 1) echo "checked ";
-        echo "/>&nbsp;".ucfirst($nom_periode[$i]);
-    $i++;
-    }
-   echo "<br />\n<input type=\"radio\" name=\"num_periode\" value=\"annee\" />&nbsp;Année entière";
-   echo "\n</td><td valign=\"top\">";
+	echo " | Classe : ".$current_eleve_classe."</p>\n";
+	echo "<form target=\"_blank\" name=\"visu_toutes_notes\" method=\"post\" action=\"visu_toutes_notes.php\">\n";
+	echo "<table border=\"1\" cellspacing=\"1\" cellpadding=\"10\"><tr>";
+	echo "<td valign=\"top\"><b>Choisissez&nbsp;la&nbsp;période&nbsp;:&nbsp;</b><br />\n";
+	include "../lib/periodes.inc.php";
+	$i="1";
+	while ($i < $nb_periode) {
+		echo "<br />\n<input type=\"radio\" name=\"num_periode\" id='num_periode_$i' value=\"$i\" ";
+		if ($i == 1) echo "checked ";
+		echo "/>&nbsp;";
+		echo "<label for='num_periode_$i' style='cursor:pointer;'>\n";
+		echo ucfirst($nom_periode[$i]);
+		echo "</label>\n";
+		$i++;
+	}
+	echo "<br />\n<input type=\"radio\" name=\"num_periode\" id='num_periode_annee' value=\"annee\" />&nbsp;";
+	echo "<label for='num_periode_annee' style='cursor:pointer;'>\n";
+	echo "Année entière";
+	echo "</label>\n";
+	echo "</td>\n";
 
-    echo "<b>Paramètres d'affichage</b><br />\n";
+	echo "<td valign=\"top\">\n";
+	echo "<b>Paramètres d'affichage</b><br />\n";
 	echo "<input type=\"hidden\" name=\"id_classe\" value=\"".$id_classe."\" />";
 
 	echo "<table border='0' width='100%'>\n";
@@ -152,8 +159,12 @@ if (isset($id_classe)) {
 		echo "<td><input type=text name=bord size=3 value=\"1\" /></td>\n";
 		echo "</tr>\n";
 		echo "<tr>\n";
-		echo "<td>Couleurs de fond des lignes alternées : </td>\n";
-		echo "<td><input type=\"checkbox\" name=\"couleur_alterne\" checked /></td>\n";
+		echo "<td>\n";
+		echo "<label for='couleur_alterne' style='cursor:pointer;'>\n";
+		echo "Couleurs de fond des lignes alternées : \n";
+		echo "</label>\n";
+		echo "</td>\n";
+		echo "<td><input type=\"checkbox\" name=\"couleur_alterne\" id=\"couleur_alterne\" checked /></td>\n";
 		echo "</tr>\n";
 		echo "</table>\n";
 
@@ -162,16 +173,28 @@ if (isset($id_classe)) {
 
 		echo "<table border='0'>\n";
 		echo "<tr>\n";
-		echo "<td><input type=\"checkbox\" name=\"aff_abs\" checked /></td>\n";
-		echo "<td>Afficher les absences</td>\n";
+		echo "<td><input type=\"checkbox\" name=\"aff_abs\" id=\"aff_abs\" checked /></td>\n";
+		echo "<td>\n";
+		echo "<label for='aff_abs' style='cursor:pointer;'>\n";
+		echo "Afficher les absences";
+		echo "</label>\n";
+		echo "</td>\n";
 		echo "</tr>\n";
 		echo "<tr>\n";
-		echo "<td><input type=\"checkbox\" name=\"aff_reg\" checked /></td>\n";
-		echo "<td>Afficher le régime</td>\n";
+		echo "<td><input type=\"checkbox\" name=\"aff_reg\" id=\"aff_reg\" checked /></td>\n";
+		echo "<td>\n";
+		echo "<label for='aff_reg' style='cursor:pointer;'>\n";
+		echo "Afficher le régime\n";
+		echo "</label>\n";
+		echo "</td>\n";
 		echo "</tr>\n";
 		echo "<tr>\n";
-		echo "<td><input type=\"checkbox\" name=\"aff_doub\" checked /></td>\n";
-		echo "<td>Afficher la mention doublant</td>\n";
+		echo "<td><input type=\"checkbox\" name=\"aff_doub\" id=\"aff_doub\" checked /></td>\n";
+		echo "<td>\n";
+		echo "<label for='aff_doub' style='cursor:pointer;'>\n";
+		echo "Afficher la mention doublant\n";
+		echo "</label>\n";
+		echo "</td>\n";
 		echo "</tr>\n";
 
 		$affiche_rang = sql_query1("SELECT display_rang FROM classes WHERE id='".$id_classe."'");
@@ -186,8 +209,12 @@ if (isset($id_classe)) {
 		}
 
 		echo "<tr>\n";
-		echo "<td><input type=\"checkbox\" name=\"aff_date_naiss\" /></td>\n";
-		echo "<td>Afficher la date de naissance des élèves</td>\n";
+		echo "<td valign='top'><input type=\"checkbox\" name=\"aff_date_naiss\" id=\"aff_date_naiss\" /></td>\n";
+		echo "<td>\n";
+		echo "<label for='aff_date_naiss' style='cursor:pointer;'>\n";
+		echo "Afficher la date de naissance des élèves\n";
+		echo "</label>\n";
+		echo "</td>\n";
 		echo "</tr>\n";
 		echo "</table>\n";
 
@@ -196,35 +223,35 @@ if (isset($id_classe)) {
 	echo "</table>\n";
 
 /*
-    echo "<br />\nLargeur en pixel du tableau : <input type=text name=larg_tab size=3 value=\"680\" />";
-    echo "<br />\nBords en pixel du tableau : <input type=text name=bord size=3 value=\"1\" />";
-    echo "<br />\nCouleurs de fond des lignes alternées : <input type=\"checkbox\" name=\"couleur_alterne\" checked />";
-    echo "<br /><br /><table cellpadding=\"3\"><tr><td>\n<input type=\"checkbox\" name=\"aff_abs\" checked />Afficher les absences</td>
-    <td><input type=\"checkbox\" name=\"aff_reg\" checked /> Afficher le régime</td>
-    <td><input type=\"checkbox\" name=\"aff_doub\" checked />Afficher la mention doublant</td>";
-    $affiche_rang = sql_query1("SELECT display_rang FROM classes WHERE id='".$id_classe."'");
-    // On teste la présence d'au moins un coeff pour afficher la colonne des coef
-    $test_coef = mysql_num_rows(mysql_query("SELECT coef FROM j_groupes_classes WHERE (id_classe='".$id_classe."' and coef > 0)"));
+	echo "<br />\nLargeur en pixel du tableau : <input type=text name=larg_tab size=3 value=\"680\" />";
+	echo "<br />\nBords en pixel du tableau : <input type=text name=bord size=3 value=\"1\" />";
+	echo "<br />\nCouleurs de fond des lignes alternées : <input type=\"checkbox\" name=\"couleur_alterne\" checked />";
+	echo "<br /><br /><table cellpadding=\"3\"><tr><td>\n<input type=\"checkbox\" name=\"aff_abs\" checked />Afficher les absences</td>
+	<td><input type=\"checkbox\" name=\"aff_reg\" checked /> Afficher le régime</td>
+	<td><input type=\"checkbox\" name=\"aff_doub\" checked />Afficher la mention doublant</td>";
+	$affiche_rang = sql_query1("SELECT display_rang FROM classes WHERE id='".$id_classe."'");
+	// On teste la présence d'au moins un coeff pour afficher la colonne des coef
+	$test_coef = mysql_num_rows(mysql_query("SELECT coef FROM j_groupes_classes WHERE (id_classe='".$id_classe."' and coef > 0)"));
 
 
-    if (($affiche_rang == 'y') and ($test_coef != 0)) {
-       echo "<td><input type=\"checkbox\" name=\"aff_rang\" checked />Afficher le rang des élèves</td>";
-    }
-    echo "</tr></table>";
+	if (($affiche_rang == 'y') and ($test_coef != 0)) {
+	echo "<td><input type=\"checkbox\" name=\"aff_rang\" checked />Afficher le rang des élèves</td>";
+	}
+	echo "</tr></table>";
 */
-    echo "<br />\n<center><input type=\"submit\" name=\"ok\" value=\"Valider\" /></center>";
-    echo "<br />\n<span class='small'>Remarque : le tableau des notes s'affiche sans en-tête et dans une nouvelle page. Pour revenir à cet écran, il vous suffit de fermer la fenêtre du tableau des notes.</span>";
-    if ($_SESSION['statut'] == "professeur"
+	echo "<br />\n<center><input type=\"submit\" name=\"ok\" value=\"Valider\" /></center>";
+	echo "<br />\n<span class='small'>Remarque : le tableau des notes s'affiche sans en-tête et dans une nouvelle page. Pour revenir à cet écran, il vous suffit de fermer la fenêtre du tableau des notes.</span>";
+	if ($_SESSION['statut'] == "professeur"
 	AND getSettingValue("GepiAccesMoyennesProfToutesClasses") != "yes"
 	AND getSettingValue("GepiAccesMoyennesProfToutesTousEleves") != "yes") {
 		echo "<br />\n<span class='small'>Si vous n'enseignez pas à des classes entières, seuls les élèves auxquels vous enseignez apparaîtront dans la liste, et les moyennes calculés ne prendront en compte que les élèves affichés.</span>";
 	}
-    echo "</td></tr>\n</table>\n</form>\n";
+	echo "</td></tr>\n</table>\n</form>\n";
 } else {
-    //echo "</p><b>Visualiser les notes par classe :</b><br />";
-    echo "</p><b>Visualiser les moyennes par classe :</b><br />";
-    //$appel_donnees = mysql_query("SELECT DISTINCT c.* FROM classes c, periodes p WHERE p.id_classe = c.id  ORDER BY classe");
-    //$appel_donnees = mysql_query("SELECT DISTINCT c.* FROM classes c, periodes p, j_scol_classes jsc WHERE p.id_classe = c.id  AND jsc.id_classe=c.id AND jsc.login='".$_SESSION['login']."' ORDER BY classe");
+	//echo "</p><b>Visualiser les notes par classe :</b><br />";
+	echo "</p><b>Visualiser les moyennes par classe :</b><br />";
+	//$appel_donnees = mysql_query("SELECT DISTINCT c.* FROM classes c, periodes p WHERE p.id_classe = c.id  ORDER BY classe");
+	//$appel_donnees = mysql_query("SELECT DISTINCT c.* FROM classes c, periodes p, j_scol_classes jsc WHERE p.id_classe = c.id  AND jsc.id_classe=c.id AND jsc.login='".$_SESSION['login']."' ORDER BY classe");
 	if($_SESSION['statut']=='scolarite'){
 		$appel_donnees = mysql_query("SELECT DISTINCT c.* FROM classes c, periodes p, j_scol_classes jsc WHERE p.id_classe = c.id  AND jsc.id_classe=c.id AND jsc.login='".$_SESSION['login']."' ORDER BY classe");
 	}
@@ -243,7 +270,7 @@ if (isset($id_classe)) {
 			jecpe.cpe_login='".$_SESSION['login']."'
 			ORDER BY classe");
 	}
-    $lignes = mysql_num_rows($appel_donnees);
+	$lignes = mysql_num_rows($appel_donnees);
 
 	if($lignes==0){
 		echo "<p>Aucune classe ne vous est attribuée.<br />Contactez l'administrateur pour qu'il effectue le paramétrage approprié dans la Gestion des classes.</p>\n";
