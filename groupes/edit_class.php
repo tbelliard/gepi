29560e97bccebd0a43dcba85257e55db7de2f151
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2008-05-28 17:40:26 +0000

    Ajout d'un champ select de choix de la classe.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@1875 e073496e-d129-0410-8f7b-841bfd467322

--- groupes/edit_class.php
+++ groupes/edit_class.php
@@ -47,25 +47,38 @@ $classe = get_classe($id_classe);
 
 // =================================
 // AJOUT: boireaus
+$chaine_options_classes="";
 $sql="SELECT id, classe FROM classes ORDER BY classe";
 $res_class_tmp=mysql_query($sql);
 if(mysql_num_rows($res_class_tmp)>0){
     $id_class_prec=0;
     $id_class_suiv=0;
     $temoin_tmp=0;
+    $cpt_classe=0;
+	$num_classe=-1;
     while($lig_class_tmp=mysql_fetch_object($res_class_tmp)){
         if($lig_class_tmp->id==$id_classe){
+			// Index de la classe dans les <option>
+			$num_classe=$cpt_classe;
+
+			$chaine_options_classes.="<option value='$lig_class_tmp->id' selected='true'>$lig_class_tmp->classe</option>\n";
             $temoin_tmp=1;
             if($lig_class_tmp=mysql_fetch_object($res_class_tmp)){
+				$chaine_options_classes.="<option value='$lig_class_tmp->id'>$lig_class_tmp->classe</option>\n";
                 $id_class_suiv=$lig_class_tmp->id;
             }
             else{
                 $id_class_suiv=0;
             }
         }
+		else {
+			$chaine_options_classes.="<option value='$lig_class_tmp->id'>$lig_class_tmp->classe</option>\n";
+		}
+
         if($temoin_tmp==0){
             $id_class_prec=$lig_class_tmp->id;
         }
+		$cpt_classe++;
     }
 }// =================================
 
@@ -146,13 +159,44 @@ $themessage  = 'Des informations ont été modifiées. Voulez-vous vraiment quitter
 $titre_page = "Gestion des enseignements";
 require_once("../lib/header.inc");
 //**************** FIN EN-TETE **********************************
-echo "<table border='0'><tr>";
+echo "<table border='0'><tr>\n";
 echo "<td width='40%' align='left'>";
+echo "<form action='".$_SERVER['PHP_SELF']."' name='form1' method='post'>\n";
 echo "<p class='bold'>";
 echo "<a href='../classes/index.php' onclick=\"return confirm_abandon (this, change, '$themessage')\"><img src='../images/icons/back.png' alt='Retour' class='back_link'/> Retour</a>";
 if($id_class_prec!=0){echo " | <a href='".$_SERVER['PHP_SELF']."?id_classe=$id_class_prec' onclick=\"return confirm_abandon (this, change, '$themessage')\">Classe précédente</a>";}
+if($chaine_options_classes!="") {
+
+	echo "<script type='text/javascript'>
+	// Initialisation
+	change='no';
+
+	function confirm_changement_classe(thechange, themessage)
+	{
+		if (!(thechange)) thechange='no';
+		if (thechange != 'yes') {
+			document.form1.submit();
+		}
+		else{
+			var is_confirmed = confirm(themessage);
+			if(is_confirmed){
+				document.form1.submit();
+			}
+			else{
+				document.getElementById('id_classe').selectedIndex=$num_classe;
+			}
+		}
+	}
+</script>\n";
+
+
+	echo " | <select name='id_classe' id='id_classe' onchange=\"confirm_changement_classe(change, '$themessage');\">\n";
+	echo $chaine_options_classes;
+	echo "</select>\n";
+}
 if($id_class_suiv!=0){echo " | <a href='".$_SERVER['PHP_SELF']."?id_classe=$id_class_suiv' onclick=\"return confirm_abandon (this, change, '$themessage')\">Classe suivante</a>";}
 echo "</p>\n";
+echo "</form>\n";
 
 echo "<h3>Gestion des enseignements pour la classe :" . $classe["classe"]."</h3>\n";
 
