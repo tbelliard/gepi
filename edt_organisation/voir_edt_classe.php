edad301193b821cdbf39e3c95fcf1cac20315184
jjocal <jjocal@e073496e-d129-0410-8f7b-841bfd467322>
2008-06-30 21:48:05 +0000

    Modification de conformité aux standards WC3.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@2034 e073496e-d129-0410-8f7b-841bfd467322

--- edt_organisation/voir_edt_classe.php
+++ edt_organisation/voir_edt_classe.php
@@ -53,7 +53,7 @@ if($indice_classe_select != -1){
 }
 
 echo '
-
+	<p>
 		<select name="login_edt" onchange=\'document.liste_classe.submit();\'>
 			<option value="rien">Choix de la classe</option>
 	';
@@ -75,9 +75,9 @@ if(isset($login_edt)){
 
 
 echo '
-		</select>
+		</select></p><p>
 			<input type="hidden" name="type_edt_2" value="classe" />
-			<input type="hidden" name="visioedt" value="classe1" />
+			<input type="hidden" name="visioedt" value="classe1" /></p>
 	';
 
 if($indice_classe_select != -1){
