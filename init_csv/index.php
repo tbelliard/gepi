938ad5147897a08e4ef13bedb451c43ba229569d
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2008-07-21 19:40:01 +0000

    Ajout d'une condition sur l'affichage d'un message concernant les années antérieures.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@2129 e073496e-d129-0410-8f7b-841bfd467322

--- init_csv/index.php
+++ init_csv/index.php
@@ -48,10 +48,14 @@ require_once("../lib/header.inc");
 <p class=bold><a href="../gestion/index.php"><img src='../images/icons/back.png' alt='Retour' class='back_link'/> Retour</a></p>
 
 <p>Vous allez effectuer l'initialisation de l'année scolaire qui vient de débuter.<br />
-Avez-vous pensé à <a href='#' onmouseover="afficher_div('archivage','y',20,20);" onclick="return false;">archiver</a> l'année qui se termine ?</p>
 <?php
-	$texte="<p>L'archivage de l'année en cours vous permettra, une fois passé à l'année suivante, de consulter les bulletins antérieurs de chacun de vos élèves, pour peu qu'ils aient été scolarisés dans votre établissement.</p><p>Cela nécessite l'activation du <a href='../mod_annees_anterieures/admin.php'>module 'Années antérieures'</a>.</p>";
-	$tabdiv_infobulle[]=creer_div_infobulle('archivage',"Archivage d'une année","",$texte,"",30,0,'y','y','n','n');
+	$sql="SELECT 1=1 FROM matieres_notes LIMIT 1;";
+	$test=mysql_query($sql);
+	if(mysql_num_rows($test)>0) {
+		echo "Avez-vous pensé à <a href='#' onmouseover="afficher_div('archivage','y',20,20);" onclick="return false;">archiver</a> l'année qui se termine ?</p>\n";
+		$texte="<p>L'archivage de l'année en cours vous permettra, une fois passé à l'année suivante, de consulter les bulletins antérieurs de chacun de vos élèves, pour peu qu'ils aient été scolarisés dans votre établissement.</p><p>Cela nécessite l'activation du <a href='../mod_annees_anterieures/admin.php'>module 'Années antérieures'</a>.</p>";
+		$tabdiv_infobulle[]=creer_div_infobulle('archivage',"Archivage d'une année","",$texte,"",30,0,'y','y','n','n');
+	}
 ?>
 </p>
 <ul>
