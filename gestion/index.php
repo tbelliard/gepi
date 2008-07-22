a0e63d5cc8635c91fa33ed2ed5a737a364390c04
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2008-07-22 16:01:38 +0000

    Ajout d'un message concernant les méthodes d'initialisation obsolètes.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@2135 e073496e-d129-0410-8f7b-841bfd467322

--- gestion/index.php
+++ gestion/index.php
@@ -46,24 +46,24 @@ require_once("../lib/header.inc");
 //**************** FIN EN-TETE *****************
 
 ?>
-<p class=bold><a href="../accueil.php"><img src='../images/icons/back.png' alt='Retour' class='back_link'/> Retour</a></p>
+<p class='bold'><a href="../accueil.php"><img src='../images/icons/back.png' alt='Retour' class='back_link' /> Retour</a></p>
 <center>
 <table class='menu' summary='Menu sécurité'>
 <tr>
 	<th colspan='2'><img src='../images/icons/securite.png' alt='Sécurité' class='link'/> - Sécurité</th>
 </tr>
 <tr>
-    <td width=200><a href="gestion_connect.php">Gestion des connexions</a></td>
+    <td width='200'><a href="gestion_connect.php">Gestion des connexions</a></td>
     <td>Affichage des connexions en cours, activation/désactivation des connexions pour le site, protection contre les attaques forces brutes, journal des connexions, changement de mot de passe obligatoire.
     </td>
 </tr>
 <tr>
-    <td width=200><a href="security_panel.php">Panneau de contrôle sécurité</a></td>
+    <td width='200'><a href="security_panel.php">Panneau de contrôle sécurité</a></td>
     <td>Visualiser les tentatives d'utilisation illégale de Gepi.
     </td>
 </tr>
 <tr>
-    <td width=200><a href="security_policy.php">Politique de sécurité</a></td>
+    <td width='200'><a href="security_policy.php">Politique de sécurité</a></td>
     <td>Définir les seuils d'alerte et les actions à entreprendre dans le cas de tentatives d'intrusion ou d'accès illégal à des ressources.
     </td>
 </tr>
@@ -75,69 +75,69 @@ require_once("../lib/header.inc");
 
 <table class='menu' summary='Menu général'>
 <tr>
-	<th colspan='2'><img src='../images/icons/configure.png' alt='Configuration' class='link'/> - Général</th>
+	<th colspan='2'><img src='../images/icons/configure.png' alt='Configuration' class='link' /> - Général</th>
 </tr>
 <tr>
-    <td width=200><a href="param_gen.php">Configuration générale</a></td>
+    <td width='200'><a href="param_gen.php">Configuration générale</a></td>
     <td>Permet de modifier des paramètres généraux (nom de l'établissement, adresse, ...).
     </td>
 </tr>
 <tr>
-    <td width=200><a href="droits_acces.php">Droits d'accès</a></td>
+    <td width='200'><a href="droits_acces.php">Droits d'accès</a></td>
     <td>Modifier les droits d'accès à certaines fonctionnalités selon le statut de l'utilisateur.
     </td>
 </tr>
 <tr>
-    <td width=200><a href="options_connect.php">Options de connexions</a></td>
+    <td width='200'><a href="options_connect.php">Options de connexions</a></td>
     <td>Gestion de la procédure automatisée de récupération de mot de passe, paramétrage du mode de connexion (autonome ou Single Sign-On), changement de mot de passe obligatoire, réglage de la durée de conservation des connexions, suppression de toutes les entrées du journal de connexion.
     </td>
 </tr>
 <tr>
-    <td width=200><a href="modify_impression.php">Gestion de la fiche "bienvenue"</a></td>
+    <td width='200'><a href="modify_impression.php">Gestion de la fiche "bienvenue"</a></td>
     <td>Permet de modifier la feuille d'information à imprimer pour chaque nouvel utilisateur créé.
     </td>
 </tr>
 <tr>
-    <td width=200><a href="config_prefs.php">Paramétrage de l'interface professeur</a></td>
+    <td width='200'><a href="config_prefs.php">Paramétrage de l'interface professeur</a></td>
     <td>Paramétrage des items de l'interface simplifiée pour certaines pages. Gestion du menu en barre horizontale.</td>
 </tr>
 <tr>
-    <td width=200><a href="param_couleurs.php">Paramétrage des couleurs</a></td>
+    <td width='200'><a href="param_couleurs.php">Paramétrage des couleurs</a></td>
     <td>Paramétrage des couleurs de fond d'écran et du dégradé d'entête.</td>
 </tr>
 </table>
 
 <table class='menu' summary='Menu gestion des BDD'>
 <tr>
-	<th colspan='2'><img src='../images/icons/database.png' alt='Gestion bases de données' class='link'/> - Gestion des bases de données </th>
+	<th colspan='2'><img src='../images/icons/database.png' alt='Gestion bases de données' class='link' /> - Gestion des bases de données </th>
 </tr>
 <tr>
-    <td width=200><a href="accueil_sauve.php">Sauvegardes et restauration</a></td>
+    <td width='200'><a href="accueil_sauve.php">Sauvegardes et restauration</a></td>
     <td>Sauvegarder la base GEPI sous la forme d'un fichier au format "mysql".<br />
     Restaurer des données dans la base Mysql de GEPI à partir d'un fichier.
     </td>
 </tr>
 <tr>
-    <td width=200><a href="../utilitaires/maj.php">Mise à jour de la base</a></td>
+    <td width='200'><a href="../utilitaires/maj.php">Mise à jour de la base</a></td>
     <td>Permet d'effectuer une mise à jour de la base MySql après un changement de version  de GEPI.
     </td>
 </tr>
 <tr>
-    <td width=200><a href="../utilitaires/clean_tables.php">Nettoyage des tables</a></td>
+    <td width='200'><a href="../utilitaires/clean_tables.php">Nettoyage des tables</a></td>
     <td>Procéder à un nettoyage des tables de la base MySql de GEPI (suppression de certains doublons et/ou lignes obsolètes ou orphelines).
     </td>
 </tr>
 <tr>
-    <td width=200><a href="efface_base.php">Effacer la base</a></td>
+    <td width='200'><a href="efface_base.php">Effacer la base</a></td>
     <td>Permet de réinitialiser les bases en effaçant toutes les données élèves de la base.
     </td>
 </tr>
 <tr>
-    <td width=200><a href="efface_photos.php">Effacer les photos</a></td>
+    <td width='200'><a href="efface_photos.php">Effacer les photos</a></td>
     <td>Permet d'effacer les photos des élèves qui ne sont plus dans la base.</td>
 </tr>
 <tr>
-    <td width=200><a href="gestion_temp_dir.php">Gestion des dossiers temporaires</a></td>
+    <td width='200'><a href="gestion_temp_dir.php">Gestion des dossiers temporaires</a></td>
     <td>Permet de contrôler le volume occupé par les dossiers temporaires (<i>utilisés notamment pour générer les fichiers tableur OpenOffice (ODS), lorsque la fonction est activée dans le module carnet de notes</i>), de supprimer ces dossiers,...</td>
 </tr>
 
@@ -145,14 +145,14 @@ require_once("../lib/header.inc");
 
 <table class='menu' summary='Menu initialisation'>
 <tr>
-<th colspan='2'><img src='../images/icons/package.png' alt='Initialisation' class='link'/> - Outils d'initialisation</th>
+<th colspan='2'><img src='../images/icons/package.png' alt='Initialisation' class='link' /> - Outils d'initialisation</th>
 </tr>
 <?php
 $use_sso = getSettingValue('use_sso');
 if ($use_sso == "ldap_scribe") {
     ?>
 <tr>
-    <td width=200><a href="../init_scribe/index.php">Initialisation à partir de l'annuaire LDAP du serveur Eole Scribe</a></td>
+    <td width='200'><a href="../init_scribe/index.php">Initialisation à partir de l'annuaire LDAP du serveur Eole Scribe</a></td>
     <td>Permet d'importer les données élèves, classes, professeurs, matières directement depuis le serveur LDAP de Scribe.
     </td>
 </tr>
@@ -160,7 +160,7 @@ if ($use_sso == "ldap_scribe") {
 } else if ($use_sso == "lcs") {
     ?>
 <tr>
-    <td width=200><a href="../init_lcs/index.php">Initialisation à partir de l'annuaire LDAP du serveur LCS</a></td>
+    <td width='200'><a href="../init_lcs/index.php">Initialisation à partir de l'annuaire LDAP du serveur LCS</a></td>
     <td>Permet d'importer les données élèves, classes, professeurs, matières directement depuis le serveur LDAP de LCS.
     </td>
 </tr>
@@ -168,25 +168,26 @@ if ($use_sso == "ldap_scribe") {
 } else {
 ?>
 <tr>
-    <td width=200><a href="../init_csv/index.php">Initialisation des données à partir de fichiers CSV</a></td>
+    <td width='200'><a href="../init_csv/index.php">Initialisation des données à partir de fichiers CSV</a></td>
     <td>Permet d'importer les données élèves, classes, professeurs, matières depuis des fichiers CSV, par exemple des exports depuis Sconet.
     </td>
 </tr>
 <tr>
-    <td width=200><a href="../init_xml2/index.php">Initialisation des données à partir de fichiers XML</a></td>
+    <td width='200'><a href="../init_xml2/index.php">Initialisation des données à partir de fichiers XML</a></td>
     <td>Permet d'importer les données élèves, classes, professeurs, matières depuis les exports XML de Sconet/STS.<br />
 	<b>Nouvelle procédure:</b> Plus simple et moins gourmande en ressources que l'ancienne méthode ci-dessous.
     </td>
 </tr>
 <tr>
-    <td width=200><a href="../init_xml/index.php">Initialisation des données à partir de fichiers XML</a></td>
+    <td width='200'><a href="../init_xml/index.php">Initialisation des données à partir de fichiers XML</a></td>
     <td>Permet d'importer les données élèves, classes, professeurs, matières depuis les exports XML de Sconet/STS.<br />
 	<i>Les XML sont traités pour générer des fichiers CSV qui sont ensuite réclamés dans les différentes étapes de l'initialisation.</i>
     </td>
 </tr>
 <tr>
-    <td width=200><a href="../init_dbf_sts/index.php">Initialisation des données à partir de fichiers DBF et XML</a> (OBSOLETE)</td>
-    <td>Permet d'importer les données élèves, classes, professeurs, matières depuis deux fichiers DBF et l'export XML de STS.
+    <td width='200'><a href="../init_dbf_sts/index.php">Initialisation des données à partir de fichiers DBF et XML</a> (OBSOLETE)</td>
+    <td>Permet d'importer les données élèves, classes, professeurs, matières depuis deux fichiers DBF et l'export XML de STS.<br />
+	<span style='color:red; '>Cette solution ne sera plus maintenue dans la future version 1.5.2 de Gepi.</span>
     </td>
 </tr>
 <?php
@@ -194,8 +195,9 @@ if ($use_sso == "ldap_scribe") {
 ?>
 
 <tr>
-    <td width=200><a href="../initialisation/index.php">Initialisation des données à partir des fichiers GEP</a> (OBSOLETE)</td>
-    <td>Permet d'importer les données élèves, classes, professeurs, matières depuis les fichiers GEP. Cette procédure est désormais obsolète avec la généralisation de Sconet.
+    <td width='200'><a href="../initialisation/index.php">Initialisation des données à partir des fichiers GEP</a> (OBSOLETE)</td>
+    <td>Permet d'importer les données élèves, classes, professeurs, matières depuis les fichiers GEP. Cette procédure est désormais obsolète avec la généralisation de Sconet.<br />
+	<span style='color:red; '>Cette solution ne sera plus maintenue dans la future version 1.5.2 de Gepi.</span>
     </td>
 </tr>
 </table>
