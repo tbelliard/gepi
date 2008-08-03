6eb0998239bd24b2b6206ff947341cc1f01ba897
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2008-08-03 17:25:06 +0000

    Ajout d'un indice de la liste des classes pour éviter des coupures des noms de classes sur plusieurs lignes.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@2256 e073496e-d129-0410-8f7b-841bfd467322

--- lib/share.inc.php
+++ lib/share.inc.php
@@ -2109,12 +2109,16 @@ function get_class_from_ele_login($ele_login){
 	$tab_classe=array();
 	if(mysql_num_rows($res_class)>0){
 		$tab_classe['liste'] = "";
+		$tab_classe['liste_nbsp'] = "";
 		while($lig_tmp=mysql_fetch_object($res_class)){
 
 			$tab_classe[$lig_tmp->id_classe]=$lig_tmp->classe;
 
 			if($a>0) {$tab_classe['liste'].=", ";}
 			$tab_classe['liste'].=$lig_tmp->classe;
+
+			if($a>0) {$tab_classe['liste_nbsp'].=", ";}
+			$tab_classe['liste_nbsp'].=ereg_replace(" ","&nbsp;",$lig_tmp->classe);
 
 			$tab_classe['id'.$a] = $lig_tmp->id_classe;
 			$a = $a++;
