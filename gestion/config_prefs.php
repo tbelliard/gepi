d602e98a8ca5ff72be4b47e3e225bcbfaf129ec5
jjocal <jjocal@e073496e-d129-0410-8f7b-841bfd467322>
2008-08-24 06:29:25 +0000

    Demande de confirmation de modification.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@2306 e073496e-d129-0410-8f7b-841bfd467322

--- gestion/config_prefs.php
+++ gestion/config_prefs.php
@@ -85,6 +85,9 @@ if($_SESSION['statut']!="administrateur"){
 			/*
 			$req_setting = mysql_fetch_array(mysql_query("SELECT value FROM setting WHERE name = '".$Settings."'"))
 								OR DIE ('Erreur requête eval_setting (admin) : '.mysql_error());
+
+				--> A VERIFIER ICI : l'admin charge un setting normalement pas sa préférence : CONFIRMEZ VOUS le changement ?
+				-- > si le changement est confirmé, il faudrait ensuite effacer les commentaires (le svn garde une trace de tout ça ;) )
 			*/
 			$test=mysql_query("SELECT value FROM preferences WHERE name = '".$Settings."'");
 			if(mysql_num_rows($test)>0) {
