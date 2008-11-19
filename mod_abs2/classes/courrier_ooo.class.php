68c613c25cdbcc9e5f4185c8dfebd951af48b745
jjocal <jjocal@e073496e-d129-0410-8f7b-841bfd467322>
2008-11-19 21:38:00 +0000

    Réflexion sur l'utilisation de pclZip.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@2660 e073496e-d129-0410-8f7b-841bfd467322

--- mod_abs2/classes/courrier_ooo.class.php
+++ mod_abs2/classes/courrier_ooo.class.php
@@ -17,61 +17,110 @@ class odfDoc{
 		private $content; // le fichier content.xml contenu dans le fichier .odt est celui qui contient les écrits
 		private $vars = array(); // Array with all the data to change
 
-		function odfDoc($file) {
+		public function __construct($file) {
 			$this->file = $file;
-			$zip = new ZipArchive();
-			if ($zip->open($this->file) === TRUE) {
-				$this->content = $zip->getFromName('content.xml');
-				$zip->close();
-			} else {
-				exit("Impossible d'ouvrir le fichier  '$file' - Vérifiez votre fichier .odt\n");
+
+		  if (!class_exists('ZipArchive')) {
+
+        $this->useLibPcl();
+
+			}else{
+        $zip = new ZipArchive();
+        if ($zip->open($this->file) === TRUE) {
+				  $this->content = $zip->getFromName('content.xml');
+				  $zip->close();
+        }else{
+          throw new Exception('Impossible de décompresser le fichier.');
+        }
+
 			}
 		}
 
-		function setVars($key, $value) {
-			$this->vars[$key] = $value;
+		private function useLibPcl(){
+
+      require_once('../../lib/pclzip.lib.php');
+      $zip = new PclZip($this->file);
+      $aff_fichiers = $zip->listContent();
+
+
+      if ($zip->content = $zip->extract(PCLZIP_OPT_BY_NAME, $aff_fichiers[9]['filename'],  //on extrait content.xml
+	                       PCLZIP_OPT_PATH, $aff_fichiers[9]['stored_filename']) == 0) { //de l'archive dans le dossier archive (id unique)
+      echo "ERROR : ".$zip->errorInfo(true);
+     }
+A REVOIR DE PLUS PRES
+      /*
+      echo '<pre>';
+      print_r($aff_fichiers);
+      echo '</pre>';
+      exit();
+      */
+
+    }
+
+		public function setVars($key, $value) {
+			$this->vars[$key] = utf8_encode($value);
 		}
 
 
-		function parse() {
+		protected function parse() {
 			if ($this->content != NULL) {
 				$this->content = str_replace(array_keys($this->vars), array_values($this->vars), $this->content);
 			} else {
-				exit("Impossible de parser le fichier content.xml, vérifiez que l'intégrité du fichier a bien été respecté.\n");
+				throw new Exception("Impossible de parser le fichier content.xml, vérifiez que l'intégrité du fichier a bien été respecté.\n" . $this->content);
 			}
 		}
 
-		function printVars() {
+		public function printVars() {
 			echo '<pre>';
 			print_r($this->vars);
 			echo '</pre>';
 		}
 
-		function save($newfile) {
+		public function save($newfile) {
+
+		  $this->parse();
+
 			if ($newfile != $this->file){
 				copy($this->file, $newfile);
 				$this->file = $newfile;
 			}
 			$zip = new ZipArchive();
 			if ($zip->open($this->file, ZIPARCHIVE::CREATE) === TRUE) {
-				if (!$zip->addFromString('content.xml', $this->content))
-					exit('Impossible d\'enregistrer le fichier');
+				if (!$zip->addFromString('content.xml', $this->content)){
+          throw new Exception("Impossible d'enregistrer le fichier");
+        }
+
 				$zip->close();
 
-				header('Content-type: multipart/x-zip');
-				header('Content-disposition: attachment; filename=' . $newfile);
-				readfile($newfile);
 			} else {
-				exit('Impossible d\'enregistrer le fichier');
+				throw new Exception("Impossible d'enregistrer le fichier");
 			}
 		}
+
+		public function versNavigateur(){
+
+      header('Content-type: multipart/x-zip');
+			header('Content-disposition: attachment; filename=' . $this->file);
+			readfile($this->file);
+
+    }
+
+    public function versDisque(){
+
+      // A faire si on en a besoin
+
+    }
 }
 
 
-
+try{
    $odf = new odfDoc("test.odt");
    $odf->setVars("{titre1}", "Premier titre pour voir ;)");
    $odf->setVars("{titre2}", "deuxième titre avec un accent pour voir aussi");
-   $odf->parse();
    $odf->save("fichierResultat.odt");
+   $odf->versNavigateur();
+}catch(exception $e){
+  echo $e->getMessage();
+}
+
 ?>
