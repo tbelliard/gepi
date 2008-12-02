b5cb2e73853cac630da9c3c7ed08a50bdf6c4271
jjocal <jjocal@e073496e-d129-0410-8f7b-841bfd467322>
2008-12-02 22:56:31 +0000

    Définitions de quelques méthodes dynamiques et refactorisation du code.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@2726 e073496e-d129-0410-8f7b-841bfd467322

--- mod_abs2/activeRecordGepi.class.php
+++ mod_abs2/activeRecordGepi.class.php
@@ -27,6 +27,7 @@ class ActiveRecordGepi{
   *
   */
   private $_table = NULL;
+  private $newTuple = FALSE;
 
   /**
   * $typeChamps permet de conserver une trace du type de chaque champ
@@ -68,7 +69,7 @@ class ActiveRecordGepi{
    *
    * @access protected
    */
-  protected function __construct($_classe){
+  protected function __construct($_table){
 
     $this->_table = $_table;
 
@@ -100,12 +101,13 @@ class ActiveRecordGepi{
 				$reponse[$return[$a]->Field] = $return[$a]->Type; // Pour en conserver une trace dans l'objet
 				$reponseKey[$return[$a]->Field] = $return[$a]->Key; // Pour en conserver une trace dans l'objet
 
-				$this->$return[$a]->Field = $return[$a]->Default; // Pour les modifier à loisir
-				return true;
+        $champ = $return[$a]->Field;
+
+				$this->$champ = $return[$a]->Default; // Pour les modifier à loisir
 
 			}
 			$this->typeChamps  = $reponse; // On stocke le type de chaque champ
-			$this->typeKeys    = $reponseKey;
+			$this->typeKeys    = $reponseKey; // On stocke le type de clé pour chaque champ
 			return true;
 		}
 	}
@@ -113,7 +115,7 @@ class ActiveRecordGepi{
   protected function _requete($sql){
 
     // Il faut vérifier de quel type est la requête query/exec
-    $this->verif_requete($sql);
+    return $this->verif_requete($sql);
 
   }
 
@@ -130,7 +132,11 @@ class ActiveRecordGepi{
 
       }elseif($test == 'INSE' OR $test == 'UPDA' OR $test == 'DELE'){
 
-        $reponse = self::pdo_connect()->exec($sql);
+        if ($reponse = self::pdo_connect()->exec($sql)) {
+
+        }else{
+          throw new Exception('Erreur dans la requête ' . $reponse);
+        }
 
         if($test == 'INSE'){ // on retourne alors le dernier id enregistré
 
@@ -166,7 +172,23 @@ class ActiveRecordGepi{
 			return false;
 		}
 
-		if (!isset($this->id) OR $this->id == '') {
+		if (!$this->chercherClePrimaire()) {
+		  // Alors id est la clé primaire
+		  // Si le champ id n'existe pas alors qu'il y a plusieurs champs clé primaire
+		  // C'est qu'il y a une erreur de conception dans la table
+		  $this->_pk = $this->id;
+
+		}elseif($this->chercherClePrimaire() == 'no'){
+      // Il n'y a pas de clé primaire dans cette table
+      // Difficile de savoir s'il s'agit d'un insert ou d'un update
+    }else{
+      // Il n'y a qu'une clé primaire
+      // et c'est $this->_pk
+    }
+
+    $clePrimaire = $this->_pk;
+
+		if (!isset($this->$clePrimaire) OR $this->$clePrimaire == '' OR $this->newTuple === true) {
 
 			$sql = "INSERT INTO ".$this->_table." SET ";
 			$verif = 'insert';
@@ -188,22 +210,20 @@ class ActiveRecordGepi{
 
 		$sql = substr($sql, 0, -2); // On enlève la dernière virgule et le dernier espace
 		if ($verif == 'update') {
-			$sql .= ' WHERE id = ' . $this->id;
+			$sql .= " WHERE " . $this->_pk . " = '" . $this->$clePrimaire . "'";
 		}
 
-		if ($this->_requete($sql)) {
+		if ($query = $this->_requete($sql)) {
 
-			if ($verif == 'insert') {
-
-				return self::pdo_connect()->lastInsertId();
-
-			}else{
-
-				return true;
-
-			}
+      return $query;
 
 		}
+
+  }
+
+  public function isNew(){
+
+    $this->newTuple = true;
 
   }
 
@@ -242,6 +262,8 @@ class ActiveRecordGepi{
 		  if ($test == 1) {
 		    $this->_pk = $_keys[0];
 		    return true;
+		  }elseif($test == 0){
+		    return 'no';
 		  }else{
         $this->_pk = $_keys;
         return false;
@@ -263,7 +285,8 @@ class ActiveRecordGepi{
     if (!self::$conn) {
       // Il faut donc ouvrir une connexion
       include("../secure/connect.inc.php"); // Penser à l'enlever car il est déjà inclu en production
-      self::$conn = new PDO('mysql:host='.$GLOBALS["dbHost"].';dbname='.$GLOBALS["dbDb"], $GLOBALS["dbUser"], $GLOBALS["dbPass"]);
+      //self::$conn = new PDO('mysql:host='.$GLOBALS["dbHost"].';dbname='.$GLOBALS["dbDb"], $GLOBALS["dbUser"], $GLOBALS["dbPass"]);
+      self::$conn = new PDO('mysql:host='.$dbHost.';dbname='.$dbDb, $dbUser, $dbPass);
 
     }
 
@@ -316,11 +339,133 @@ class ActiveRecordGepi{
 
   /**
   * Méthode magique : __call permet une construction dynamique des requêtes
+  * Tous les findBychamp() ou les findCampByautrechamp()
+  *
   * @acces public
   */
-  public function __call($methode, $valeur){
+    public function __call($methode, $valeur){
 
+		$test = explode('By', $methode);
+
+		if ($test[0] == 'find') {
+
+			return $this->findBy(strtolower(substr($methode, 6)), $valeur);
+
+		} elseif ( (substr($test[0], 0, 4) == 'find') AND $test[0] != 'find' ){
+
+			$infos = array(0 => strtolower(substr($test[0], 4)), 1 => strtolower($test[1]));
+
+			return $this->findByFk($infos, $valeur);
+
+		} else {
+
+			$this->gest_erreurs("Cette méthodes n'est pas disponible dans cette classe");
+
+		}
+
+	}
+
+  /**
+  * Cette fonction peuple les propriétés de l'objet créé
+  * avec les valeurs des différents champs de la table
+  *
+  * @acces protected
+  */
+
+	protected function findBy($where, $valeur){
+
+		$sql = "SELECT * FROM " . $this->_table . " WHERE " . $where . ' = ' . $this->echappe($valeur[0]) . 'LIMIT 1';
+
+		if ($query_s = $this->_requete($sql)) {
+
+			$rep = $query_s->fetch(PDO::FETCH_OBJ);
+
+			foreach($rep as $cle => $valeur){
+
+				$this->$cle = $valeur;
+
+			}
+
+		} else {
+
+			return false;
+
+		}
+
+	}
+
+  /**
+  *
+	* findByFk() permet d'ajouter les infos avec les clés étrangères
+	* On va permettre de faire simplement des liens avec les clés étrangères
+	* exemple $utilisateurs->findj_groupes_professeursByLogin($valeur)
+	* où $valeur est le login de l'utilisateur (ou toute autre information en rapport avec $this->_table
+  * Login est le champs qui sert de clé étrangère vers la table j_groupes_professeurs
+	* ce qui veut donc dire que la table j_groupes_professeurs à un champ login qui va servir pour la jointure
+	*
+	* $other_key[0] est la table de recherche
+  * $other_key[1] est le champ de jointure
+  * $valeur est la valeur de ce champ de jointure
+	*
+	* @acces protected
+	**/
+
+	protected function findByFk($other_key, $valeur){
+
+		$sql = NULL;
+
+		$sql .= "SELECT * FROM " . $other_key[0]; // . ", " . $this->_table;
+		$sql .= " WHERE " . $other_key[1] . " = " . $this->echappe($valeur[0]);
+
+		if ($query_s = $this->_requete($sql)) {
+
+			$return = $query_s->fetchAll(PDO::FETCH_OBJ);
+			$return["nbre"] = count($return);
+      return $return;
+
+		}else{
+
+      return false;
+
+    }
+
+	}
+
+
+}
+
+/**
+ *
+ *
+
+class utilisateurs extends ActiveRecordGepi{
+
+  public function __construct(){
+    parent::__construct('utilisateurs');
   }
 }
 
+try{
+  $test = new utilisateurs();
+  $test->findByLogin("SGARCIA"); // Il suffit de mettre un login de votre base pour tester
+
+
+  if ($test2 = $test->findJ_groupes_professeursByLogin($test->login)) { // findJ_aid_utilisateursByid_utilisateur($this->login) marche très bien aussi
+    echo '<pre>';
+    print_r($test2);
+    echo '</pre>';
+  }else{
+    throw new Exception('Impossible de lister les groupes de ce professeur : '.$test->login);
+  }
+
+}catch(Exception $e){
+
+  //Les exceptions ne sont pas directement liées à la classe ci dessus mais une bien belle façon de traiter les erreurs.
+
+  echo '<pre>';
+  print_r($e);
+  echo '</pre>';
+
+}
+ */
 ?>
