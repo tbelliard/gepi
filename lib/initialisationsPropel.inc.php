b947052b8d1b2aea1fd399b60b02a3bb0862c7c1
jjocal <jjocal@e073496e-d129-0410-8f7b-841bfd467322>
2009-01-01 11:03:53 +0000

    Modification des bonschemins pour les librairies de Propel.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@2847 e073496e-d129-0410-8f7b-841bfd467322

--- lib/initialisationsPropel.inc.php
+++ lib/initialisationsPropel.inc.php
@@ -4,36 +4,36 @@ if (isset($niveau_arbo) and ($niveau_arbo == "0")) {
    // Database configuration file
    require_once("./secure/connect.inc.php");
    //propel objects
-   set_include_path("./lib/propel-build/classes" . PATH_SEPARATOR . "./lib" . PATH_SEPARATOR . get_include_path());
+   set_include_path("./orm/propel-build/classes" . PATH_SEPARATOR . "./orm" . PATH_SEPARATOR . get_include_path());
    require_once("propel/Propel.php");
    require_once("propel/logger/BasicFileLogger.php");
    $logger = new BasicFileLogger();
    Propel::setLogger($logger);
-   Propel::init("./lib/propel-build/conf/gepi-conf.php");
+   Propel::init("./orm/propel-build/conf/gepi-conf.php");
 
 // Pour les scripts situés dans un sous-répertoire à l'intérieur d'une sous-répertoire de GEPI
 } else if (isset($niveau_arbo) and ($niveau_arbo == "2")) {
    // Database configuration file
    require_once("../../secure/connect.inc.php");
    //propel objects
-   set_include_path("../../lib/propel-build/classes" . PATH_SEPARATOR . "../../lib/propel" . PATH_SEPARATOR . get_include_path());
+   set_include_path("../../orm/propel-build/classes" . PATH_SEPARATOR . "../../orm/propel" . PATH_SEPARATOR . get_include_path());
    require_once("propel/Propel.php");
    require_once("propel/logger/BasicFileLogger.php");
    $logger = new BasicFileLogger();
    Propel::setLogger($logger);
-   Propel::init("../../lib/propel-build/conf/gepi-conf.php");
+   Propel::init("../../orm/propel-build/conf/gepi-conf.php");
 
 // Pour les scripts situés dans un sous-sous-répertoire à l'intérieur d'une sous-répertoire de GEPI
 } else if (isset($niveau_arbo) and ($niveau_arbo == "3")) {
    // Database configuration file
    require_once("../../../secure/connect.inc.php");
    //propel objects
-   set_include_path("../../../lib/propel-build/classes" . PATH_SEPARATOR . "../../../lib/propel" . PATH_SEPARATOR . get_include_path());
+   set_include_path("../../../orm/propel-build/classes" . PATH_SEPARATOR . "../../../orm/propel" . PATH_SEPARATOR . get_include_path());
    require_once("propel/Propel.php");
    require_once("propel/logger/BasicFileLogger.php");
    $logger = new BasicFileLogger();
    Propel::setLogger($logger);
-   Propel::init("../../../lib/propel-build/conf/gepi-conf.php");
+   Propel::init("../../../orm/propel-build/conf/gepi-conf.php");
 
 // Pour les scripts situés dans le sous-répertoire "public"
 // Ces scripts font appel au fichier /public/secure/connect.inc et non pas /secure/connect.inc
@@ -41,23 +41,23 @@ if (isset($niveau_arbo) and ($niveau_arbo == "0")) {
     // Database configuration file
     require_once("../secure/connect.inc.php");
 	//propel objects
-    set_include_path("../lib/propel-build/classes" . PATH_SEPARATOR . "../lib/propel" . PATH_SEPARATOR . get_include_path());
+    set_include_path("../orm/propel-build/classes" . PATH_SEPARATOR . "../orm/propel" . PATH_SEPARATOR . get_include_path());
     require_once("propel/Propel.php");
     require_once("propel/logger/BasicFileLogger.php");
     $logger = new BasicFileLogger();
     Propel::setLogger($logger);
-    Propel::init("../lib/propel-build/conf/gepi-conf.php");
+    Propel::init("../orm/propel-build/conf/gepi-conf.php");
 
 // Pour les scripts situés dans un sous-répertoire GEPI
 } else {
    // Database configuration file
    require_once("../secure/connect.inc.php");
 	//propel objects
-   set_include_path("../lib/propel-build/classes" . PATH_SEPARATOR . "../lib/propel" . PATH_SEPARATOR . get_include_path());
-   require_once("propel/Propel.php");
-   require_once("propel/logger/BasicFileLogger.php");
+   set_include_path("../orm/propel-build/classes" . PATH_SEPARATOR . "../orm/propel" . PATH_SEPARATOR . get_include_path());
+   require_once("../orm/propel/Propel.php");
+   require_once("../orm/propel/logger/BasicFileLogger.php");
    $logger = new BasicFileLogger();
    Propel::setLogger($logger);
-   Propel::init("../lib/propel-build/conf/gepi-conf.php");
+   Propel::init("../orm/propel-build/conf/gepi-conf.php");
 }
 ?>
