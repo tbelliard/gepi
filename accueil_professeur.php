3ac6ee6d63e123806d43dbe492c22e812aa6326b
jjocal <jjocal@e073496e-d129-0410-8f7b-841bfd467322>
2009-03-19 14:34:59 +0000

    Une version du nouvel accueil (commit de Josselin)
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@3006 e073496e-d129-0410-8f7b-841bfd467322

--- accueil_professeur.php
+++ accueil_professeur.php
@@ -105,35 +105,50 @@ function affiche_ligne($chemin_, $titre_, $expli_, $statut_) {
 	}
 }
 
+//fonction compte_fin_module permet de mettre des balises tr ou td tous les deux modules affiché
+$compteur_module = 0;
+function compte_fin_module () {
+	global $compteur_module;
+	$compteur_module = $compteur_module + 1;
+	if ($compteur_module % 2 == 1) {
+		echo '</td><td	valign=top>';
+	} else {
+		echo '</td></tr>';
+		echo '<tr><td valign=top>';
+	}
+}
+
 echo "<p class='bold'>\n";
 echo "<a href='accueil_simpl_prof.php'>Interface graphique</a>";
 echo "</p>\n";
 
 echo '<table align="center"><tr><td	valign=top>';
+
 /**********************************************************************
 	Outils de gestion au quotidien
  * **********************************************************************/
-echo "<div class='bloc_page_acceuil_professeur'>";
-echo '<table class="table_acceuil">';
-echo '<tbody>';
-echo '<th class="accueil">&nbsp;&nbsp;<img src="./images/icons/saisie.png" alt="Saisie" class="link" />&nbsp;- Au quotidien&nbsp;:</th>';
-
-// On teste si on l'utilisateur est un prof avec des matières. Si oui, on affiche les lignes relatives au cahier de texte et au carnet de notes
 $test_prof_matiere = sql_count(sql_query("SELECT login FROM j_groupes_professeurs WHERE login = '".$_SESSION['login']."'"));
-if (($test_prof_matiere != "0") and (getSettingValue("active_cahiers_texte")=='y')) {
-	affiche_ligne( "/cahier_texte/index.php", "Cahier de textes",  "Cet outil vous permet de constituer un cahier de texte pour chacune de vos classes.", $_SESSION['statut']);
-	affiche_ligne( "/cahier_notes/index.php", "Carnet de notes",  "Cet outil vous permet de constituer un carnet de notes pour chaque p&eacute;riode et de saisir les notes de toutes vos &eacute;valuations.", $_SESSION['statut']);
+//on test si il faut afficher le bloc
+if ((($test_prof_matiere != "0") and (getSettingValue("active_cahiers_texte")=='y')) || (($test_prof_matiere != "0") and (getSettingValue("active_carnets_notes")=='y')) || (getSettingValue("active_module_absence_professeur")=='y'  and getSettingValue("active_module_absence")=='y' )) {
+	echo "<div class='bloc_page_acceuil_professeur'>";
+	echo '<table class="table_acceuil">';
+	echo '<tbody>';
+	echo '<th class="accueil">&nbsp;&nbsp;<img src="./images/icons/saisie.png" alt="Saisie" class="link" />&nbsp;- Au quotidien&nbsp;:</th>';
+	if (($test_prof_matiere != "0") and (getSettingValue("active_cahiers_texte")=='y')) {
+		affiche_ligne( "/cahier_texte/index.php", "Cahier de textes",  "Cet outil vous permet de constituer un cahier de texte pour chacune de vos classes.", $_SESSION['statut']);
+	}
+	if (($test_prof_matiere != "0") and (getSettingValue("active_carnets_notes")=='y')) {
+		affiche_ligne( "/cahier_notes/index.php", "Carnet de notes",  "Cet outil vous permet de constituer un carnet de notes pour chaque p&eacute;riode et de saisir les notes de toutes vos &eacute;valuations.", $_SESSION['statut']);
+	}
+	if (getSettingValue("active_module_absence_professeur")=='y'  and getSettingValue("active_module_absence")=='y' ) {
+		affiche_ligne( "/mod_absences/professeurs/prof_ajout_abs.php", "Absences",  "Cet outil vous permet de g&eacute;rer les absences durant vos cours.", $_SESSION['statut']);
+	}
+	echo '</tbody>';
+	echo '</table>';
+	echo "</div>";
+	compte_fin_module();
 }
-if (getSettingValue("active_module_absence_professeur")=='y') {
-	affiche_ligne( "/mod_absences/professeurs/prof_ajout_abs.php", "Absences",  "Cet outil vous permet de g&eacute;rer les absences durant vos cours.", $_SESSION['statut']);
-}
-echo '</tbody>';
-echo '</table>';
-echo "</div>";
-/**********************************************************************
-	Fin Outils de gestion au quotidien
-**********************************************************************/
-echo '</td><td	valign=top>';
+
 /**********************************************************************
 	En fin de periode
  * **********************************************************************/
@@ -147,24 +162,14 @@ if ($test_prof_matiere != "0") {
 	echo '</tbody>';
 	echo '</table>';
 	echo "</div>";
+	compte_fin_module();
 }
-/**********************************************************************
-	fin de periode
- * **********************************************************************/
-echo '</td></tr>';
 
-echo '<tr><td valign=top>';
 /**********************************************************************
 	Visualiser / imprimer
 **********************************************************************/
-echo "<div class='bloc_page_acceuil_professeur'>";
-echo '<table class="table_acceuil">';
-echo '<tbody>';
-echo '<th class="accueil">&nbsp;&nbsp;<img src="./images/icons/contact.png" alt="Trombi" class="link" /><img src="./images/icons/print.png" alt="Imprimer" class="link" />- Visualiser/Imprimer :</th>';
-
-/** relevés de notes**/
 $test_prof_suivi = sql_count(sql_query("SELECT professeur FROM j_eleves_professeurs  WHERE professeur = '".$_SESSION['login']."'"));
-$condition = (
+$condition_releve_note = (
 	(getSettingValue("active_carnets_notes")=='y')
 	AND
 	(
@@ -174,54 +179,8 @@ $condition = (
 		((getSettingValue("GepiAccesReleveProfP") == "yes") AND ($test_prof_suivi != "0"))
 	)
 );
-if ($condition) {
-	affiche_ligne( "/cahier_notes/visu_releve_notes.php", "Les relev&eacute;s de notes",   "Cet outil vous permet de visualiser &agrave; l'&eacute;cran et d'imprimer les relev&eacute;s de notes, ".$gepiSettings['denomination_eleve']." par ".$gepiSettings['denomination_eleve'].", classe par classe.", $_SESSION['statut']);
-}
-
-// Bulletins simplifiés
-if (getSettingValue("GepiAccesBulletinSimplePP")=="yes") {
-	$sql="SELECT 1=1 FROM j_eleves_professeurs WHERE professeur='".$_SESSION['login']."';";
-	$test_pp=mysql_num_rows(mysql_query($sql));
-	if($test_pp>0) {
-		affiche_ligne( "/prepa_conseil/index3.php", "Les bulletins simplifi?s",   "Cet outil vous permet de visualiser &agrave; l'&eacute;cran et d'imprimer les relev&eacute;s de notes, ".$gepiSettings['denomination_eleve']." par ".$gepiSettings['denomination_eleve'].", classe par classe.", $_SESSION['statut']);
-	}
-}
-
-//Trombinoscope
 $active_module_trombinoscopes=getSettingValue("active_module_trombinoscopes");
 $active_module_trombino_pers=getSettingValue("active_module_trombino_pers");
-if (($active_module_trombinoscopes=='y') || ($active_module_trombino_pers=='y')) {
-	affiche_ligne( "/mod_trombinoscopes/trombinoscopes.php", "Les trombinoscopes",   "Cet outil vous permet de visualiser les trombinoscopes des classes.", $_SESSION['statut']);
-
-	// On appelle les aid "trombinoscope"
-	$chemin = array();
-	$titre = array();
-	$expli = array();
-	$call_data = mysql_query("SELECT * FROM aid_config WHERE indice_aid= '".getSettingValue("num_aid_trombinoscopes")."' ORDER BY nom");
-	$nb_aid = mysql_num_rows($call_data);
-	$i=0;
-	while ($i < $nb_aid) {
-		$indice_aid = @mysql_result($call_data, $i, "indice_aid");
-		$call_prof = mysql_query("SELECT * FROM j_aid_utilisateurs_gest WHERE (id_utilisateur = '" . $_SESSION['login'] . "' and indice_aid = '$indice_aid')");
-		$nb_result = mysql_num_rows($call_prof);
-		if (($nb_result != 0) or ($_SESSION['statut'] == 'secours')) {
-			$nom_aid = @mysql_result($call_data, $i, "nom");
-			$chemin[] = "/aid/index2.php?indice_aid=".$indice_aid;
-			$titre[] = $nom_aid;
-			$expli[] = "Cet outil vous permet de visualiser quels ".$gepiSettings['denomination_eleves']." ont le droit d'envoyer/modifier leur photo.";
-		}
-		$i++;
-	}
-
-	$nb_ligne = count($chemin);
-	for ($i=0;$i<$nb_ligne;$i++) {
-		if (acces($chemin[$i],$_SESSION['statut'])==1)  {affiche_ligne($chemin[$i],$titre[$i],$expli[$i], $_SESSION['statut']);}
-	}
-}
-
-//equipe pedagogique et fiche eleve
-affiche_ligne( "/groupes/visu_profs_class.php", "Les &eacute;quipes p&eacute;dagogiques",   "Ceci vous permet de conna&icirc;tre tous les ".$gepiSettings['denomination_professeurs']." des classes dans lesquelles vous intervenez, ainsi que les compositions des groupes concern&eacute;s.", $_SESSION['statut']);
-affiche_ligne( "/eleves/visu_eleve.php", "Les fiches ".$gepiSettings['denomination_eleve'],  "Ce menu vous permet de consulter dans une m&ecirc;me page les informations concernant un ".$gepiSettings['denomination_eleve'], $_SESSION['statut']);
 
 //aid
 $call_data = sql_query("select indice_aid, nom from aid_config WHERE outils_complementaires = 'y' order by nom_complet");
@@ -230,51 +189,108 @@ $call_data2 = sql_query("select id from archivage_types_aid WHERE outils_complem
 $nb_aid_annees_anterieures = mysql_num_rows($call_data2);
 $nb_total=$nb_aid+$nb_aid_annees_anterieures;
 
-if ($nb_total != 0) {
-	$chemin = array();
-	$titre = array();
-	$expli = array();
-	$i = 0;
-	while ($i<$nb_aid) {
-		$indice_aid = mysql_result($call_data,$i,"indice_aid");
-		$_indice_aid[] = mysql_result($call_data,$i,"indice_aid");
-		$nom_aid = mysql_result($call_data,$i,"nom");
-		$chemin[]="/aid/index_fiches.php?indice_aid=".$indice_aid;
-		$titre[] = $nom_aid;
-		$expli[] = "Tableau r&eacute;capitulatif, liste des ".$gepiSettings['denomination_eleves'].", ...";
-		$i++;
-	}
-	$nb_ligne = count($chemin);
-	$affiche = 'no';
-	for ($i=0;$i<$nb_ligne;$i++) {
-		if ((acces($chemin[$i],$_SESSION['statut'])==1))  {$affiche = 'yes';}
-	}
-	if (($nb_aid_annees_anterieures > 0) and (acces("/aid/annees_anterieures_accueil.php",$_SESSION['statut'])==1)) $affiche = 'yes';
+if ($condition_releve_note ||  (getSettingValue("GepiAccesBulletinSimplePP")=="yes") || ($active_module_trombinoscopes=='y') || ($active_module_trombino_pers=='y') || ($nb_total != 0)) {
+	echo "<div class='bloc_page_acceuil_professeur'>";
+	echo '<table class="table_acceuil">';
+	echo '<tbody>';
+	echo '<th class="accueil">&nbsp;&nbsp;<img src="./images/icons/contact.png" alt="Trombi" class="link" /><img src="./images/icons/print.png" alt="Imprimer" class="link" />- Visualiser/Imprimer :</th>';
 
-	if ($affiche=='yes') {
-		for ($i=0;$i<$nb_ligne;$i++) {
-			if (AfficheAid($_SESSION['statut'],$_SESSION['login'],$_indice_aid[$i]))
-			affiche_ligne($chemin[$i],$titre[$i],$expli[$i],$tab,$_SESSION['statut']);
-		}
-		if (($nb_aid_annees_anterieures > 0) and (acces("/aid/annees_anterieures_accueil.php",$_SESSION['statut'])==1)) {
-			$chemin_="/aid/annees_anterieures_accueil.php";
-			$titre_ = "Fiches projets des ann&eacute;es ant&eacute;rieures";
-			$expli_ = "Acc&egrave;s administrateur aux fiches projets des ann&eacute;es ant&eacute;rieures";
-			affiche_ligne($chemin_,$titre_,$expli_,$tab,$_SESSION['statut']);
+/** relevés de notes**/
+	if ($condition_releve_note) {
+		affiche_ligne( "/cahier_notes/visu_releve_notes.php", "Les relev&eacute;s de notes",   "Cet outil vous permet de visualiser &agrave; l'&eacute;cran et d'imprimer les relev&eacute;s de notes, ".$gepiSettings['denomination_eleve']." par ".$gepiSettings['denomination_eleve'].", classe par classe.", $_SESSION['statut']);
+	}
+
+	// Bulletins simplifiés
+	if (getSettingValue("GepiAccesBulletinSimplePP")=="yes") {
+		$sql="SELECT 1=1 FROM j_eleves_professeurs WHERE professeur='".$_SESSION['login']."';";
+		$test_pp=mysql_num_rows(mysql_query($sql));
+		if($test_pp>0) {
+			affiche_ligne( "/prepa_conseil/index3.php", "Les bulletins simplifi?s",   "Cet outil vous permet de visualiser &agrave; l'&eacute;cran et d'imprimer les relev&eacute;s de notes, ".$gepiSettings['denomination_eleve']." par ".$gepiSettings['denomination_eleve'].", classe par classe.", $_SESSION['statut']);
 		}
 	}
-}
+
+	//Trombinoscope
+	if (($active_module_trombinoscopes=='y') || ($active_module_trombino_pers=='y')) {
+		affiche_ligne( "/mod_trombinoscopes/trombinoscopes.php", "Les trombinoscopes",   "Cet outil vous permet de visualiser les trombinoscopes des classes.", $_SESSION['statut']);
+
+		// On appelle les aid "trombinoscope"
+		$chemin = array();
+		$titre = array();
+		$expli = array();
+		$call_data = mysql_query("SELECT * FROM aid_config WHERE indice_aid= '".getSettingValue("num_aid_trombinoscopes")."' ORDER BY nom");
+		$nb_aid = mysql_num_rows($call_data);
+		$i=0;
+		while ($i < $nb_aid) {
+			$indice_aid = @mysql_result($call_data, $i, "indice_aid");
+			$call_prof = mysql_query("SELECT * FROM j_aid_utilisateurs_gest WHERE (id_utilisateur = '" . $_SESSION['login'] . "' and indice_aid = '$indice_aid')");
+			$nb_result = mysql_num_rows($call_prof);
+			if (($nb_result != 0) or ($_SESSION['statut'] == 'secours')) {
+				$nom_aid = @mysql_result($call_data, $i, "nom");
+				$chemin[] = "/aid/index2.php?indice_aid=".$indice_aid;
+				$titre[] = $nom_aid;
+				$expli[] = "Cet outil vous permet de visualiser quels ".$gepiSettings['denomination_eleves']." ont le droit d'envoyer/modifier leur photo.";
+			}
+			$i++;
+		}
+
+		$nb_ligne = count($chemin);
+		for ($i=0;$i<$nb_ligne;$i++) {
+			if (acces($chemin[$i],$_SESSION['statut'])==1)  {affiche_ligne($chemin[$i],$titre[$i],$expli[$i], $_SESSION['statut']);}
+		}
+	}
+
+	//equipe pedagogique et fiche eleve
+	affiche_ligne( "/groupes/visu_profs_class.php", "Les &eacute;quipes p&eacute;dagogiques",   "Ceci vous permet de conna&icirc;tre tous les ".$gepiSettings['denomination_professeurs']." des classes dans lesquelles vous intervenez, ainsi que les compositions des groupes concern&eacute;s.", $_SESSION['statut']);
+	affiche_ligne( "/eleves/visu_eleve.php", "Les fiches ".$gepiSettings['denomination_eleve'],  "Ce menu vous permet de consulter dans une m&ecirc;me page les informations concernant un ".$gepiSettings['denomination_eleve'], $_SESSION['statut']);
+
+	//aid
+	if ($nb_total != 0) {
+		$chemin = array();
+		$titre = array();
+		$expli = array();
+		$i = 0;
+		while ($i<$nb_aid) {
+			$indice_aid = mysql_result($call_data,$i,"indice_aid");
+			$_indice_aid[] = mysql_result($call_data,$i,"indice_aid");
+			$nom_aid = mysql_result($call_data,$i,"nom");
+			$chemin[]="/aid/index_fiches.php?indice_aid=".$indice_aid;
+			$titre[] = $nom_aid;
+			$expli[] = "Tableau r&eacute;capitulatif, liste des ".$gepiSettings['denomination_eleves'].", ...";
+			$i++;
+		}
+		$nb_ligne = count($chemin);
+		$affiche = 'no';
+		for ($i=0;$i<$nb_ligne;$i++) {
+			if ((acces($chemin[$i],$_SESSION['statut'])==1))  {$affiche = 'yes';}
+		}
+		if (($nb_aid_annees_anterieures > 0) and (acces("/aid/annees_anterieures_accueil.php",$_SESSION['statut'])==1)) $affiche = 'yes';
+
+		if ($affiche=='yes') {
+			for ($i=0;$i<$nb_ligne;$i++) {
+				if (AfficheAid($_SESSION['statut'],$_SESSION['login'],$_indice_aid[$i]))
+				affiche_ligne($chemin[$i],$titre[$i],$expli[$i],$tab,$_SESSION['statut']);
+			}
+			if (($nb_aid_annees_anterieures > 0) and (acces("/aid/annees_anterieures_accueil.php",$_SESSION['statut'])==1)) {
+				$chemin_="/aid/annees_anterieures_accueil.php";
+				$titre_ = "Fiches projets des ann&eacute;es ant&eacute;rieures";
+				$expli_ = "Acc&egrave;s administrateur aux fiches projets des ann&eacute;es ant&eacute;rieures";
+				affiche_ligne($chemin_,$titre_,$expli_,$tab,$_SESSION['statut']);
+			}
+		}
+	}
 
 /* Emploi du temps ***/
-affiche_ligne( "/edt_organisation/index_edt.php", "L'emploi du temps",  "Cet outil permet la consultation des emplois du temps.", $_SESSION['statut']);
+	affiche_ligne( "/edt_organisation/index_edt.php", "L'emploi du temps",  "Cet outil permet la consultation des emplois du temps.", $_SESSION['statut']);
 
-echo '</tbody>';
-echo '</table>';
-echo "</div>";/*
- *********************************************************************
+	echo '</tbody>';
+	echo '</table>';
+	echo "</div>";
+	compte_fin_module();
+}
+ /*********************************************************************
 	Fin Visualiser / imprimer
 **********************************************************************/
-echo '</td><td valign=top>';
+
 /**********************************************
 	Preparer le conseil
 **********************************************/
@@ -290,13 +306,12 @@ if (($test_prof_suivi != "0") and (getSettingValue("GepiRubConseilProf")=='yes')
 	echo '</tbody>';
 	echo '</table>';
 	echo "</div>";
+	compte_fin_module();
 }
 /**********************************************
 	Fin Preparer le conseil
 **********************************************/
-echo '</td></tr>';
 
-echo '<tr><td>';
 /**********************************************
 	Outils
 **********************************************/
@@ -312,10 +327,11 @@ if (($test_prof_suivi != "0") and (getSettingValue("GepiRubConseilProf")=='yes')
 echo '</tbody>';
 echo '</table>';
 echo "</div>";
+compte_fin_module();
 /**********************************************
 	Fin Outils
 **********************************************/
-echo '</td><td valign=top>';
+
 /**********************************************
 	Gestion de projets/evenements
 **********************************************/
@@ -328,6 +344,7 @@ if (getSettingValue("active_inscription")=='y' && getSettingValue("active_inscri
 	echo '</tbody>';
 	echo '</table>';
 	echo "</div>";
+	compte_fin_module();
 }
 /**********************************************
 	Fin Gestion de projets/evenements
