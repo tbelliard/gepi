110846d222ee23e9785ab4f994e207ecb2b62249
jjocal <jjocal@e073496e-d129-0410-8f7b-841bfd467322>
2009-06-18 14:46:48 +0000

    Correction de la conf.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@3230 e073496e-d129-0410-8f7b-841bfd467322

---
+++ orm/propel-build/classes/gepi/CahierTexteSequence.php
@@ -0,0 +1,30 @@
+<?php
+
+require 'gepi/om/BaseCahierTexteSequence.php';
+
+
+/**
+ * Skeleton subclass for representing a row from the 'ct_sequences' table.
+ *
+ * Sequence de plusieurs compte-rendus
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    gepi
+ */
+class CahierTexteSequence extends BaseCahierTexteSequence {
+
+	/**
+	 * Initializes internal state of CahierTexteSequence object.
+	 * @see        parent::__construct()
+	 */
+	public function __construct()
+	{
+		// Make sure that parent constructor is always invoked, since that
+		// is where any default values for this object are set.
+		parent::__construct();
+	}
+
+} // CahierTexteSequence
