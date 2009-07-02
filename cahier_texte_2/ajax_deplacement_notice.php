99198affa55e2fb9a23066673294b54b499206c4
jjacquard <jjacquard@e073496e-d129-0410-8f7b-841bfd467322>
2009-07-02 20:15:05 +0000

    implementation du deplacement des notices privees
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@3265 e073496e-d129-0410-8f7b-841bfd467322

--- cahier_texte_2/ajax_deplacement_notice.php
+++ cahier_texte_2/ajax_deplacement_notice.php
@@ -49,6 +49,8 @@ if ($type == 'CahierTexteTravailAFaire') {
 	$ctCompteRendu = CahierTexteTravailAFairePeer::retrieveByPK($id_ct);
 } elseif ($type == 'CahierTexteCompteRendu') {
 	$ctCompteRendu = CahierTexteCompteRenduPeer::retrieveByPK($id_ct);
+} elseif ($type == 'CahierTexteNoticePrivee') {
+	$ctCompteRendu = CahierTexteNoticePriveePeer::retrieveByPK($id_ct);
 }
 
 if ($ctCompteRendu == null) {
@@ -66,5 +68,5 @@ $ctCompteRendu->setDateCt($date_deplacement);
 
 $ctCompteRendu->save();
 $utilisateur->clearAllReferences();
-echo("Deplacement effectuée");
+echo("Deplacement effectué");
 ?>
