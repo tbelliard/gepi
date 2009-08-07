9c7e52d19fdcdb5a99cd6aca8316996641ddd44e
tbelliard <tbelliard@e073496e-d129-0410-8f7b-841bfd467322>
2009-08-07 15:32:50 +0000

    Déplacement de la fonction traite_requête, pour qu'elle soit disponible en ligne de commande.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@3326 e073496e-d129-0410-8f7b-841bfd467322

--- utilitaires/updates/access_rights.inc.php
+++ utilitaires/updates/access_rights.inc.php
@@ -5,6 +5,46 @@
  * ci-après
  *
  */
+
+function traite_requete($requete = "") {
+	global $pb_maj;
+	$retour = "";
+	$res = mysql_query($requete);
+	$erreur_no = mysql_errno();
+	if (!$erreur_no) {
+		$retour = "";
+	} else {
+		switch ($erreur_no) {
+			case "1060" :
+				// le champ existe déjà : pas de problème
+				$retour = "";
+				break;
+			case "1061" :
+				// La cléf existe déjà : pas de problème
+				$retour = "";
+				break;
+			case "1062" :
+				// Présence d'un doublon : création de la cléf impossible
+				$retour = "<font color=\"#FF0000\">Erreur (<b>non critique</b>) sur la requête : <i>" . $requete . "</i> (" . mysql_errno() . " : " . mysql_error() . ")</font><br />\n";
+				$pb_maj = 'yes';
+				break;
+			case "1068" :
+				// Des cléfs existent déjà : pas de problème
+				$retour = "";
+				break;
+			case "1091" :
+				// Déjà supprimé : pas de problème
+				$retour = "";
+				break;
+			default :
+				$retour = "<font color=\"#FF0000\">Erreur sur la requête : <i>" . $requete . "</i> (" . mysql_errno() . " : " . mysql_error() . ")</font><br />\n";
+				$pb_maj = 'yes';
+				break;
+		}
+	}
+	return $retour;
+}
+
 // statuts dynamiques
 $result .= "&nbsp;->Ajout d'un champ 'autre' à la table 'droits'<br />";
 $test1 = mysql_num_rows(mysql_query("SHOW COLUMNS FROM droits LIKE 'autre'"));
