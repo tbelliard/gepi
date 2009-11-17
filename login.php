ea5e795ed72d94c39765bce76420ca75fd9f0467
regis <regis@e073496e-d129-0410-8f7b-841bfd467322>
2009-11-17 20:23:25 +0000

    Affichage de la page de login en utilisant un gabarit
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@3734 e073496e-d129-0410-8f7b-841bfd467322

--- login.php
+++ login.php
@@ -20,7 +20,61 @@
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-// Pour le multisite
+
+
+
+/* ---------Variables envoyées au gabarit
+*	$tbs_CdT_public_titre						cahier de texte public activé
+*	$tbs_multisite									rne si on est en multisite
+*	$tbs_gepiSchoolName							nom de l'établissement
+*	$tbs_gepiYear										année scolaire en cours
+*	$tbs_password_recovery					adresse page de récupération de mot de passe oublié
+* $tbs_SSO_lien										adresse page de login SSO
+*	$tbs_admin_java									nom du script pour contacter l'administrateur
+*
+*	----- tableaux -----
+*	$tbs_Site_ferme									message de fermeture									tbs_blk1
+* $tbs_message										message sous l'entête									tbs_message
+*				-> classe									classe CSS ("" ou "txt_rouge")
+*				-> texte									le texte à afficher
+*	$tbs_admin_adr									adresse courriel administrateur				tbs_blk2
+*				-> nom
+*				-> fai
+* $tbs_dossier_gabarit						liste des gabarits disponibles				tbs_blk3
+*				-> texte									texte à afficher dans la liste de choix
+*				-> value									nom du dossier
+*				-> selection							`y` si gabarit par défaut, `n` ou rien sinon
+*/
+
+
+/*
+table à ajouter pour pouvoir utiliser plusieurs gabarits et données du gabarit d'origine
+
+CREATE TABLE `gabarits` (
+`index` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`texte` VARCHAR( 32 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
+`repertoire` VARCHAR( 16 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
+`pardefaut` CHAR( 1 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'n'
+) ENGINE = MYISAM ;
+
+INSERT INTO `gabarits` (
+`index` ,
+`texte` ,
+`repertoire` ,
+`pardefaut`
+)
+VALUES (
+NULL , 'Interface de GEPI', 'origine', 'n'
+);
+
+
+*/
+
+
+
+
+
+// Pour le tbs_multisite
 if (isset($_GET["rne"])) {
 	setcookie('RNE', $_GET["rne"]);
 }
@@ -71,245 +125,201 @@ if ($session_gepi->auth_locale && isset($_POST['login']) && isset($_POST['no_ant
 	}
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<!-- correction Regis html xmlns + majuscules -->
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<meta http-equiv="Pragma" content="no-cache" />
-<meta http-equiv="Cache-Control" content="no-cache" />
-<meta http-equiv="Expires" content="0" />
-<!-- correction Regis : déclaration par défaut pour les scripts et les mises en page-->
-<meta http-equiv="Content-Script-Type" content="text/javascript" />
-<meta http-equiv="Content-Style-Type" content="text/css" />
 
-<title><?php echo getSettingValue("gepiSchoolName"); ?> : base de données élèves | Identifiez vous...</title>
 
-<!-- Icones -->
-<link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" />
-<link rel="icon" type="image/ico" href="./favicon.ico" />
-
-<!-- fonction javascript -->
-<script src="lib/functions.js" type="text/javascript"></script>
-
-<script type="text/javascript">
-//<![CDATA[
-	// écrire à 'a' chez 'b'
-	function pigeon(a,b){
-		chaine_mel = "mailto:"+a+"_CHEZ_"+b+"?subject=[GEPI]";
-		chaine_mel += "&body=Pour que le mail parvienne à son destinataire, pensez à remplacer la chaîne de caractères _CHEZ_ par un @";
-		location.href = chaine_mel;
-	}
-
-	// écrire à l'administrateur
-	function pigeon2(){
-		chaine_tmp="";
-		for(i=0;i<adm_adr.length;i=i+2){
-			chaine_tmp=chaine_tmp+","+adm_adr[i]+"_CHEZ_"+adm_adr[i+1];
-		}
-		chaine_tmp=chaine_tmp.substring(1);
-		chaine_mel = "mailto:"+chaine_tmp+"?subject=[GEPI]";
-		chaine_mel += "&body=Pour que le mail parvienne à son destinataire, pensez à remplacer la chaîne de caractères _CHEZ_ par un @ dans l'adresse du destinataire.";
-		location.href = chaine_mel;
-	}
-//<!]]>
-</script>
-
-<!-- feuilles de style -->
 
 <?php
 
-	$style = getSettingValue("gepi_stylesheet");
-	if (empty($style)) $style = "style";
-	$style = "css/".$style;
-?>
+$test = 'templates/accueil_externe.php' ;
 
-<!-- feuillesde style générale -->
-<link rel="stylesheet" type="text/css" href="./<?php echo $style;?>.css" />
 
-<!-- feuilles de style normales -->
-<link rel="stylesheet" type="text/css" href="./<?php echo $style;?>_ecran.css" media="screen" />
-<link rel="stylesheet" type="text/css" href="./<?php echo $style;?>_telephone.css" media="handheld" />
-
-<!-- feuilles de style page login -->
-<link rel="stylesheet" type="text/css" href="./<?php echo $style;?>_ecran_login.css" media="screen" />
-<link rel="stylesheet" type="text/css" href="./<?php echo $style;?>_telephone_login.css" media="handheld" />
-
-<!-- feuilles de style IE -->
-<!--[if lte IE 7]>
-	<link rel="stylesheet" type="text/css" href="./<?php echo $style;?>_ecran_login_IE.css" media="screen" />
-<![endif]-->
-
-<?php
-	// Styles paramétrables depuis l'interface:
-	if($style_screen_ajout=='y'){
-		// La variable $style_screen_ajout se paramètre dans le /lib/global.inc
-		// C'est une sécurité... il suffit de passer la variable à 'n' pour désactiver ce fichier CSS et éventuellement rétablir un accès après avoir imposé une couleur noire sur noire
-		echo "<link rel='stylesheet' type='text/css' href='$gepiPath/style_screen_ajout.css' />\n";
+//==================================
+//Site en maintenance
+	$tbs_Site_ferme = array();
+	if ((getSettingValue("disable_login"))!='no'){
+		// Fermeture du site à afficher en rouge et plus grand
+		$tbs_Site_ferme[0] = "Le site est en cours de maintenance et temporairement inaccessible.";
+		$tbs_Site_ferme[1] = "Veuillez nous excuser de ce dérangement et réessayer de vous connecter ultérieurement.";
 	}
-?>
-<!-- fin des feuilles de style -->
-
-</head>
-<body onload="document.getElementById('login').focus()">
-
-	<?php
-
-		//==================================
-		//On vérifie si le module est activé
-		if (getSettingValue("active_cahiers_texte")=='y' and getSettingValue("cahier_texte_acces_public") == "yes" and getSettingValue("disable_login")!='yes') {
-			echo "<div id='ct_public'><a href=\"./public/index.php?id_classe=-1\">\n";
-				echo "<img src='./images/icons/cahier_texte.png' alt='' class='link' /> Consulter les cahiers de texte</a> (accès public)\n";
-			echo "</div>\n";
-		}
-		
-		//==================================
 
 
-		//==================================
+//==================================
+//On vérifie si le module cahiers de texte public est activé
+	$tbs_CdT_public_titre =  "" ;
+	if (getSettingValue("active_cahiers_texte")=='y' and getSettingValue("cahier_texte_acces_public") == "yes" and getSettingValue("disable_login")!='yes') {
+		$tbs_CdT_public_titre = "Consulter les cahiers de texte (accès public)";
+	}
+//==================================
+//Utilisation tbs_multisite
+	$tbs_multisite = "";
+	if (getSettingValue("tbs_multisite") == "y" AND isset($_GET["rne"]) AND $_GET["rne"] != '') {
+		$tbs_multisite = $_GET["rne"];
+	}
 
-		if ((getSettingValue("disable_login"))!='no'){
-		// if ((getSettingValue("disable_login"))!='yes'){
-			// Fermeture du site à afficher en rouge et plus grand
-			echo "<div id='Site_ferme'>\n";
-				echo "<p>Le site est en cours de maintenance et temporairement inaccessible.<br />Veuillez nous excuser de ce dérangement et réessayer de vous connecter ultérieurement.</p>\n";
-			echo "</div>\n";
-		}
-		//==================================
-	?>
-		<form action='login.php' method='post' >
-			<div id ='identification'>
-				<!-- Utilisation multisite -->
-				<?php	
-					if (getSettingValue("multisite") == "y" AND isset($_GET["rne"]) AND $_GET["rne"] != '') {
-						echo '	<input type="hidden" name="rne" value="'.$_GET["rne"].'" />';
-					}
-				?>				
-				<div id='div_login_entete'>
-					<?php
-						echo "<h1>".getSettingValue("gepiSchoolName")."</h1>\n";
-						echo "<p class='annee'>".getSettingValue("gepiYear")."</p>\n";
-					?>
-				</div>
-					
-				<div id='div_login'>
-					<?php
-					if (isset($message)) {
-						// echo("<p style='color:red; margin:0; padding:0;'>" . $message . "</p>");
-						echo("<p class='txt_rouge message'>" . $message . "</p>");
-					} else {
-						// echo "<p style='margin:0; padding:0;'>Afin d'utiliser Gepi, vous devez vous identifier.</p>";
-						echo "<p class='message'>Afin d'utiliser Gepi, vous devez vous identifier.</p>";
-					}
-					?>
-					<div>
-						<img class='cadenas' src='images/icons/lock.png' alt='Cadenas' />
-					</div>
-					<p class='droite'>
-						<label for='login'>Identifiant</label>
-						<input type='text' id='login' name='login' size='16' tabindex='1' />
-					</p>
-					<p class='droite'>
-						<label for='no_anti_inject_password'>Mot de passe</label>
-						<input type='password' id='no_anti_inject_password' name='no_anti_inject_password' size='16' onkeypress='capsDetect(arguments[0]);' tabindex='2' />
-					</p>
-						<p class='droite'>
-						<input type='submit' name='submit' value='Valider' style='font-variant: small-caps;' tabindex='3' />
-						</p>
-						<?php
-							if (getSettingValue("enable_password_recovery") == "yes") {
-								//echo "<a class='small' href='recover_password.php'>Mot de passe oublié ?</a>";
-								echo "<p id='mot_passe'><a class='small' href='recover_password.php'>Mot de passe oublié ?</a></p>\n";
-							}
-						?>
-				</div>
-			</div>
-		</form>
+//==================================
+//      Cadre identification
+//==================================
+
+//==================================
+//Nom année
+	$tbs_gepiSchoolName = getSettingValue("gepiSchoolName");
+	$tbs_gepiYear = getSettingValue("gepiYear");
+
+//==================================
+//Message
+	if (isset($message)) {
+		$tbs_message[] =array("classe"=>"txt_rouge","texte" => $message);
+	} else {
+		//$tbs_message_class = "message";
+		$tbs_message[] =array("classe"=>"","texte" => "Afin d'utiliser Gepi, vous devez vous identifier.");
+	}
+
+//==================================
+//	Mot de passe oublié
+	$tbs_password_recovery = "";
+	if (getSettingValue("enable_password_recovery") == "yes") {
+		$tbs_password_recovery = "recover_password.php";
+	}
+
+//==================================
+//	authentification unique
+	$tbs_SSO_lien = "";
+	if ($session_gepi->auth_sso) {
+		$tbs_SSO_lien = 'login_sso.php';
+	}
 
 
 
-		<div class="center info" >
-		<?php
-		if ($session_gepi->auth_sso) {
-			echo "<p><a href='login_sso.php'><img src='images/icons/forward_.png' /> Se connecter en utilisant le service d'authentification unique</a></p>";
-		}
-		?>
-		<p>
-			<a href="javascript:centrerpopup('gestion/info_vie_privee.php',700,480,'scrollbars=yes,statusbar=no,resizable=yes')">
-				<img src='./images/icons/vie_privee.png' alt='' class='link' /> Informations vie privée
-			</a>
-		</p>
-
-		<?php
-			if(getSettingValue("gepiAdminAdressPageLogin")!='n'){
-				$gepiAdminAdress=getSettingValue("gepiAdminAdress");
-				//$tmp_adr=explode("@",$gepiAdminAdress);
-				//echo("<a href=\"javascript:pigeon('$tmp_adr[0]','$tmp_adr[1]');\">[Contacter l'administrateur]</a> \n");
-
-				//echo "$gepiAdminAdress<br />";
-
-				$compteur=0;
-				$tab_adr=array();
-				$tmp_adr1=explode(",",$gepiAdminAdress);
-				for($i=0;$i<count($tmp_adr1);$i++){
-					//echo "\$tmp_adr1[$i]=$tmp_adr1[$i]<br />";
-					$tmp_adr2=explode("@",$tmp_adr1[$i]);
-					//echo "\$tmp_adr2[0]=$tmp_adr2[0]<br />";
-					//echo "\$tmp_adr2[1]=$tmp_adr2[1]<br />";
-					if((isset($tmp_adr2[0]))&&(isset($tmp_adr2[1]))) {
-						$tab_adr[$compteur]=$tmp_adr2[0];
-						$compteur++;
-						$tab_adr[$compteur]=$tmp_adr2[1];
-						$compteur++;
-					}
-				}
-
-				echo "<script type='text/javascript'>\n";
-					echo "adm_adr=new Array();\n";
-					for($i=0;$i<count($tab_adr);$i++){
-						echo "adm_adr[$i]='$tab_adr[$i]';\n";
-					}
-				echo "</script>\n";
-
-				if(count($tab_adr)>0){
-					//echo("<a href=\"javascript:pigeon2(adm_adr);\">[Contacter l'administrateur]</a> \n");
-					echo("<p><a href=\"javascript:pigeon2();\">[Contacter l'administrateur]</a></p>\n");
-				}
+//==================================
+//	administrateurs
+	$tbs_admin_adr=array();
+	$tbs_admin_titre="";
+	if(getSettingValue("gepiAdminAdressPageLogin")!='n'){
+		$gepiAdminAdress=getSettingValue("gepiAdminAdress");
+		//$tmp_adr=explode("@",$gepiAdminAdress);
+		//echo("<a href=\"javascript:pigeon('$tmp_adr[0]','$tmp_adr[1]');\">[Contacter l'administrateur]</a> \n");
+		//echo "$gepiAdminAdress<br />";
+		//$compteur=0;
+		$tab_adr=array();
+		$tmp_adr1=explode(",",$gepiAdminAdress);
+		for($i=0;$i<count($tmp_adr1);$i++){
+			//echo "\$tmp_adr1[$i]=$tmp_adr1[$i]<br />";
+			$tmp_adr2=explode("@",$tmp_adr1[$i]);
+			//echo "\$tmp_adr2[0]=$tmp_adr2[0]<br />";
+			//echo "\$tmp_adr2[1]=$tmp_adr2[1]<br />";
+			if((isset($tmp_adr2[0]))&&(isset($tmp_adr2[1]))) {
+				$tbs_admin_adr[]=array("nom"=>$tmp_adr2[0] , "fai"=>$tmp_adr2[1]);
+				/*
+				$tab_adr[$compteur]=$tmp_adr2[0];
+				$compteur++;
+				$tab_adr[$compteur]=$tmp_adr2[1];
+				$compteur++;
+				*/
 			}
-		?>
+		}
 
-		</div>
-
-
-		<div id="new_login_footer" class="center">
-			<a href="http://gepi.mutualibre.org/">GEPI : Outil de gestion, de suivi, et de visualisation graphique des résultats scolaires (écoles, collèges, lycées)</a><br />
-			Copyright &copy; 2001-2008
-			<?php
-				reset($gepiAuthors);
-				$i = 0;
-				while (list($name, $adress) = each($gepiAuthors)) {
-					if ($i != "0") echo ", ";
-					//echo("<a href=\"mailto:" . $adress . "\">" . $name . "</a> ");
-					$tmp_adr=explode("@",$adress);
-					//echo("<a href=\"javascript:pigeon('" . $adress . "');\">" . $name . "</a> ");
-					if((isset($tmp_adr[0]))&&(isset($tmp_adr[1]))) {
-						echo("<a href=\"javascript:pigeon('$tmp_adr[0]','$tmp_adr[1]');\">" . $name . "</a> \n");
-					}
-					$i++;
-				}
-				echo "<br/><br/>\n";
-				echo "<img src='".$gepiPath."/images/php-powered.png' alt='php powered' />&nbsp;\n";
-				echo "<img src='".$gepiPath."/images/mysql-powered.png' alt='mysql powered' />\n";
-
-			?>
-		</div> 
-
-
-		<?php
-			if(getSettingValue("gepi_pmv")!="n"){
-				if (@file_exists($gepiPath."/pmv.php")) require ($gepiPath."/pmv.php");
+		//echo "<script type='text/javascript'>\n";
+		//echo "adm_adr=new Array();\n";
+		/*
+			for($i=0;$i<count($tab_adr);$i++){
+				echo "adm_adr[$i]='$tab_adr[$i]';\n";
 			}
-		?>
+		//echo "</script>\n";
+		if(count($tab_adr)>0){
+			//echo("<a href=\"javascript:pigeon2(adm_adr);\">[Contacter l'administrateur]</a> \n");
+			//echo("<p><a href=\"javascript:pigeon2();\">[Contacter l'administrateur]</a></p>\n");
+		}
+		*/
+	}
 
-</body>
-</html>
+
+//==================================
+//	gabarits dynamiques
+
+
+
+
+//==================================
+//	switcher de gabarits
+
+	$tbs_dossier_gabarit=array();
+
+
+$test = mysql_query("SHOW TABLES LIKE 'gabarits'");
+
+		$sql="SELECT texte, repertoire, pardefaut FROM gabarits ;";
+		$res_gab=mysql_query($sql);
+	if($res_gab){
+
+		if(mysql_num_rows($res_gab)>0) {
+			while($lig_gab=mysql_fetch_object($res_gab)) {
+				$texte_gab=$lig_gab->texte;
+				$repertoire_gab=$lig_gab->repertoire;
+				$defaut_gab=$lig_gab->pardefaut;
+				if($defaut_gab=="y"){
+					$value_gab="selected='selected'";
+					$gabarit=$lig_gab->repertoire;
+				}else{
+					$value_gab="";
+				}
+			$tbs_dossier_gabarit[]=array("texte"=>$texte_gab, "selection"=>$value_gab, "value"=>$repertoire_gab);
+			}
+		}
+
+	}else{
+		$gabarit="origine";
+	}
+
+	if ((isset($_GET['template'])) or (isset($_POST['template'])) or (isset($gabarit))) {
+		$gabarit = isset($_POST['template']) ? unslashes($_POST['template']) : (isset($_GET['template']) ? unslashes($_GET['template']) : $gabarit);
+	}
+	else{
+		$gabarit="origine";
+	}
+
+
+
+//==================================
+// Décommenter la ligne ci-dessous pour afficher les variables $_GET, $_POST, $_SESSION et $_SERVER pour DEBUG:
+//debug_var();
+
+// appel des bibliothèques tinyButStrong
+
+		$testphp = phpversion();
+		// on teste le premier chiffre
+		$version = substr($testphp, 0, 1);
+		if ($version == 5) {
+			$_SESSION['tbs_class'] = 'tbs/tbs_class_php5.php';
+		}elseif($version == 4){
+			$_SESSION['tbs_class'] = 'tbs/tbs_class.php';
+		}else{
+			echo ("GEPI necessite PHP5 pour fonctionner");
+			die();
+		}
+			include_once($_SESSION['tbs_class']);
+
+
+
+	$_SESSION['rep_gabarits'] = $gabarit;
+
+//==================================
+// Appel de script externe
+
+	$entete_externe = "templates/".$_SESSION['rep_gabarits']."/login_entete_externe.php" ;
+	$corps_externe = "templates/".$_SESSION['rep_gabarits']."/login_corps_externe.php" ;
+	$pied_externe = "templates/".$_SESSION['rep_gabarits']."/login_pied_externe.php" ;
+
+	$fichier_gabarits='templates/'.$_SESSION['rep_gabarits'].'/login_template.html' ;
+
+	$TBS = new clsTinyButStrong ;
+	$TBS->LoadTemplate($fichier_gabarits) ;
+	$TBS->MergeBlock('tbs_blk1',$tbs_Site_ferme);
+	$TBS->MergeBlock('tbs_blk2',$tbs_admin_adr);
+	$TBS->MergeBlock('tbs_blk3',$tbs_dossier_gabarit);
+	$TBS->MergeBlock("tbs_message",$tbs_message);
+	$TBS->Show() ;
+
+// ------ on vide les tableaux -----
+	unset($tbs_Site_ferme,$tbs_admin_adr,$tbs_dossier_gabarit,$tbs_message);
+
+?> 
