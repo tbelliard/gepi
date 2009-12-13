96495c144a17bff3ba11570d03da716e77b171d4
regis <regis@e073496e-d129-0410-8f7b-841bfd467322>
2009-12-13 12:08:34 +0000

    logout.php : utilisation d'un gabarit php pour l'affichage
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@3917 e073496e-d129-0410-8f7b-841bfd467322

--- logout.php
+++ logout.php
@@ -35,22 +35,25 @@ if ($session_gepi->current_auth_mode == "sso" and $session_gepi->auth_sso == "ca
 }
 
 // Ajout pour le multisite
-	if (isset($_COOKIE["RNE"])) {
-		unset($_COOKIE['RNE']);
-		setcookie('RNE', 'RNE'); // permet d'effacer le contenu du cookie.
-	}
+if (isset($_COOKIE["RNE"])) {
+	unset($_COOKIE['RNE']);
+	setcookie('RNE', 'RNE'); // permet d'effacer le contenu du cookie.
+}
 
-    $message = "<h1 class='gepi'>Déconnexion</h1>";
-	$message .= "<img src='$gepiPath/images/icons/lock-open.png' alt='lock-open' /><br/><br/>";
+	
+    //$message = "<h1 class='gepi'>Déconnexion</h1>";
+    $titre= "Déconnexion";
+    $message = "";
+	//$message .= "<img src='$gepiPath/images/icons/lock-open.png' alt='lock-open' /><br/><br/>";
     if (!$_GET['auto']) {
         $session_gepi->close($_GET['auto']);
-        $message .= "Vous avez fermé votre session GEPI.<br />";
-        $message .= "<a href=\"$gepiPath/login.php\">Ouvrir une nouvelle session</a>.";
+        $message .= "Vous avez fermé votre session GEPI.";
+        //$message .= "<a href=\"$gepiPath/login.php\">Ouvrir une nouvelle session</a>.";
     } else if ($_GET['auto']==2) {
         $session_gepi->close($_GET['auto']);
-        $message .= "Vous avez été déconnecté. Il peut s'agir d'une mauvaise configuration de la variable \$GepiPath dans la fichier \"connect.inc.php\"<br />
-        <a href='aide_gepipath.php'><b>Aide à la configuration de \$GepiPath</b></a><br /><br />";
-        $message .= "<a href=\"$gepiPath/login.php\">Ouvrir une nouvelle session</a>.";
+        $message .= "Vous avez été déconnecté. Il peut s'agir d'une mauvaise configuration de la variable \$GepiPath dans le fichier \"connect.inc.php\"<br />
+        <a href='aide_gepipath.php'><b>Aide à la configuration de \$GepiPath</b></a>";
+        //$message .= "<a href=\"$gepiPath/login.php\">Ouvrir une nouvelle session</a>.";
     } else if ($_GET['auto']==3) {
         $date_fermeture = date("d\/m\/Y\ \à\ H\ \h\ i");
         $debut_session = urldecode($_GET['debut_session']);
@@ -58,59 +61,24 @@ if ($session_gepi->current_auth_mode == "sso" and $session_gepi->auth_sso == "ca
         if (sql_query1($sql)) {
            // Le temps d'inactivité est dépassé
            $session_gepi->close($_GET['auto']);
-           $message .= "Votre session GEPI a expiré car le temps maximum (".getSettingValue("sessionMaxLength")." minutes) sans échange avec le serveur a été atteint.<br /><br />Date et heure de la déconnexion : ".$date_fermeture."<br /><br />";
-           $message .= "<a href=\"$gepiPath/login.php\">Ouvrir une nouvelle session</a>.";
+           $message .= "Votre session GEPI a expiré car le temps maximum (".getSettingValue("sessionMaxLength")." minutes) sans échange avec le serveur a été atteint.<br /><br />Date et heure de la déconnexion : ".$date_fermeture."";
+           //$message .= "<a href=\"$gepiPath/login.php\">Ouvrir une nouvelle session</a>.";
         } else {
            $message .= "<h1 class='gepi'>Fermeture d'une fenêtre GEPI</h1>";
+           $titre= "Fermeture d'une fenêtre GEPI";
            $message .= "A l'heure ci-dessous, une fenêtre GEPI s'est automatiquement fermée par mesure de sécurité car
            le temps maximum d'inactivité (".getSettingValue("sessionMaxLength")." minutes) avait été atteint.<br /><br />
            Heure et date de fermeture de la fenêtre : ".$date_fermeture;
+           //$message .= "<a href=\"$gepiPath/login.php\">Ouvrir une nouvelle session</a>.";
         }
     } else {
         $session_gepi->close($_GET['auto']);
         $message .= "Votre session GEPI a expiré, ou bien vous avez été déconnecté.<br />";
-        if ((getSettingValue("disable_login"))=='yes') $message .=  "<br /><font color=\"red\" size=\"+1\">Le site est momentanément inaccessible. Veuillez nous excuser de ce dérangement !</font><br /><br />";
-        $message .= "<a href=\"$gepiPath/login.php\">Ouvrir une nouvelle session</a>.";
+        if ((getSettingValue("disable_login"))=='yes')  {
+        	$message .=  "<br /><span class=\"rouge gras\">Le site est momentanément inaccessible. Veuillez nous excuser de ce dérangement !<span>";
+        }
+        //$message .= "<a href=\"$gepiPath/login.php\">Ouvrir une nouvelle session</a>.";
     }
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html lang="fr">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<META HTTP-EQUIV="Pragma" CONTENT="no-cache" />
-<META HTTP-EQUIV="Cache-Control" CONTENT="no-cache" />
-<META HTTP-EQUIV="Expires" CONTENT="0" />
-<title>Déconnexion</title>
-<link rel="stylesheet" type="text/css" href="./<?php echo getSettingValue("gepi_stylesheet");?>.css" />
-<link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" />
-<link rel="icon" type="image/ico" href="./favicon.ico" />
-<?php
-	// Styles paramétrables depuis l'interface:
-	if($style_screen_ajout=='y'){
-		// La variable $style_screen_ajout se paramètre dans le /lib/global.inc
-		// C'est une sécurité... il suffit de passer la variable à 'n' pour désactiver ce fichier CSS et éventuellement rétablir un accès après avoir imposé une couleur noire sur noire
-		echo "<link rel='stylesheet' type='text/css' href='$gepiPath/style_screen_ajout.css' />";
-	}
-?>
-</head>
-<body>
-<div class="center">
-<?php
-echo $message;
 
-$agent = $_SERVER['HTTP_USER_AGENT'];
-
-if (my_eregi("msie",$agent) && !eregi("opera",$agent)) {
-	echo "<div style='width: 70%; margin: auto;'>";
-	echo "<p><b>Note aux utilisateurs de Microsoft Internet Explorer :</b>";
-	echo "<br/>Si vous subissez des déconnexions intempestives, si vous n'arrivez pas à vous connecter à Gepi, " .
-			"ou bien s'il vous faut répéter plusieurs fois la procédure de connexion avant de pouvoir accéder aux outils de Gepi, " .
-			"il est possible que votre navigateur en soit la cause. Nous vous recommandons de télécharger gratuitement et d'installer <a href='http://www.mozilla-europe.org/fr/products/firefox/'>Mozilla Firefox</a>, " .
-			"qui vous garantira les meilleures conditions d'utilisation de Gepi.</p>";
-	echo "</div>";
-}
-
+include('./templates/origine/logout_template.php');
 ?>
-</div>
-</body>
-</html>
---
+++ templates/origine/logout_template.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * $Id: logout_template.php $
+ */
+?>
+ 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Cache-Control" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
+<title>Déconnexion</title>
+<link rel="stylesheet" type="text/css" href="<?php echo $gepiPath;?>/css/style.css" />
+<link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" />
+<link rel="icon" type="image/ico" href="./favicon.ico" />
+<?php
+	// Styles paramétrables depuis l'interface:
+	if($style_screen_ajout=='y'){
+		// La variable $style_screen_ajout se paramètre dans le /lib/global.inc
+		// C'est une sécurité... il suffit de passer la variable à 'n' pour désactiver ce fichier CSS et éventuellement rétablir un accès après avoir imposé une couleur noire sur noire
+		echo "<link rel='stylesheet' type='text/css' href='$gepiPath/templates/origine/css/logout.css' />";
+		echo "<link rel='stylesheet' type='text/css' href='$gepiPath/style_screen_ajout.css' />";
+	}
+?>
+</head>
+<body>
+<div class="center">
+	<h1 class='gepi'><?php echo $titre;?></h1>
+	<img src="<?php echo $gepiPath;?>/images/icons/lock-open.png" alt='lock-open' />
+	<br/><br/>
+<?php
+	echo $message;
+?>
+	<br />
+	<a href="<?php echo $gepiPath;?>/login.php">Ouvrir une nouvelle session</a>
+
+<?php
+$agent = $_SERVER['HTTP_USER_AGENT'];
+if (my_eregi("msie",$agent) && !my_eregi("opera",$agent)) {
+
+	echo "<div style='width: 70%; margin: auto;'>";
+	echo "<p><b>Note aux utilisateurs de Microsoft Internet Explorer :</b>";
+	echo "<br/>Si vous subissez des déconnexions intempestives, si vous n'arrivez pas à vous connecter à Gepi, " .
+			"ou bien s'il vous faut répéter plusieurs fois la procédure de connexion avant de pouvoir accéder aux outils de Gepi, " .
+			"il est possible que votre navigateur en soit la cause. Nous vous recommandons de télécharger gratuitement et d'installer <a href='http://www.mozilla-europe.org/fr/products/firefox/'>Mozilla Firefox</a>, " .
+			"qui vous garantira les meilleures conditions d'utilisation de Gepi.</p>";
+	echo "</div>";
+}
+?>
+
+
+</div>
+</body>
+</html>
