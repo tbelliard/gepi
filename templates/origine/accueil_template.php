0c04acec9d88b3b6bdb83aab47631a358fb74f78
regis <regis@e073496e-d129-0410-8f7b-841bfd467322>
2009-12-14 16:30:47 +0000

    accueil.php : correction du CSS de la case sauvegarde
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@3930 e073496e-d129-0410-8f7b-841bfd467322

--- templates/origine/accueil_template.php
+++ templates/origine/accueil_template.php
@@ -217,8 +217,7 @@
 				<input type=\"submit\" value=\"Lancer une sauvegarde de la base de données\" />
 			</p>
 		</form>
-		<!--p class='small'-->
-		<p style='font-size:x-small'>
+		<p class='small'>
 			Les répertoires \"documents\" (<em>contenant les documents joints aux cahiers de texte</em>) et \"photos\" (<em>contenant les photos du trombinoscope</em>) ne seront pas sauvegardés.<br />
 			Un outil de sauvegarde spécifique se trouve en bas de la page <a href='./gestion/accueil_sauve.php#zip'>gestion des sauvegardes</a>.
 		</p>
