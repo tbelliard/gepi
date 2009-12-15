20a0e271f0d3af01b13f17ab62f1be1412bf26c5
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2009-12-15 18:29:03 +0000

    Affichage des moyennes générales sur le graphe en étoile.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@3933 e073496e-d129-0410-8f7b-841bfd467322

--- visualisation/draw_graphe_star.php
+++ visualisation/draw_graphe_star.php
@@ -790,7 +790,10 @@
 	for($k=1;$k<=$nb_series;$k++){
 		//$largeur_eleve[$k] = strlen($eleve[$k]) * ImageFontWidth($taille_police);
 		//$total_largeur_eleves=$total_largeur_eleves+$largeur_eleve[$k];
-		$largeur_chaine[$k] = strlen($chaine[$k]) * ImageFontWidth($taille_police);
+
+		//$largeur_chaine[$k] = strlen($chaine[$k]) * ImageFontWidth($taille_police);
+		$largeur_chaine[$k] = strlen($chaine[$k]." (".nf($mgen[$k]).")") * ImageFontWidth($taille_police);
+
 		$total_largeur_chaines=$total_largeur_chaines+$largeur_chaine[$k];
 	}
 
@@ -816,7 +819,8 @@
 			$xtmp=$xtmp+$espace;
 			//imagestring ($img, $taille_police, $xtmp, 5, $eleve[$k], $couleureleve[$k]);
 			//$xtmp=$xtmp+$largeur_eleve[$k];
-			imagestring ($img, $taille_police, $xtmp, ImageFontHeight($taille_police)+5, strtr($chaine[$k],"_"," "), $couleureleve[$k]);
+			//imagestring ($img, $taille_police, $xtmp, ImageFontHeight($taille_police)+5, strtr($chaine[$k],"_"," "), $couleureleve[$k]);
+			imagestring ($img, $taille_police, $xtmp, ImageFontHeight($taille_police)+5, strtr($chaine[$k],"_"," ")." (".nf($mgen[$k]).")", $couleureleve[$k]);
 			$xtmp=$xtmp+$largeur_chaine[$k];
 		}
 
@@ -833,7 +837,8 @@
 			$xtmp=$xtmp+$espace;
 			//imagestring ($img, $taille_police, $xtmp, 5, $eleve[$k], $couleureleve[$k]);
 			//$xtmp=$xtmp+$largeur_eleve[$k];
-			imagestring ($img, $taille_police, $xtmp, 5, strtr($chaine[$k],"_"," "), $couleureleve[$k]);
+			//imagestring ($img, $taille_police, $xtmp, 5, strtr($chaine[$k],"_"," "), $couleureleve[$k]);
+			imagestring ($img, $taille_police, $xtmp, 5, strtr($chaine[$k],"_"," ")." (".nf($mgen[$k]).")", $couleureleve[$k]);
 			$xtmp=$xtmp+$largeur_chaine[$k];
 		}
 	}
