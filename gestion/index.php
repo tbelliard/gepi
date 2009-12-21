56a318d11d1b57016521da0f3048233121ecb7e6
regis <regis@e073496e-d129-0410-8f7b-841bfd467322>
2009-12-21 18:32:30 +0000

    gestion/index.php : affichage par gabarit de Gestion générale
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@3953 e073496e-d129-0410-8f7b-841bfd467322

--- gestion/index.php
+++ gestion/index.php
@@ -20,6 +20,32 @@
  * along with GEPI; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+ 
+ 
+/* ---------Variables envoyées au gabarit
+*
+*	$tbs_menu
+*				-> classe								classe CSS
+*				-> image								icone du lien
+*				-> texte								texte du titre du menu
+*				-> entree								entrées du menu
+*							-> lien						lien vers la page
+*							-> titre   				texte du lien
+*							-> expli					explications
+*	$niveau_arbo									Niveau dans l'arborescence
+*	$titre_page										Titre de la page
+*	$tbs_last_connection					Vide, pour ne pas avoir d'erreur dans le bandeau
+*	$tbs_retour										Lien retour arrière
+*	$tbs_ariane										Fil d'arianne
+*
+*
+*	Variables héritées de :
+*
+*	header_template.inc
+*	header_barre_prof_template.inc
+*	footer_template.inc.php
+*
+ */
 
 // Initialisations files
 require_once("../lib/initialisations.inc.php");
@@ -41,13 +67,26 @@ if (!checkAccess()) {
 }
 
 //**************** EN-TETE *****************
+// Begin standart header
+$niveau_arbo = 1;
 $titre_page = "Gestion générale";
-require_once("../lib/header.inc");
+$tbs_last_connection="";
+
+// ====== Inclusion des balises head et du bandeau =====
+include_once("./../lib/header_template.inc");
+// require_once("../lib/header.inc");
 //**************** FIN EN-TETE *****************
 
 ?>
-<p class='bold'><a href="../accueil.php"><img src='../images/icons/back.png' alt='Retour' class='back_link' /> Retour</a></p>
+<!-- <p class='bold'><a href="../accueil.php"><img src='../images/icons/back.png' alt='Retour' class='back_link' /> Retour</a></p> -->
+<?php 
+	$tbs_retour="../accueil.php"; 
+	$tbs_ariane[0]=array("titre" => "accueil" , "lien"=>"../accueil.php");
+?>
+<!-- 
 <center>
+-->
+<!-- 
 <table class='menu' summary='Menu sécurité'>
 <tr>
 	<th colspan='2'><img src='../images/icons/securite.png' alt='Sécurité' class='link'/> - Sécurité</th>
@@ -72,7 +111,37 @@ require_once("../lib/header.inc");
 	<td>Voir la configuration du serveur php/Mysql pour v&eacute;rifier la compatibilit&eacute; avec Gepi.</td>
 </tr>
 </table>
+ -->
+<?php 
+	$nummenu=0;
+	$tbs_menu[$nummenu]=array('classe'=>'accueil' , 'image'=>'../images/icons/securite.png' , 'texte'=>"Sécurité");
+	$chemin = array();
+  $titre = array();
+  $expli = array();
+  
+  $chemin = "gestion_connect.php";
+  $titre = "Gestion des connexions";
+  $expli = "Affichage des connexions en cours, activation/désactivation des connexions pour le site, protection contre les attaques forces brutes, journal des connexions, changement de mot de passe obligatoire.";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+  
+  $chemin = "security_panel.php";
+  $titre = "Panneau de contrôle sécurité";
+  $expli = "Visualiser les tentatives d'utilisation illégale de Gepi.";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+  
+  $chemin = "security_policy.php";
+  $titre = "Politique de sécurité";
+  $expli = "Définir les seuils d'alerte et les actions à entreprendre dans le cas de tentatives d'intrusion ou d'accès illégal à des ressources.";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+  
+  $chemin = "../mod_serveur/test_serveur.php";
+  $titre = "Configuration serveur";
+  $expli = "Voir la configuration du serveur php/Mysql pour v&eacute;rifier la compatibilit&eacute; avec Gepi.";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
 
+?>
+
+<!--
 <table class='menu' summary='Menu général'>
 <tr>
 	<th colspan='2'><img src='../images/icons/configure.png' alt='Configuration' class='link' /> - Général</th>
@@ -106,7 +175,47 @@ require_once("../lib/header.inc");
     <td>Paramétrage des couleurs de fond d'écran et du dégradé d'entête.</td>
 </tr>
 </table>
+ -->
+<?php 
+	$nummenu=1;
+	$tbs_menu[$nummenu]=array('classe'=>'accueil' , 'image'=>'../images/icons/configure.png' , 'texte'=>"Général");
+	$chemin = array();
+  $titre = array();
+  $expli = array();
+  
+  $chemin = "param_gen.php";
+  $titre = "Configuration générale";
+  $expli = "Permet de modifier des paramètres généraux (nom de l'établissement, adresse, ...).";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+  
+  $chemin = "droits_acces.php";
+  $titre = "Droits d'accès";
+  $expli = "Modifier les droits d'accès à certaines fonctionnalités selon le statut de l'utilisateur.";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+  
+  $chemin = "options_connect.php";
+  $titre = "Options de connexions";
+  $expli = "Gestion de la procédure automatisée de récupération de mot de passe, paramétrage du mode de connexion (autonome ou Single Sign-On), changement de mot de passe obligatoire, réglage de la durée de conservation des connexions, suppression de toutes les entrées du journal de connexion.";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+  
+  $chemin = "modify_impression.php";
+  $titre = "Gestion de la fiche \"bienvenue\"";
+  $expli = "Permet de modifier la feuille d'information à imprimer pour chaque nouvel utilisateur créé.";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+  
+  $chemin = "config_prefs.php";
+  $titre = "Paramétrage de l'interface ".$gepiSettings['denomination_professeur'];
+  $expli = "Paramétrage des items de l'interface simplifiée pour certaines pages. Gestion du menu en barre horizontale.";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+  
+  $chemin = "param_couleurs.php";
+  $titre = "Paramétrage des couleurs";
+  $expli = "Paramétrage des couleurs de fond d'écran et du dégradé d'entête.";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+  
+?>
 
+<!--
 <table class='menu' summary='Menu gestion des BDD'>
 <tr>
 	<th colspan='2'><img src='../images/icons/database.png' alt='Gestion bases de données' class='link' /> - Gestion des bases de données </th>
@@ -142,7 +251,48 @@ require_once("../lib/header.inc");
 </tr>
 
 </table>
+ -->
+<?php 
+	$nummenu=2;
+	$tbs_menu[$nummenu]=array('classe'=>'accueil' , 'image'=>'../images/icons/database.png' , 'texte'=>"Gestion des bases de données");
+	$chemin = array();
+  $titre = array();
+  $expli = array();  
 
+  $chemin = "accueil_sauve.php";
+  $titre = "Sauvegardes et restauration";
+  $expli = "Sauvegarder la base GEPI sous la forme d'un fichier au format \"mysql\".<br />
+    Restaurer des données dans la base Mysql de GEPI à partir d'un fichier.";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+  
+  $chemin = "../utilitaires/maj.php";
+  $titre = "Mise à jour de la base";
+  $expli = "Permet d'effectuer une mise à jour de la base MySql après un changement de version  de GEPI.";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+  
+  $chemin = "../utilitaires/clean_tables.php";
+  $titre = "Nettoyage des tables";
+  $expli = "Procéder à un nettoyage des tables de la base MySql de GEPI (suppression de certains doublons et/ou lignes obsolètes ou orphelines).";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+  
+  $chemin = "efface_base.php";
+  $titre = "Effacer la base";
+  $expli = "Permet de réinitialiser les bases en effaçant toutes les données ".$gepiSettings['denomination_eleves']." de la base.";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+  
+  $chemin = "efface_photos.php";
+  $titre = "Effacer les photos";
+  $expli = "Permet d'effacer les photos des ".$gepiSettings['denomination_eleves']." qui ne sont plus dans la base.";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+  
+  $chemin = "gestion_temp_dir.php";
+  $titre = "Gestion des dossiers temporaires";
+  $expli = "Permet de contrôler le volume occupé par les dossiers temporaires (<i>utilisés notamment pour générer les fichiers tableur OpenOffice (ODS), lorsque la fonction est activée dans le module carnet de notes</i>), de supprimer ces dossiers,...";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli); 
+  
+?>
+
+<!--
 <table class='menu' summary='Menu initialisation'>
 <tr>
 <th colspan='2'><img src='../images/icons/package.png' alt='Initialisation' class='link' /> - Outils d'initialisation</th>
@@ -181,6 +331,7 @@ if (LDAPServer::is_setup()) {
 	<i>Les XML sont traités pour générer des fichiers CSV qui sont ensuite réclamés dans les différentes étapes de l'initialisation.</i>
     </td>
 </tr>
+ -->
 <!--tr>
     <td width='200'><a href="../init_dbf_sts/index.php">Initialisation des données à partir de fichiers DBF et XML</a> (OBSOLETE)</td>
     <td>Permet d'importer les données <?php echo $gepiSettings['denomination_eleves']; ?>, classes, <?php echo $gepiSettings['denomination_professeurs']; ?>, matières depuis deux fichiers DBF et l'export XML de STS.<br />
@@ -193,6 +344,68 @@ if (LDAPServer::is_setup()) {
 	<span style='color:red; '>Cette solution ne sera plus maintenue dans la future version 1.5.2 de Gepi.</span>
     </td>
 </tr-->
+<!--
 </table>
+ -->
+<?php 
+	$nummenu=3;
+	$tbs_menu[$nummenu]=array('classe'=>'accueil' , 'image'=>'../images/icons/package.png' , 'texte'=>"Outils d'initialisation");
+	$chemin = array();
+  $titre = array();
+  $expli = array();
+	
+if (LDAPServer::is_setup()) {	
+	
+	$chemin="../init_scribe/index.php";
+	$titre = "Initialisation à partir de l'annuaire LDAP du serveur Eole Scribe";
+	$expli = "Permet d'importer les données ".$gepiSettings['denomination_eleves'].", classes, ".$gepiSettings['denomination_professeurs'].", matières directement depuis le serveur LDAP de Scribe.";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+
+	$chemin="../init_lcs/index.php";
+	$titre = "Initialisation à partir de l'annuaire LDAP du serveur LCS";
+	$expli = "Permet d'importer les données ".$gepiSettings['denomination_eleves'].", classes, ".$gepiSettings['denomination_professeurs'].", matières directement depuis le serveur LDAP de LCS.";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+    
+}
+	
+	$chemin = "../init_csv/index.php";
+	$titre = "Initialisation des données à partir de fichiers CSV";
+	$expli = "Permet d'importer les données ".$gepiSettings['denomination_eleves'].", classes, ".$gepiSettings['denomination_professeurs'].", matières depuis des fichiers CSV, par exemple des exports depuis Sconet.";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+  
+  $chemin = "../init_xml2/index.php";
+  $titre = "Initialisation des données à partir de fichiers XML";
+  $expli = "Permet d'importer les données ".$gepiSettings['denomination_eleves'].", classes, ".$gepiSettings['denomination_professeurs'].", matières depuis les exports XML de Sconet/STS.<br />
+	<b>Nouvelle procédure:</b> Plus simple et moins gourmande en ressources que l'ancienne méthode ci-dessous.";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+  
+  $chemin = "../init_xml/index.php";
+  $titre = "Initialisation des données à partir de fichiers XML";
+  $expli = "Permet d'importer les données ".$gepiSettings['denomination_eleves'].", classes, ".$gepiSettings['denomination_professeurs'].", matières depuis les exports XML de Sconet/STS.<br />
+	<i>Les XML sont traités pour générer des fichiers CSV qui sont ensuite réclamés dans les différentes étapes de l'initialisation.</i>";
+  $tbs_menu[$nummenu]['entree'][]=array('lien'=>$chemin , 'titre'=>$titre, 'expli'=>$expli);
+	
+	
+	
+	
+?>	
+	
+<!--
 </center>
-<?php require("../lib/footer.inc.php");?>
+ -->
+
+
+<?php 
+	$tbs_microtime	="";
+	$tbs_pmv="";
+	require_once ("./../lib/footer_template.inc.php");
+
+	
+//==================================
+// Décommenter la ligne ci-dessous pour afficher les variables $_GET, $_POST, $_SESSION et $_SERVER pour DEBUG:
+//debug_var();
+
+
+include('./../templates/origine/gestion_generale_template.php');
+
+?>
---
+++ templates/origine/gestion_generale_template.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php
+/*
+ * $Id: gestion_generale_template.php $
+*/
+?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+
+<head>
+
+<!-- on inclut l'entête -->
+	<?php include('./../templates/origine/header_template.php');?>
+	
+	<link rel="stylesheet" type="text/css" href="./../accessibilite.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="./../accessibilite_print.css" media="print" />
+
+	<link rel="stylesheet" type="text/css" href="./../templates/origine/css/accueil.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="./../templates/origine/css/bandeau.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="./../templates/origine/css/gestion_generale.css" media="screen" />
+	
+<!-- corrections internet Exploreur -->	
+	<!--[if lte IE 7]>
+		<link title='bandeau' rel='stylesheet' type='text/css' href='./templates/origine/css/accueil_ie.css' media='screen' />
+		<link title='bandeau' rel='stylesheet' type='text/css' href='./templates/origine/css/bandeau_ie.css' media='screen' />
+	<![endif]-->
+	<!--[if lte IE 6]>
+		<link title='bandeau' rel='stylesheet' type='text/css' href='./templates/origine/css/accueil_ie6.css' media='screen' />
+	<![endif]-->
+	<!--[if IE 7]>
+		<link title='bandeau' rel='stylesheet' type='text/css' href='./templates/origine/css/accueil_ie7.css' media='screen' />
+	<![endif]-->
+
+<!-- Fin des styles -->
+</head>
+
+<body onload="show_message_deconnexion();">	
+
+<!-- on inclut le bandeau -->
+	<?php include('./../templates/origine/bandeau_template.php');?>
+	
+<!-- fin bandeau_template.html      -->
+
+<div id='container'>
+	<p class='ariane'>
+		<a href="<?php echo $tbs_ariane[0]['lien'];?>">
+			<?php echo $tbs_ariane[0]['titre'];?>
+		</a>
+	</p>
+	<p class='bold'>
+		<a href="<?php echo $tbs_retour;?>">
+			<img src='../images/icons/back.png' alt='Retour' class='back_link' /> Retour
+		</a>
+	</p>
+
+<a name="contenu" class="invisible">Début de la page</a>	
+	
+<?php	
+				if (count($tbs_menu)) {
+				$menu=array_values($tbs_menu);
+				if ($menu[0]['texte']!="") {
+					foreach ($tbs_menu as $value) {
+						echo "
+	<h2 class='$value[classe]' style='margin-bottom:0;'> 
+		<img src='$value[image]' alt='' /> - $value[texte]
+	</h2>
+				";
+						if ($value['texte']=="Administration") {
+							echo "
+<!-- sauvegarde -->	
+	<div class=\"div_tableau cellule_1\">
+		<form enctype=\"multipart/form-data\" action=\"gestion/accueil_sauve.php\" method=\"post\" id=\"formulaire\" >
+			<p>
+				<input type='hidden' name='action' value='system_dump' />
+				<input type=\"submit\" value=\"Lancer une sauvegarde de la base de données\" />
+			</p>
+		</form>
+		<p class='small'>
+			Les répertoires \"documents\" (<em>contenant les documents joints aux cahiers de texte</em>) et \"photos\" (<em>contenant les photos du trombinoscope</em>) ne seront pas sauvegardés.<br />
+			Un outil de sauvegarde spécifique se trouve en bas de la page <a href='./gestion/accueil_sauve.php#zip'>gestion des sauvegardes</a>.
+		</p>
+	</div>	
+				";
+						}
+							echo "
+<!-- autres menus -->		
+<!-- accueil_menu_template.php -->
+				";
+						if (count($value['entree'])) {
+							foreach ($value['entree'] as $newentree) {
+								include('./../templates/origine/accueil_menu_template.php');
+							}
+						}
+							echo "
+<!-- Fin menu	général -->
+				";
+					}
+				}
+			}
+	?>
+</div>
+</body>
+</html>
