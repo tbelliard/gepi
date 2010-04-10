a0f6c92fa878584d9d8af7b4ea5720b55c0e49ea
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2010-04-10 19:35:13 +0000

    Mise en place des listes aAllowedTags et aAllowedAttr proposés par misterphi. Le dispositif est encore inactif par défaut.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@4272 e073496e-d129-0410-8f7b-841bfd467322

--- lib/traitement_data.inc.php
+++ lib/traitement_data.inc.php
@@ -121,8 +121,13 @@ array_walk($_COOKIE, 'anti_inject');
 $filtrage_html="n";
 if($filtrage_html=='y') {
 
-	$aAllowedTags = array("u", "b", "i", "span", "p", "blink", "blockquote", "br", "caption");
-	$aAllowedAttr = array("abbr", "align", "alt", "axis", "background", "behavior", "title", "class");
+	//$aAllowedTags = array("u", "b", "i", "span", "p", "blink", "blockquote", "br", "caption");
+	//$aAllowedAttr = array("abbr", "align", "alt", "axis", "background", "behavior", "title", "class");
+
+	$aAllowedTags = array("a", "b", "blink", "blockquote", "br", "caption", "center", "col", "colgroup", "comment", "em", "font", "h1", "h2", "h3", "h4", "h5", "h6", "hr", "img", "li", "marquee", "ol", "p", "pre", "s", "small", "span", "strike", "strong", "sub", "sup", "table", "tbody", "td", "tfoot", "th", "thead", "tr", "tt", "u", "ul", "color");
+	
+	$aAllowedAttr = array("abbr", "align", "alt", "axis", "background", "behavior", "bgcolor", "border", "bordercolor", "bordercolordark", "bordercolorlight", "bottompadding", "cellpadding", "cellspacing", "char", "charoff", "cite", "clear", "color", "cols", "direction", "face", "font-weight", "headers", "height", "href", "hspace", "leftpadding", "loop", "noshade", "nowrap", "point-size", "rel", "rev", "rightpadding", "rowspan", "rules", "scope", "scrollamount", "scrolldelay", "size", "span", "src", "start", "summary", "target", "title", "toppadding", "type", "valign", "value", "vspace", "width", "wrap", "style");
+
 
 	$oMyFilter = new InputFilter($aAllowedTags, $aAllowedAttr, 0, 0, 1);
 
