e86e8f314be347dfda55b04fa360eea8669b9959
jjacquard <jjacquard@e073496e-d129-0410-8f7b-841bfd467322>
2010-04-13 12:20:46 +0000

    Mise à jour des objet propel suite a generation
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@4302 e073496e-d129-0410-8f7b-841bfd467322

--- orm/propel-build/classes/gepi/JGroupesClassesPeer.php
+++ orm/propel-build/classes/gepi/JGroupesClassesPeer.php
@@ -4,7 +4,7 @@
 /**
  * Skeleton subclass for performing query and update operations on the 'j_groupes_classes' table.
  *
- * Table permettant la jointure entre groupe d'eleves et une classe. Cette jointure permet de definir un enseignement, c'est à dire un groupe d'eleves dans une meme classe. Est rarement utilise directement dans le code. Cette jointure permet de definir un coefficient et une valeur ects pour un groupe sur une classe
+ * Table permettant la jointure entre groupe d'enseignement et une classe. Cette jointure permet de definir un enseignement, c'est à dire un groupe d'eleves dans une meme classe. Est rarement utilise directement dans le code. Cette jointure permet de definir un coefficient et une valeur ects pour un groupe sur une classe
  *
  * You should add additional methods to this class to meet the
  * application requirements.  This class will only be generated as
