9f2a4cefc7ef8e0ec120ba63a350229ec4e82975
delineau <delineau@e073496e-d129-0410-8f7b-841bfd467322>
2010-05-15 19:34:09 +0000

    Correction de bug dans le cas où plugin.xml ne contient pas de requete mysql à éxécuter
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@4445 e073496e-d129-0410-8f7b-841bfd467322

--- mod_plugins/traiterRequetes.class.php
+++ mod_plugins/traiterRequetes.class.php
@@ -65,8 +65,13 @@ class traiterRequetes {
   public function  __construct(simpleXMLElement $requetes) {
 
     $this->_requetes = $requetes;
+    if (count($this->_requetes->requete)==0)
+            $this->_reponse = true;
     foreach ($this->_requetes->requete as $requete) {
       // On est face à une liste de requêtes
+      if (trim($requete)=='') {
+        $this->_reponse = true;
+      }else
       if ($this->verifRequete($requete) === true){
         $this->insertRequete($requete);
       }else{
