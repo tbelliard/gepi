7c788adc33ad5a4009f4dc3023eb3b02cd6de493
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2010-07-06 14:22:39 +0000

    Correction d'un oubli/prise en compte du changement de fonctionnement de bulletin_classe().
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@4693 e073496e-d129-0410-8f7b-841bfd467322

--- saisie/saisie_synthese_app_classe.php
+++ saisie/saisie_synthese_app_classe.php
@@ -183,18 +183,152 @@ if((isset($id_classe))&&(isset($num_periode))) {
 	}
 	*/
 	unset($tab_moy_gen);
+	unset($tab_moy);
 	//unset($tab_moy_cat_classe);
 	for($loop=$periode1;$loop<=$periode2;$loop++) {
 		$periode_num=$loop;
 		include "../lib/calcul_moy_gen.inc.php";
 		$tab_moy_gen[$loop]=$moy_generale_classe;
 		//$tab_moy_cat_classe
-	}
+
+		$tab_moy['periodes'][$periode_num]=array();
+		$tab_moy['periodes'][$periode_num]['tab_login_indice']=$tab_login_indice;         // [$login_eleve]
+		$tab_moy['periodes'][$periode_num]['moy_gen_eleve']=$moy_gen_eleve;               // [$i]
+		$tab_moy['periodes'][$periode_num]['moy_gen_eleve1']=$moy_gen_eleve1;             // [$i]
+		//$tab_moy['periodes'][$periode_num]['moy_gen_classe1']=$moy_gen_classe1;           // [$i]
+		$tab_moy['periodes'][$periode_num]['moy_generale_classe']=$moy_generale_classe;
+		$tab_moy['periodes'][$periode_num]['moy_generale_classe1']=$moy_generale_classe1;
+		$tab_moy['periodes'][$periode_num]['moy_max_classe']=$moy_max_classe;
+		$tab_moy['periodes'][$periode_num]['moy_min_classe']=$moy_min_classe;
 	
+		// Il faudrait récupérer/stocker les catégories?
+		$tab_moy['periodes'][$periode_num]['moy_cat_eleve']=$moy_cat_eleve;               // [$i][$cat]
+		$tab_moy['periodes'][$periode_num]['moy_cat_classe']=$moy_cat_classe;             // [$i][$cat]
+		$tab_moy['periodes'][$periode_num]['moy_cat_min']=$moy_cat_min;                   // [$i][$cat]
+		$tab_moy['periodes'][$periode_num]['moy_cat_max']=$moy_cat_max;                   // [$i][$cat]
+	
+		$tab_moy['periodes'][$periode_num]['quartile1_classe_gen']=$quartile1_classe_gen;
+		$tab_moy['periodes'][$periode_num]['quartile2_classe_gen']=$quartile2_classe_gen;
+		$tab_moy['periodes'][$periode_num]['quartile3_classe_gen']=$quartile3_classe_gen;
+		$tab_moy['periodes'][$periode_num]['quartile4_classe_gen']=$quartile4_classe_gen;
+		$tab_moy['periodes'][$periode_num]['quartile5_classe_gen']=$quartile5_classe_gen;
+		$tab_moy['periodes'][$periode_num]['quartile6_classe_gen']=$quartile6_classe_gen;
+		$tab_moy['periodes'][$periode_num]['place_eleve_classe']=$place_eleve_classe;
+	
+		$tab_moy['periodes'][$periode_num]['current_eleve_login']=$current_eleve_login;   // [$i]
+		//$tab_moy['periodes'][$periode_num]['current_group']=$current_group;
+		if($loop==$periode1) {
+			$tab_moy['current_group']=$current_group;                                     // [$j]
+		}
+		$tab_moy['periodes'][$periode_num]['current_eleve_note']=$current_eleve_note;     // [$j][$i]
+		$tab_moy['periodes'][$periode_num]['current_eleve_statut']=$current_eleve_statut; // [$j][$i]
+		//$tab_moy['periodes'][$periode_num]['current_group']=$current_group;
+		$tab_moy['periodes'][$periode_num]['current_coef']=$current_coef;                 // [$j]
+		$tab_moy['periodes'][$periode_num]['current_classe_matiere_moyenne']=$current_classe_matiere_moyenne; // [$j]
+	
+		$tab_moy['periodes'][$periode_num]['current_coef_eleve']=$current_coef_eleve;     // [$i][$j] ATTENTION
+		$tab_moy['periodes'][$periode_num]['moy_min_classe_grp']=$moy_min_classe_grp;     // [$j]
+		$tab_moy['periodes'][$periode_num]['moy_max_classe_grp']=$moy_max_classe_grp;     // [$j]
+		if(isset($current_eleve_rang)) {
+			// $current_eleve_rang n'est pas renseigné si $affiche_rang='n'
+			$tab_moy['periodes'][$periode_num]['current_eleve_rang']=$current_eleve_rang; // [$j][$i]
+		}
+		$tab_moy['periodes'][$periode_num]['quartile1_grp']=$quartile1_grp;               // [$j]
+		$tab_moy['periodes'][$periode_num]['quartile2_grp']=$quartile2_grp;               // [$j]
+		$tab_moy['periodes'][$periode_num]['quartile3_grp']=$quartile3_grp;               // [$j]
+		$tab_moy['periodes'][$periode_num]['quartile4_grp']=$quartile4_grp;               // [$j]
+		$tab_moy['periodes'][$periode_num]['quartile5_grp']=$quartile5_grp;               // [$j]
+		$tab_moy['periodes'][$periode_num]['quartile6_grp']=$quartile6_grp;               // [$j]
+		$tab_moy['periodes'][$periode_num]['place_eleve_grp']=$place_eleve_grp;           // [$j][$i]
+	
+		$tab_moy['periodes'][$periode_num]['current_group_effectif_avec_note']=$current_group_effectif_avec_note; // [$j]
+	
+		/*
+		// De calcul_moy_gen.inc.php, on récupère en sortie:
+		//     - $moy_gen_eleve[$i]
+		//     - $moy_gen_eleve1[$i] idem avec les coef forcés à 1
+		//     - $moy_gen_classe[$i]
+		//     - $moy_gen_classe1[$i] idem avec les coef forcés à 1
+		//     - $moy_generale_classe
+		//     - $moy_max_classe
+		//     - $moy_min_classe
+		
+		// A VERIFIER, mais s'il n'y a pas de coef spécifique pour un élève, on devrait avoir
+		//             $moy_gen_classe[$i] == $moy_generale_classe
+		// NON: Cela correspond à un mode de calcul qui ne retient que les matières suivies par l'élève pour calculer la moyenne générale
+		//      Le LATIN n'est pas compté dans cette moyenne générale si l'élève ne fait pas latin.
+		//      L'Allemand n'est pas comptabilisé si l'élève ne fait pas allemand
+		// FAIRE LE TOUR DES PAGES POUR VIRER TOUS CES $moy_gen_classe s'il en reste?
+		
+		//     - $moy_cat_classe[$i][$cat]
+		//     - $moy_cat_eleve[$i][$cat]
+		
+		//     - $moy_cat_min[$i][$cat] égale à $moy_min_categorie[$cat]
+		//     - $moy_cat_max[$i][$cat] égale à $moy_max_categorie[$cat]
+		
+		// Là le positionnement au niveau moyenne générale:
+		//     - $quartile1_classe_gen
+		//       à
+		//     - $quartile6_classe_gen
+		//     - $place_eleve_classe[$i]
+		
+		// On a récupéré en intermédiaire les
+		//     - $current_eleve_login[$i]
+		//     - $current_group[$j]
+		//     - $current_eleve_note[$j][$i]
+		//     - $current_eleve_statut[$j][$i]
+		//     - $current_coef[$j] (qui peut être différent du $coef_eleve pour une matière spécifique)
+		//     - $categories -> id
+		//     - $current_classe_matiere_moyenne[$j] (moyenne de la classe dans la matière)
+		
+		// AJOUTé:
+		//     - $current_coef_eleve[$i][$j]
+		//     - $moy_min_classe_grp[$j]
+		//     - $moy_max_classe_grp[$j]
+		//     - $current_eleve_rang[$j][$i] sous réserve que $affiche_rang=='y'
+		//     - $quartile1_grp[$j] à $quartile6_grp[$j]
+		//     - $place_eleve_grp[$j][$i]
+		//     - $current_group_effectif_avec_note[$j] pour le nombre de "vraies" moyennes pour le rang (pas disp, abs,...)
+		//     - $tab_login_indice[LOGIN_ELEVE]=$i
+		
+		//     $categories[] = $row["id"];
+		//     $tab_noms_categories[$row["id"]]=$row["nom_complet"];
+		//     $tab_id_categories[$row["nom_complet"]]=$row["id"];
+		
+		*/
+
+	}
+
+	$tab_moy['categories']['id']=$categories;
+	$tab_moy['categories']['nom_from_id']=$tab_noms_categories;
+	$tab_moy['categories']['id_from_nom']=$tab_id_categories;
+	
+	
+	$sql="SELECT DISTINCT e.*
+	FROM eleves e, j_eleves_classes c 
+	WHERE (
+	c.id_classe='$id_classe' AND 
+	e.login = c.login
+	) ORDER BY e.nom,e.prenom;";
+	$res_ele= mysql_query($sql);
+	if(mysql_num_rows($res_ele)>0) {
+		while($lig_ele=mysql_fetch_object($res_ele)) {
+			$tab_moy['eleves'][]=$lig_ele->login;
+			/*
+			$tab_moy['ele'][$lig_ele->login]=array();
+			$tab_moy['ele'][$lig_ele->login]['nom']=$lig_ele->nom;
+			$tab_moy['ele'][$lig_ele->login]['prenom']=$lig_ele->prenom;
+			$tab_moy['ele'][$lig_ele->login]['sexe']=$lig_ele->sexe;
+			$tab_moy['ele'][$lig_ele->login]['naissance']=$lig_ele->naissance;
+			$tab_moy['ele'][$lig_ele->login]['elenoet']=$lig_ele->elenoet;
+			*/
+		}
+	}
+
 	$display_moy_gen=sql_query1("SELECT display_moy_gen FROM classes WHERE id='".$id_classe."'");
 
 
-	bulletin_classe($nombre_eleves,$periode1,$periode2,$nom_periode,$gepiYear,$id_classe,$nb_coef_superieurs_a_zero,$affiche_categories);
+	bulletin_classe($tab_moy, $nombre_eleves,$periode1,$periode2,$nom_periode,$gepiYear,$id_classe,$nb_coef_superieurs_a_zero,$affiche_categories);
 
 	// Formulaire de saisie
 	echo "<form enctype=\"multipart/form-data\" action=\"".$_SERVER['PHP_SELF']."\" method=\"post\">\n";
