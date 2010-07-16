92186ce0ca62db2b5598c32ab3efb7469984c297
jjacquard <jjacquard@e073496e-d129-0410-8f7b-841bfd467322>
2010-07-16 20:41:54 +0000

    ajout de l'actiovation facile des logs propel
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@4788 e073496e-d129-0410-8f7b-841bfd467322

--- lib/initialisationsPropel.inc.php
+++ lib/initialisationsPropel.inc.php
@@ -21,6 +21,8 @@
  * along with GEPI; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+$propel_conf_file_name = 'gepi-conf.php';
+//$propel_conf_file_name = 'gepi-conf_debug.php';
 
 // Pour les scripts situés à la racine de GEPI
 if (isset($niveau_arbo) and ($niveau_arbo == "0")) {
@@ -29,8 +31,7 @@ if (isset($niveau_arbo) and ($niveau_arbo == "0")) {
    //propel objects
    set_include_path("./orm/propel-build/classes" . PATH_SEPARATOR . "./orm" . PATH_SEPARATOR . get_include_path());
    require_once("propel/Propel.php");
-   //Propel::setLogger(null);
-   Propel::init("./orm/propel-build/conf/gepi-conf.php");
+   Propel::init('./orm/propel-build/conf/'.$propel_conf_file_name);
 
 // Pour les scripts situés dans un sous-répertoire à l'intérieur d'une sous-répertoire de GEPI
 } else if (isset($niveau_arbo) and ($niveau_arbo == "2")) {
@@ -39,8 +40,7 @@ if (isset($niveau_arbo) and ($niveau_arbo == "0")) {
    //propel objects
    set_include_path("../../orm/propel-build/classes" . PATH_SEPARATOR . "../../orm/propel" . PATH_SEPARATOR . "../../orm" . PATH_SEPARATOR . get_include_path());
    require_once("propel/Propel.php");
-   //Propel::setLogger(null);
-   Propel::init("../../orm/propel-build/conf/gepi-conf.php");
+   Propel::init('../../orm/propel-build/conf/'.$propel_conf_file_name);
 
 // Pour les scripts situés dans un sous-sous-répertoire à l'intérieur d'une sous-répertoire de GEPI
 } else if (isset($niveau_arbo) and ($niveau_arbo == "3")) {
@@ -49,8 +49,7 @@ if (isset($niveau_arbo) and ($niveau_arbo == "0")) {
    //propel objects
    set_include_path("../../../orm/propel-build/classes" . PATH_SEPARATOR . "../../../orm/propel" . PATH_SEPARATOR . "../../../orm" . PATH_SEPARATOR . get_include_path());
    require_once("propel/Propel.php");
-   //Propel::setLogger(null);
-   Propel::init("../../../orm/propel-build/conf/gepi-conf.php");
+   Propel::init('../../../orm/propel-build/conf/'.$propel_conf_file_name);
 
 // Pour les scripts situés dans le sous-répertoire "public"
 // Ces scripts font appel au fichier /public/secure/connect.inc et non pas /secure/connect.inc
@@ -60,8 +59,7 @@ if (isset($niveau_arbo) and ($niveau_arbo == "0")) {
 	//propel objects
     set_include_path("../orm/propel-build/classes" . PATH_SEPARATOR . "../orm/propel" . PATH_SEPARATOR . "../orm" . PATH_SEPARATOR . get_include_path());
     require_once("propel/Propel.php");
-    //Propel::setLogger(null);
-    Propel::init("../orm/propel-build/conf/gepi-conf.php");
+    Propel::init('../orm/propel-build/conf/'.$propel_conf_file_name);
 
 // Pour les scripts situés dans un sous-répertoire GEPI
 } else {
@@ -70,7 +68,6 @@ if (isset($niveau_arbo) and ($niveau_arbo == "0")) {
 	//propel objects
    set_include_path("../orm/propel-build/classes" . PATH_SEPARATOR . "../orm/propel" . PATH_SEPARATOR . "../orm" . PATH_SEPARATOR . get_include_path());
    include("../orm/propel/Propel.php");
-   //Propel::setLogger(null);
-   Propel::init("../orm/propel-build/conf/gepi-conf.php");
+   Propel::init('../orm/propel-build/conf/'.$propel_conf_file_name);
 }
 ?>
