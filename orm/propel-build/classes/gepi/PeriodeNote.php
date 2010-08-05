0e62373dc8d0a2842927c76d1cdefa37e35ca9ac
jjacquard <jjacquard@e073496e-d129-0410-8f7b-841bfd467322>
2010-08-05 10:12:05 +0000

    Correction d'un bug sur les timezone
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@5024 e073496e-d129-0410-8f7b-841bfd467322

--- orm/propel-build/classes/gepi/PeriodeNote.php
+++ orm/propel-build/classes/gepi/PeriodeNote.php
@@ -41,6 +41,7 @@ class PeriodeNote extends BasePeriodeNote {
 				if ($edt_periode != null) {
 				    $dateDebut = $edt_periode->getJourdebutCalendrier(null);
 				} else {
+				    //c'est la premiere periode
 				    //on va renvoyer par default le 31 aout
 				    $dateDebut = new DateTime('now');
 				    $dateDebut->setDate($dateDebut->format('Y'), 8, 31);
@@ -57,6 +58,14 @@ class PeriodeNote extends BasePeriodeNote {
 				if ($periode_prec != null) {
 				    $dateDebut = $periode_prec->getDateFin(null);
 				}
+				//on prend le lendemain
+				if ($dateDebut !== null) {
+				    $dateDebut->modify("+24 hours");
+				}
+			    }
+			    if ($dateDebut !== null) {
+				//on commence la periode a 00:00
+				$dateDebut->setTime(0,0,0);
 			    }
 			    $this->dateDebut = $dateDebut;
 		    }
@@ -97,4 +106,30 @@ class PeriodeNote extends BasePeriodeNote {
 		return $a->getNumPeriode() - $b->getNumPeriode();
 	}
 
+	/**
+	 * Get the [optionally formatted] temporal [date_fin] column value.
+	 * date de verrouillage de la periode
+	 *
+	 * @param      string $format The date/time format string (either date()-style or strftime()-style).
+	 *							If format is NULL, then the raw DateTime object will be returned.
+	 * @return     mixed Formatted date/time value as string or DateTime object (if format is NULL), NULL if column is NULL, and 0 if column value is 0000-00-00 00:00:00
+	 * @throws     PropelException - if unable to parse/validate the date/time value.
+	 */
+	public function getDateFin($format = 'Y-m-d H:i:s') {
+	    //on fini la periode a 23:59
+	    $date_fin = parent::getDateFin(null);
+	    if ($date_fin == null) {
+		return null;
+	    } else {
+		$date_fin->setTime(23,59,59);
+		if ($format === null) {
+			// Because propel.useDateTimeClass is TRUE, we return a DateTime object.
+			return $date_fin;
+		} elseif (strpos($format, '%') !== false) {
+			return strftime($format, $date_fin->format('U'));
+		} else {
+			return $date_fin->format($format);
+		}
+	    }
+	}
 } // PeriodeNote
