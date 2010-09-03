9f43809c6c61e6bcb42eb6098b6b3021daec085e
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2010-09-03 10:34:14 +0000

    Correction d'un bug empêchant la suppression de CDT pour les profs dont le login comporte un POINT. Modification de style du tableau. Prise en compte du order_by après submit Suppression.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@5174 e073496e-d129-0410-8f7b-841bfd467322

--- cahier_texte_admin/admin_ct.php
+++ cahier_texte_admin/admin_ct.php
@@ -33,7 +33,7 @@ die();
 } else if ($resultat_session == '0') {
     header("Location: ../logout.php?auto=1");
     die();
-};
+}
 // Check access
 if (!checkAccess()) {
     header("Location: ../logout.php?auto=1");
@@ -43,28 +43,43 @@ if (!checkAccess()) {
 // Pour garder une trace du retour
 $_SESSION["retour"] = 'admin_ct';
 
-// Suppresion d'un ou plusieurs cahiers de texte
+// Suppression d'un ou plusieurs cahiers de texte
 if (isset($_POST['sup_ct'])) {
-  $query = sql_query("SELECT DISTINCT id_groupe, id_login FROM ct_entry ORDER BY id_groupe");
+  //$sql="SELECT DISTINCT id_groupe, id_login FROM ct_entry ORDER BY id_groupe;";
+  $sql="SELECT DISTINCT id_groupe FROM ct_entry ORDER BY id_groupe;";
+  //echo "$sql<br />\n";
+  $query = sql_query($sql);
   $msg = '';
   for ($i=0; ($row=sql_row($query,$i)); $i++) {
       $id_groupe = $row[0];
-      $id_prop = $row[1];
-      $temp = "sup".$id_groupe."_".$id_prop;
+      //$id_prop = $row[1];
+      //$temp = "sup".$id_groupe."_".$id_prop;
+      $temp = "sup".$id_groupe;
+      //echo "\$temp=$temp<br />\n";
       if (isset($_POST[$temp])) {
+         $id_prop=$_POST[$temp];
+
          $error = 'no';
-         $appel_ct = sql_query("SELECT id_ct  FROM ct_entry WHERE (id_groupe='".$id_groupe."' and id_login = '".$id_prop."')");
+         $sql="SELECT id_ct  FROM ct_entry WHERE (id_groupe='".$id_groupe."' and id_login = '".$id_prop."');";
+         //echo "$sql<br />\n";
+         $appel_ct=sql_query($sql);
          if (($appel_ct) and (sql_count($appel_ct)!=0)) {
            for ($k=0; ($row2 = sql_row($appel_ct,$k)); $k++) {
              $id_ctexte = $row2[0];
-             $appel_doc = sql_query("select emplacement from ct_documents where id_ct='".$id_ctexte."'");
+             $sql="select emplacement from ct_documents where id_ct='".$id_ctexte."';";
+             //echo "$sql<br />\n";
+             $appel_doc = sql_query($sql);
              for ($j=0; ($row3 = sql_row($appel_doc,$j)); $j++) {
                 $empl = $row3[0];
                 if ($empl != -1) $del = @unlink($empl);
              }
-             $del_doc = sql_query("delete from ct_documents where id_ct='".$id_ctexte."'");
+             $sql="delete from ct_documents where id_ct='".$id_ctexte."';";
+             //echo "$sql<br />\n";
+             $del_doc = sql_query($sql);
              if (!($del_doc)) $error = 'yes';
-             $del_ct = sql_query("delete from ct_entry where id_ct = '".$id_ctexte."'");
+             $sql="delete from ct_entry where id_ct = '".$id_ctexte."';";
+             //echo "$sql<br />\n";
+             $del_ct = sql_query($sql);
              if (!($del_ct)) $error = 'yes';
            }
            if ($error == 'no') {
@@ -78,12 +93,18 @@ if (isset($_POST['sup_ct'])) {
       }
 
    }
-  $query = sql_query("SELECT DISTINCT id_groupe, id_login FROM ct_devoirs_entry ORDER BY id_groupe");
+  //$sql="SELECT DISTINCT id_groupe, id_login FROM ct_devoirs_entry ORDER BY id_groupe;";
+  $sql="SELECT DISTINCT id_groupe FROM ct_devoirs_entry ORDER BY id_groupe;";
+  //echo "$sql<br />\n";
+  $query = sql_query($sql);
   for ($i=0; ($row=sql_row($query,$i)); $i++) {
       $id_groupe = $row[0];
-      $id_prop = $row[1];
-      $temp = "sup".$id_groupe."_".$id_prop;
+      //$id_prop = $row[1];
+      //$temp = "sup".$id_groupe."_".$id_prop;
+      $temp = "sup".$id_groupe;
       if (isset($_POST[$temp])) {
+         $id_prop=$_POST[$temp];
+
          $error = 'no';
          $del_ct_devoirs = sql_query("delete  FROM ct_devoirs_entry WHERE (id_groupe='".$id_groupe."' and id_login = '".$id_prop."')");
          if (!($del_ct_devoirs)) $error = 'yes';
@@ -97,12 +118,17 @@ if (isset($_POST['sup_ct'])) {
       }
 
    }
-  $query = sql_query("SELECT DISTINCT id_groupe, id_login FROM ct_private_entry ORDER BY id_groupe");
+  //$sql="SELECT DISTINCT id_groupe, id_login FROM ct_private_entry ORDER BY id_groupe;";
+  $sql="SELECT DISTINCT id_groupe FROM ct_private_entry ORDER BY id_groupe;";
+  $query=sql_query($sql);
   for ($i=0; ($row=sql_row($query,$i)); $i++) {
       $id_groupe = $row[0];
-      $id_prop = $row[1];
-      $temp = "sup".$id_groupe."_".$id_prop;
+      //$id_prop = $row[1];
+      //$temp = "sup".$id_groupe."_".$id_prop;
+      $temp = "sup".$id_groupe;
       if (isset($_POST[$temp])) {
+         $id_prop=$_POST[$temp];
+
          $error = 'no';
          $del_ct_devoirs = sql_query("delete  FROM ct_private_entry WHERE (id_groupe='".$id_groupe."' and id_login = '".$id_prop."')");
          if (!($del_ct_devoirs)) $error = 'yes';
@@ -148,20 +174,23 @@ if (isset($_POST['action'])) {
 
 }
 
+//===================================================
 // header
 $titre_page = "Administration des cahiers de texte";
 require_once("../lib/header.inc");
+//===================================================
 
+//debug_var();
 
 // Modification d'un cahier de texte - Etape 1
 if (isset($_GET['action'])) {
-  echo "<p class='bold'><a href=\"admin_ct.php\"><img src='../images/icons/back.png' alt='Retour' class='back_link'/> Retour</a></p>";
+  echo "<p class='bold'><a href=\"admin_ct.php\"><img src='../images/icons/back.png' alt='Retour' class='back_link'/> Retour</a></p>\n";
   $id_groupe = $_GET['id_groupe'];
   $id_prop = $_GET['id_prop'];
   $classes = null;
   $nom_groupe = sql_query1("select name from groupes where id = '".$id_groupe."'");
   if ($nom_groupe == "-1") {
-     $nom_groupe = "<font color='red'>".$id_groupe." : groupe inexistant</font>";
+     $nom_groupe = "<font color='red'>".$id_groupe." : groupe inexistant</font>\n";
   } else {
   	  $get_classes = mysql_query("SELECT c.classe FROM classes c, j_groupes_classes jc WHERE (c.id = jc.id_classe and jc.id_groupe = '" . $id_groupe . "')");
       $nb_classes = mysql_num_rows($get_classes);
@@ -173,22 +202,22 @@ if (isset($_GET['action'])) {
   }
   $sql_prof = sql_query("select nom, prenom from utilisateurs where login = '".$id_prop."'");
   if (!($sql_prof)) {
-     $nom_prof = "<font color='red'>".$id_prop." : utilisateur inexistant</font>";
+     $nom_prof = "<font color='red'>".$id_prop." : utilisateur inexistant</font>\n";
   } else {
      $row_prof=sql_row($sql_prof,0);
      $nom_prof = $row_prof[1]." ".$row_prof[0];
          $test_groupe_prof = sql_query("select login from j_groupes_professeurs WHERE (id_groupe='".$id_groupe."' and login = '".$id_prop."')");
-         if (sql_count($test_groupe_prof) == 0) $nom_prof = "<font color='red'>".$nom_prof." : <br />Ce professeur n'enseigne pas dans ce groupe</font>";
+         if (sql_count($test_groupe_prof) == 0) $nom_prof = "<font color='red'>".$nom_prof." : <br />Ce professeur n'enseigne pas dans ce groupe</font>\n";
   }
 
   if ($_GET['action'] == 'modif_groupe') {
-     echo "<form action=\"admin_ct.php\" name=\"formulaire2\" method=\"post\">";
-     echo "<H2>Cahier de texte - Modification du groupe</h2>";
-     echo "<p>Groupe actuel : <b>".$nom_groupe."</b><br />";
-     echo "Dans la (les) classe(s) de : <b>".$classes."</b><br />";
-     echo "Propriétaire actuel : <b>".$nom_prof."</b></p>";
-     echo "<p>Vous pouvez attribuer à ce cahier de texte un nouveau groupe.</p>";
-     echo "<p>Choisissez la nouvelle classe : </p>";
+     echo "<form action=\"admin_ct.php\" name=\"formulaire2\" method=\"post\">\n";
+     echo "<H2>Cahier de texte - Modification du groupe</h2>\n";
+     echo "<p>Groupe actuel : <b>".$nom_groupe."</b><br />\n";
+     echo "Dans la (les) classe(s) de : <b>".$classes."</b><br />\n";
+     echo "Propriétaire actuel : <b>".$nom_prof."</b></p>\n";
+     echo "<p>Vous pouvez attribuer à ce cahier de texte un nouveau groupe.</p>\n";
+     echo "<p>Choisissez la nouvelle classe : </p>\n";
 
      $sql_groupe = sql_query("select g.id, g.name from groupes g, classes c, j_groupes_classes jc " .
      		"WHERE (".
@@ -197,7 +226,7 @@ if (isset($_GET['action'])) {
 			"order by c.classe");
 
 
-     echo "<select name=\"id_groupe\" size=\"1\">";
+     echo "<select name=\"id_groupe\" size=\"1\">\n";
      for ($i=0; ($row=sql_row($sql_groupe,$i)); $i++) {
         $new_id_groupe = $row[0];
         $nom_groupe = $row[1];
@@ -209,42 +238,42 @@ if (isset($_GET['action'])) {
 	      	$classes .= $current_classe;
 	      	if ($c+1<$nb_classes) $classes .= ", ";
 	      }
-        echo "<option value=\"".$new_id_groupe."\">".$classes." | " . $nom_groupe ."</option>";
+        echo "<option value=\"".$new_id_groupe."\">".$classes." | " . $nom_groupe ."</option>\n";
      }
-     echo "</select>";
-     echo "<input type=\"hidden\" name=\"id_prop\" value=\"".$id_prop."\" />";
-     echo "<input type=\"hidden\" name=\"id_former_group\" value=\"".$id_groupe."\" />";
-     echo "<input type=\"hidden\" name=\"action\" value=\"change_groupe\" />";
-     echo "<br /><input type=\"submit\" value=\"Enregistrer\" />";
-     echo "</form>";
+     echo "</select>\n";
+     echo "<input type=\"hidden\" name=\"id_prop\" value=\"".$id_prop."\" />\n";
+     echo "<input type=\"hidden\" name=\"id_former_group\" value=\"".$id_groupe."\" />\n";
+     echo "<input type=\"hidden\" name=\"action\" value=\"change_groupe\" />\n";
+     echo "<br /><input type=\"submit\" value=\"Enregistrer\" />\n";
+     echo "</form>\n";
 
   }
 
   if ($_GET['action'] == 'modif_prop') {
 
-     echo "<form action=\"admin_ct.php\" name=\"formulaire2\" method=\"post\">";
-     echo "<H2>Cahier de texte - Modification du propriétaire</h2>";
-     echo "<p>Groupe actuel : <b>".$nom_groupe."</b><br />";
-     echo "Classe(s) de : <b>".$classes."</b><br />";
-     echo "Propriétaire actuel : <b>".$nom_prof."</b></p>";
-     echo "<p>Vous pouvez attribuer à ce cahier de texte un nouveau propriétaire.</p>";
-     echo "<p>Choisissez le nouveau propriétaire : </p>";
+     echo "<form action=\"admin_ct.php\" name=\"formulaire2\" method=\"post\">\n";
+     echo "<H2>Cahier de texte - Modification du propriétaire</h2>\n";
+     echo "<p>Groupe actuel : <b>".$nom_groupe."</b><br />\n";
+     echo "Classe(s) de : <b>".$classes."</b><br />\n";
+     echo "Propriétaire actuel : <b>".$nom_prof."</b></p>\n";
+     echo "<p>Vous pouvez attribuer à ce cahier de texte un nouveau propriétaire.</p>\n";
+     echo "<p>Choisissez le nouveau propriétaire : </p>\n";
      $sql_matiere = sql_query("select DISTINCT u.login, u.nom, u.prenom from utilisateurs u, matieres m, j_groupes_professeurs j where " .
      		"(u.login=j.login and " .
      		"j.id_groupe='".$id_groupe."'" .
 			") order by 'u.nom, u.prenom'");
-     echo "<select name=\"id_prop\" size=\"1\">";
+     echo "<select name=\"id_prop\" size=\"1\">\n";
      for ($i=0; ($row=sql_row($sql_matiere,$i)); $i++) {
         $id_prop = $row[0];
         $nom_prop = $row[1];
         $prenom_prop = $row[2];
-        echo "<option value=\"".$id_prop."\">".$nom_prop." ".$prenom_prop."</option>";
+        echo "<option value=\"".$id_prop."\">".$nom_prop." ".$prenom_prop."</option>\n";
      }
-     echo "</select>";
-     echo "<input type=\"hidden\" name=\"id_groupe\" value=\"".$id_groupe."\" />";
-     echo "<input type=\"hidden\" name=\"action\" value=\"change_prop\" />";
-     echo "<br /><input type=\"submit\" value=\"Enregistrer\" />";
-     echo "</form>";
+     echo "</select>\n";
+     echo "<input type=\"hidden\" name=\"id_groupe\" value=\"".$id_groupe."\" />\n";
+     echo "<input type=\"hidden\" name=\"action\" value=\"change_prop\" />\n";
+     echo "<br /><input type=\"submit\" value=\"Enregistrer\" />\n";
+     echo "</form>\n";
 
   }
 }
@@ -260,21 +289,25 @@ if (!(isset($_GET['action']))) {
 
 
   <form action="admin_ct.php" name="formulaire1" method="post">
-  <table border="1"><tr valign='center' align='center'>
-  <td><b><a href='admin_ct.php?order_by=jc.id_classe,jm.id_matiere'>Classe(s)</a></b></td>
-  <td><b><a href='admin_ct.php?order_by=jm.id_matiere,jc.id_classe'>Groupe</a></b></td>
-  <td><b><a href='admin_ct.php?order_by=ct.id_login,jc.id_classe,jm.id_matiere'>Propriétaire</a></b></td>
-  <td><b>Nombre<br />de notices</b></td>
-  <td><b>Nombre<br />de notices<br />"devoirs"</b></td>
-  <td><b>Action</b></td><td><b><input type="submit" name="sup_ct" value="Suppression" onclick="return confirmlink(this, 'La suppression d\'un cahier de texte est définitive. Les notices ainsi que les documents joints seront supprimés. Etes-vous sûr de vouloir continuer ?', 'Confirmation de la suppression')" /></b></td></tr>
+  <table border="1" class='boireaus' summary='Administration des CDT'><tr valign='center' align='center'>
+  <th><b><a href='admin_ct.php?order_by=jc.id_classe,jm.id_matiere'>Classe(s)</a></b></th>
+  <th><b><a href='admin_ct.php?order_by=jm.id_matiere,jc.id_classe'>Groupe</a></b></th>
+  <th><b><a href='admin_ct.php?order_by=ct.id_login,jc.id_classe,jm.id_matiere'>Propriétaire</a></b></th>
+  <th><b>Nombre<br />de notices</b></th>
+  <th><b>Nombre<br />de notices<br />"devoirs"</b></th>
+  <th><b>Action</b></th><th><b><input type="submit" name="sup_ct" value="Suppression" onclick="return confirmlink(this, 'La suppression d\'un cahier de texte est définitive. Les notices ainsi que les documents joints seront supprimés. Etes-vous sûr de vouloir continuer ?', 'Confirmation de la suppression')" /></b></th></tr>
 
   <?php
+  /*
   if (!isset($_GET['order_by'])) {
      $order_by = "jc.id_classe,jm.id_matiere";
   } else {
      $order_by = $_GET['order_by'];
   }
+  */
+  $order_by=isset($_GET['order_by']) ? $_GET['order_by'] : (isset($_POST['order_by']) ? $_POST['order_by'] : "jc.id_classe,jm.id_matiere");
 
+  $alt=1;
   $query = sql_query("SELECT DISTINCT ct.id_groupe, ct.id_login FROM ct_entry ct, j_groupes_classes jc, j_groupes_matieres jm WHERE (jc.id_groupe = ct.id_groupe AND jm.id_groupe = ct.id_groupe) ORDER BY ".$order_by);
   for ($i=0; ($row=sql_row($query,$i)); $i++) {
       $id_groupe = $row[0];
@@ -290,15 +323,15 @@ if (!(isset($_GET['action']))) {
       	if ($c+1<$nb_classes) $classes .= ", ";
       }
 
-      if ($nom_groupe == "-1") $nom_groupe = "<font color='red'>Groupe inexistant</font>";
+      if ($nom_groupe == "-1") $nom_groupe = "<font color='red'>Groupe inexistant</font>\n";
       $sql_prof = sql_query("select nom, prenom from utilisateurs where login = '".$id_prop."'");
       if (!($sql_prof)) {
-         $nom_prof = "<font color='red'>".$id_prop." : utilisateur inexistant</font>";
+         $nom_prof = "<font color='red'>".$id_prop." : utilisateur inexistant</font>\n";
       } else {
          $row_prof=sql_row($sql_prof,0);
          $nom_prof = $row_prof[1]." ".$row_prof[0];
          $test_groupe_prof = sql_query("select login from j_groupes_professeurs WHERE (id_groupe='".$id_groupe."' and login = '".$id_prop."')");
-         if (sql_count($test_groupe_prof) == 0) $nom_prof = "<font color='red'>".$nom_prof." : <br />Ce professeur n'enseigne pas dans ce groupe</font>";
+         if (sql_count($test_groupe_prof) == 0) $nom_prof = "<font color='red'>".$nom_prof." : <br />Ce professeur n'enseigne pas dans ce groupe</font>\n";
       }
       // Nombre de notices de chaque utilisateurs
       $nb_ct = sql_count(sql_query("select 1=1 FROM ct_entry WHERE (id_groupe='".$id_groupe."' and id_login='".$id_prop."') "));
@@ -307,18 +340,22 @@ if (!(isset($_GET['action']))) {
       $nb_ct_devoirs = sql_count(sql_query("select 1=1 FROM ct_devoirs_entry WHERE (id_groupe='".$id_groupe."' and id_login='".$id_prop."') "));
 
       // Affichage des lignes
-      echo "<tr><td>".$classes."</td>";
-      echo "<td><a href='admin_ct.php?id_groupe=".$id_groupe."&id_prop=".$id_prop."&action=modif_groupe' title='modifier la matière'>".$nom_groupe."</a></td>";
-      echo "<td><a href='admin_ct.php?id_groupe=".$id_groupe."&id_prop=".$id_prop."&action=modif_prop' title='modifier le propriétaire'>".$nom_prof."</a></td>";
-      echo "<td>".$nb_ct."</td>";
-      echo "<td>".$nb_ct_devoirs."</td>";
-      //echo "<td><a href='../public/index.php?id_groupe=".$id_groupe."' target='_blank'>Voir</a></td>";
-      echo "<td><a href='../cahier_texte/see_all.php?id_groupe=".$id_groupe."' target='_blank'>Voir</a></td>";
-      echo "<td><center><input type=\"checkbox\" name=\"sup".$id_groupe."_".$id_prop."\" /></center></td>";
-      echo "</tr>";
+      $alt=$alt*(-1);
+      echo "<tr class='lig$alt white_hover'><td>".$classes."</td>\n";
+      echo "<td><a href='admin_ct.php?id_groupe=".$id_groupe."&id_prop=".$id_prop."&action=modif_groupe' title='modifier la matière'>".$nom_groupe."</a></td>\n";
+      echo "<td><a href='admin_ct.php?id_groupe=".$id_groupe."&id_prop=".$id_prop."&action=modif_prop' title='modifier le propriétaire'>".$nom_prof."</a></td>\n";
+      echo "<td>".$nb_ct."</td>\n";
+      echo "<td>".$nb_ct_devoirs."</td>\n";
+      //echo "<td><a href='../public/index.php?id_groupe=".$id_groupe."' target='_blank'>Voir</a></td>\n";
+      echo "<td><a href='../cahier_texte/see_all.php?id_groupe=".$id_groupe."' target='_blank'>Voir</a></td>\n";
+      //echo "<td><center><input type=\"checkbox\" name=\"sup".$id_groupe."_".$id_prop."\" /></center></td>\n";
+      echo "<td><center><input type=\"checkbox\" name=\"sup".$id_groupe."\" value=\"$id_prop\" /></center></td>\n";
+      echo "</tr>\n";
 
   }
-  echo "</table></form>";
+  echo "</table>\n";
+  echo "<input type='hidden' name='order_by' value='$order_by' />\n";
+  echo "</form>\n";
 }
 require ("../lib/footer.inc.php");
 ?>
