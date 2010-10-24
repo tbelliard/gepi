c65477714d95d8b230f428e7bff458cee9651388
jjacquard <jjacquard@e073496e-d129-0410-8f7b-841bfd467322>
2010-10-24 20:15:23 +0000

    Ajout de méthode pour savoir si l'établissement est ouvert
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@5749 e073496e-d129-0410-8f7b-841bfd467322

---
+++ orm/helpers/EdtHelper.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+ * @version $Id$
+ *
+ * Copyright 2010 Josselin Jacquard
+ *
+ * This file and the mod_abs2 module is distributed under GPL version 3, or
+ * (at your option) any later version.
+ *
+ * This file is part of GEPI.
+ *
+ * GEPI is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GEPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GEPI; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/**
+ * Classe de helpers sur les edt
+ */
+class EdtHelper {
+
+   public static $semaine_declaration = array("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi");
+
+ /**
+   * Renvoi vrai ou faux selon que l'établissement est ouvert à date et l'heure indiquée
+   *
+   * @param      mixed $dt
+   * @return boolean
+   */
+    public static function isEtablissementOuvert($dt){
+            $jour_semaine = EdtHelper::$semaine_declaration[$dt->format("w")];
+	    $horaire_tab = EdtHorairesEtablissementPeer::retrieveAllEdtHorairesEtablissementArrayCopy();
+            $horaire = null;
+            if (isset($horaire_tab[$jour_semaine])) {
+                $horaire = $horaire_tab[$jour_semaine];
+            } else {
+                return false;
+            }
+            if ($dt->format('Hi') >= $horaire->getFermetureHoraireEtablissement('Hi')
+                    ||	$dt->format('Hi') < $horaire->getOuvertureHoraireEtablissement('Hi')) {
+                //etab fermé
+                return false;
+            }
+
+            //est-ce une période de calendrier ouverte
+            $edt_periode_courante = EdtCalendrierPeriodePeer::retrieveEdtCalendrierPeriodeActuelle($dt);
+            if ($edt_periode_courante != null
+                    && ($edt_periode_courante->getEtabfermeCalendrier() == 0 || $edt_periode_courante->getEtabvacancesCalendrier() == 1)) {
+                //etab fermé
+                return false;
+            }
+            
+            return true;
+    }
+
+
+  /**
+   * Renvoi vrai ou faux selon que l'établissement est ouvert à date (jour) indiquée
+   *
+   * @param      dateTime $dt
+   * @return boolean false/true
+   */
+    public static function isJourneeOuverte($dt){
+            $jour_semaine = EdtHelper::$semaine_declaration[$dt->format("w")];
+	    $horaire_tab = EdtHorairesEtablissementPeer::retrieveAllEdtHorairesEtablissementArrayCopy();
+            if (!isset($horaire_tab[$jour_semaine])) {
+                //etab fermé
+                return false;
+            }
+
+            //est-ce une période ouverte
+            $edt_periode_courante = EdtCalendrierPeriodePeer::retrieveEdtCalendrierPeriodeActuelle($dt);
+            if ($edt_periode_courante != null
+                    && ($edt_periode_courante->getEtabfermeCalendrier() == 0 || $edt_periode_courante->getEtabvacancesCalendrier() == 1)) {
+                //etab fermé
+                return false;
+            }
+
+            return true;
+    }
+}
+?>
--- orm/propel-build/classes/gepi/EdtHorairesEtablissementPeer.php
+++ orm/propel-build/classes/gepi/EdtHorairesEtablissementPeer.php
@@ -15,4 +15,32 @@
  */
 class EdtHorairesEtablissementPeer extends BaseEdtHorairesEtablissementPeer {
 
+  private static $_all_horaires;
+
+  private static $_all_horaires_array_copy;
+
+  /**
+   * Renvoie la liste des creneaux de la journee
+   *
+   * @return PropelObjectCollection EdtCreneau
+   */
+    public static function retrieveAllEdtHorairesEtablissement(){
+	    if (self::$_all_horaires == null) {
+		self::$_all_horaires = EdtHorairesEtablissementQuery::create()->find();
+	    }
+	    return clone self::$_all_horaires;
+    }
+
+  /**
+   * Renvoie la liste des creneaux de la journee
+   *
+   * @return array EdtCreneau
+   */
+    public static function retrieveAllEdtHorairesEtablissementArrayCopy(){
+	    if (self::$_all_horaires_array_copy == null) {
+		self::$_all_horaires_array_copy = EdtHorairesEtablissementPeer::retrieveAllEdtHorairesEtablissement()->getArrayCopy('JourHoraireEtablissement');
+	    }
+	    return self::$_all_horaires_array_copy;
+    }
+
 } // EdtHorairesEtablissementPeer
