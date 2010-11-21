30f76dbd02cbe30f290e69bd62f64839cd6ed329
dblanqui <dblanqui@e073496e-d129-0410-8f7b-841bfd467322>
2010-11-21 13:08:49 +0000

    correctif sur affichage nbre absences pour abs2 dans bulletins et bulletins simplifiés
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@5930 e073496e-d129-0410-8f7b-841bfd467322

--- bulletin/bull_index.php
+++ bulletin/bull_index.php
@@ -2565,9 +2565,9 @@ else {
 					    require_once("../lib/initialisationsPropel.inc.php");
 					    $eleve = EleveQuery::create()->findOneByLogin($current_eleve_login[$i]);
 					    if ($eleve != null) {
-						$current_eleve_absences = $eleve->getDemiJourneesAbsenceParPeriode($periode_num)->count();
-						$current_eleve_nj = $eleve->getDemiJourneesNonJustifieesAbsenceParPeriode($periode_num)->count();
-						$current_eleve_retards = $eleve->getRetardsParPeriode($periode_num)->count();
+						$current_eleve_absences = strval($eleve->getDemiJourneesAbsenceParPeriode($periode_num)->count());
+						$current_eleve_nj = strval($eleve->getDemiJourneesNonJustifieesAbsenceParPeriode($periode_num)->count());
+						$current_eleve_retards = strval($eleve->getRetardsParPeriode($periode_num)->count());
 						$current_eleve_absences_query = mysql_query("SELECT * FROM absences WHERE (login='$current_eleve_login' AND periode='$periode_num')");
 						$current_eleve_appreciation_absences = @mysql_result($current_eleve_absences_query, 0, "appreciation");
 					    }
