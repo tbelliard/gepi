9d8f7d1b253152ef0db8dcaf0d349358160f79ee
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2011-01-11 16:40:51 +0000

    Correctif sur le droit par défaut Autre.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@6331 e073496e-d129-0410-8f7b-841bfd467322

--- mod_trombinoscopes/trombino_pdf.php
+++ mod_trombinoscopes/trombino_pdf.php
@@ -2,7 +2,7 @@
 /*
 * $Id$
 *
-* Copyright 2001, 2006 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun, Christian Chapel
+* Copyright 2001, 2011 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun, Christian Chapel
 *
 * This file is part of GEPI.
 *
@@ -50,7 +50,7 @@ if(mysql_num_rows($test)==0) {
 	eleve='F',
 	responsable='F',
 	secours='F',
-	autre='F',
+	autre='V',
 	description='Trombinoscopes PDF',
 	statut='';";
 	$insert=mysql_query($sql);
