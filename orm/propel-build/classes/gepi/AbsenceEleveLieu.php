39ce50d1ddd2b889f39062159c4e95aa31212773
jjacquard <jjacquard@e073496e-d129-0410-8f7b-841bfd467322>
2011-01-15 19:25:17 +0000

    Ajout des fichier de modelisation du lieu pour les absences
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@6347 e073496e-d129-0410-8f7b-841bfd467322

---
+++ orm/propel-build/classes/gepi/AbsenceEleveLieu.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+/**
+ * Skeleton subclass for representing a row from the 'a_lieux' table.
+ *
+ * Lieu pour les types d'absence ou les saisies
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.gepi
+ */
+class AbsenceEleveLieu extends BaseAbsenceEleveLieu {
+
+} // AbsenceEleveLieu
