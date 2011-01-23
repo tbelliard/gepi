c61f99910c03593ae3fa1a339ef71bb047d99b35
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2011-01-23 16:11:00 +0000

    Ajout d'un token.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@6410 e073496e-d129-0410-8f7b-841bfd467322

--- classes/classes_const.php
+++ classes/classes_const.php
@@ -245,8 +245,10 @@ if (isset($is_posted)) {
 		$k++;
 	}
 
+	//debug_var();
+
 	if (($liste_cible != '') and ($autorisation_sup != 'no')) {
-		header("Location: ../lib/confirm_query.php?liste_cible=$liste_cible&liste_cible2=$liste_cible2&liste_cible3=$liste_cible3&action=retire_eleve");
+		header("Location: ../lib/confirm_query.php?liste_cible=$liste_cible&liste_cible2=$liste_cible2&liste_cible3=$liste_cible3&action=retire_eleve".add_token_in_url(false));
 	}
 
 	if ($reg_ok == 'yes') {
