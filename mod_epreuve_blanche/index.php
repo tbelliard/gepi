dd8d56a103aca5bc3440b3559286e0271cdefb60
eabgrall <eabgrall@e073496e-d129-0410-8f7b-841bfd467322>
2011-04-09 16:53:18 +0000

    Prise en compte de la date de sortie des élèves pour la définition d'une épreuve blanche.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@6758 e073496e-d129-0410-8f7b-841bfd467322

--- mod_epreuve_blanche/index.php
+++ mod_epreuve_blanche/index.php
@@ -361,9 +361,11 @@ if(($_SESSION['statut']=='administrateur')||($_SESSION['statut']=='scolarite')) 
 						$sql="SELECT DISTINCT login FROM j_eleves_groupes WHERE id_groupe='$id_groupe[$i]';";
 					}
 					else {
-						$sql="SELECT DISTINCT j.login,e.$type_anonymat FROM j_eleves_groupes j, eleves e WHERE j.id_groupe='$id_groupe[$i]' AND j.login=e.login;";
+						$sql="SELECT DISTINCT j.login,e.$type_anonymat, e.date_sortie FROM j_eleves_groupes j, eleves e WHERE j.id_groupe='$id_groupe[$i]' AND j.login=e.login AND (e.date_sortie=0 OR e.date_sortie=NULL);";
+						echo $sql;
 					}
 					// Il faudra voir comment gérer le cas d'élèves partis en cours d'année... faire choisir la période?
+					// Eric le 9-4-11 ==> utilisation de la date de sortie pour l'élève. Elève présent ==> date_sortie=0 ou null
 					$res=mysql_query($sql);
 					$cpt_ano = 1;
 					while($lig=mysql_fetch_object($res)) {
