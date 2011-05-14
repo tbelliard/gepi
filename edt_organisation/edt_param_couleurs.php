632f539a286ac6cd606f5d6a9d3c9eb1ef39fdf3
adminpaulbert <adminpaulbert@e073496e-d129-0410-8f7b-841bfd467322>
2011-05-14 09:26:25 +0000

    [EDT2] : Amélioration du visuel de la page "Définir les couleurs"
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@6919 e073496e-d129-0410-8f7b-841bfd467322

--- edt_organisation/edt_param_couleurs.php
+++ edt_organisation/edt_param_couleurs.php
@@ -52,8 +52,16 @@ if (!checkAccess()) {
 }*/
 // CSS et js particulier à l'EdT
 $javascript_specifique = "edt_organisation/script/fonctions_edt";
-$style_specifique = "templates/".NameTemplateEDT()."/css/style_edt";
+$style_specifique[] = "templates/".NameTemplateEDT()."/css/style_edt";
 $utilisation_jsdivdrag = "";
+$ua = getenv("HTTP_USER_AGENT");
+if (strstr($ua, "MSIE 6.0")) {
+	$style_specifique[] = "templates/".NameTemplateEDT()."/css/style_ie6_param";
+}
+else if (strstr($ua, "MSIE 7")) {
+	$style_specifique[] = "templates/".NameTemplateEDT()."/css/style_ie7_param";
+}
+$style_specifique[] = "templates/".NameTemplateEDT()."/css/style_param";
 //==============PROTOTYPE===============
 $utilisation_prototype = "ok";
 //============fin PROTOTYPE=============
@@ -68,17 +76,58 @@ require_once("./menu.inc.php");
 <br />
 <!-- la page du corps de l'EdT -->
 
-	<div id="lecorps">
+<div id="lecorps">
     <?php require_once("./menu.inc.new.php"); ?>
-<h3 class="gepi"><?php echo CLICK_ON_COLOR ?></h3>
-<p><?php echo TEXT1_EDT_PARAM_COLORS ?></p>
+	
+	
+	<div id="art-main">
+        <div class="art-sheet">
+            <div class="art-sheet-tl"></div>
+            <div class="art-sheet-tr"></div>
+            <div class="art-sheet-bl"></div>
+            <div class="art-sheet-br"></div>
+            <div class="art-sheet-tc"></div>
+            <div class="art-sheet-bc"></div>
+            <div class="art-sheet-cl"></div>
+            <div class="art-sheet-cr"></div>
+            <div class="art-sheet-cc"></div>
+            <div class="art-sheet-body">
+                <div class="art-nav">
+                	<div class="l"></div>
+                	<div class="r"></div>
+                </div>
+                        <div class="art-layout-cell art-sidebar1">
+                        </div>
+                        <div class="art-layout-cell art-content">
+						
+<!-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+                            <div class="art-post">
+                                <div class="art-post-tl"></div>
+                                <div class="art-post-tr"></div>
+                                <div class="art-post-bl"></div>
+                                <div class="art-post-br"></div>
+                                <div class="art-post-tc"></div>
+                                <div class="art-post-bc"></div>
+                                <div class="art-post-cl"></div>
+                                <div class="art-post-cr"></div>
+                                <div class="art-post-cc"></div>
+                                <div class="art-post-body">
+									<div class="art-post-inner art-article">
+										<div class="art-postmetadataheader">
+											<h2 class="art-postheader">
+												<?php echo CLICK_ON_COLOR ?>
+											</h2>
+										</div>
+										<div class="art-postcontent">
+											<!-- article-content -->
+	<p><?php echo "Pensez à activer l'affichage des couleurs pour qu'elles soient visibles sur les emplois du temps."; ?></p>
 
-<table id="edt_table_couleurs">
-	<thead>
-	<tr><th><?php echo FIELD ?></th><th><?php echo SHORT_NAME ?></th><th><?php echo COLOR ?></th></tr>
-	</thead>
+	<table id="edt_table_couleurs" style="width:100%">
+		<thead>
+		<tr><th><?php echo FIELD ?></th><th><?php echo SHORT_NAME ?></th><th><?php echo COLOR ?></th></tr>
+		</thead>
 
-	<tbody>
+		<tbody>
 
 <?php
 // On affiche la liste des matières
@@ -107,12 +156,29 @@ $nbre_matieres = mysql_num_rows($req_sql);
 
 	}
 ?>
+		</tbody>
 
-	</tbody>
+	</table>
+											
+											<div class="cleared"></div>
+											<!-- /article-content -->
+										</div>
+										<div class="cleared"></div>
+									</div>
+								</div>
+							</div>
 
-</table>
-<br /><br />
+							
+
+
+
+	
+						</div>
+			</div>
+		</div>
 	</div>
+	<br /><br />
+</div>
 <?php
 // inclusion du footer
 require("../lib/footer.inc.php");
