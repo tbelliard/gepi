35f9988e2050033c5545b798df2c0c13df9da93c
adminpaulbert <adminpaulbert@e073496e-d129-0410-8f7b-841bfd467322>
2011-06-25 12:40:17 +0000

    [admin] : intégration de la barre de menu horizontale dans toutes les pages (il manquait celles qui ne sont pas gérées par TBS)
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@7330 e073496e-d129-0410-8f7b-841bfd467322

---
+++ lib/header_barre_menu_admin.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Fichier qui permet de construire la barre de menu pour les admins
+ *
+ * @version $Id: header_barre_menu_admin.php 7168 2011-06-08 19:06:34Z crob $
+ * @copyright 2008-2011
+ */
+// ====== SECURITE =======
+
+if (!$_SESSION["login"]) {
+    header("Location: ../logout.php?auto=2");
+    die();
+}
+include ("header_barre_admin_template.php");
+?>
+<?php if (count($tbs_menu_admin)) : ?>
+<div id="menu_barre">
+	<div class="menu_barre_bottom"></div>
+	<div class="menu_barre_container">
+		<ul class="niveau1">
+			<?php foreach ($tbs_menu_admin as $value) { if ("$value[li]"!="") { ?>
+			<?php echo $value['li']; ?>
+			<?php }} unset($value); ?>
+		</ul>
+	</div>
+</div>
+<?php endif ?>
