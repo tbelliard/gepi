3d3713b33d305ccf469539419e28e3c72221c9f8
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2011-07-13 12:57:47 +0000

    Modification de vocabulaire.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@7414 e073496e-d129-0410-8f7b-841bfd467322

--- accueil_simpl_prof.php
+++ accueil_simpl_prof.php
@@ -152,7 +152,7 @@ echo "<a href=\"./accueil.php?accueil_simpl=n\">Accès au menu d'accueil</a>";
 //echo "<a href='index.php'> Carnet de notes </a> | \n";
 echo " | \n";
 //echo "<a href='./gestion/config_prefs.php'> Paramétrer mes interfaces simplifiées </a>\n";
-echo "<a href='./gestion/config_prefs.php'> Paramétrer mes interfaces simplifiées </a>\n";
+echo "<a href='./gestion/config_prefs.php'> Paramétrer mon interface </a>\n";
 echo "</p>\n";
 echo "</div>\n";
 
