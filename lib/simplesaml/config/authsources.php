f2c428148a608d971f556c8c950b3fbd269f23f9
jjacquard <jjacquard@e073496e-d129-0410-8f7b-841bfd467322>
2011-07-19 18:12:10 +0000

    [saml] configuration des sources d'authentifications spécifique gepi
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@7442 e073496e-d129-0410-8f7b-841bfd467322

---
+++ lib/simplesaml/config/authsources.php
@@ -0,0 +1,29 @@
+<?php
+
+$config = array(
+
+	//authentification admin saml en utilisant un profil administrateur gepi
+	'admin' => array(
+		'gepiauth:LocalDB',
+		'required_statut' => 'administrateur',
+		'name' => array(
+		    'fr' => 'Admin simplesaml',
+		),
+		'description' => array(
+		    'fr' => 'Authentification spéciale pour l\'administrateur simplesaml',
+		),
+	),
+
+
+
+	//authentification gepi
+	'local-gepi-db' => array(
+		'gepiauth:LocalDB',
+		'name' => array(
+		    'fr' => 'Accès normal',
+		),
+		'description' => array(
+		    'fr' => 'Authentification locale sur la base d\'utilisateur gepi',
+		),
+	)
+);
