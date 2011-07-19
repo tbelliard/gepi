18bee49abfd00155ef2ed85ae02cf088544748cd
jjacquard <jjacquard@e073496e-d129-0410-8f7b-841bfd467322>
2011-07-19 18:12:14 +0000

    [saml] Ajout des metadata de fournisseur d'identité (gepi) et de service distant (sacoche)
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@7443 e073496e-d129-0410-8f7b-841bfd467322

---
+++ lib/simplesaml/metadata/saml20-idp-hosted.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * SAML 2.0 IdP configuration for simpleSAMLphp.
+ *
+ * See: https://rnd.feide.no/content/idp-hosted-metadata-reference
+ */
+
+//__DYNAMIC:1__ représente l'entityID du fournisseur d'identité. Il est remplacé par l'url du serveur si c'est spécifié dynamic.
+// si pour une configuration manuelle un autre entityID est précisé, il doit correspondre à l'entityID
+// précisé dans le fichier simplesaml/metadate/saml20-idp-remote.php du fournisseur de service (sacoche)
+$metadata['gepi-idp'] = array(
+	/*
+	 * The hostname of the server (VHOST) that will use this SAML entity.
+	 *
+	 * Can be '__DEFAULT__', to use this entry by default.
+	 */
+	'host' => '__DEFAULT__',
+
+	/* X.509 key and certificate. Relative to the cert directory. */
+	'privatekey' => 'server.pem',
+	'certificate' => 'server.crt',
+
+	/*
+	 * Authentication source to use. Must be one that is configured in
+	 * 'config/authsources.php'.
+	 */
+	'auth' => 'local-gepi-db',
+
+	/* Uncomment the following to use the uri NameFormat on attributes. */
+	'AttributeNameFormat' => 'urn:oasis:names:tc:SAML:2.0:attrname-format:uri',
+);
---
+++ lib/simplesaml/metadata/saml20-sp-remote.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * SAML 2.0 remote SP metadata for simpleSAMLphp.
+ *
+ * See: http://simplesamlphp.org/docs/trunk/simplesamlphp-reference-sp-remote
+ */
+
+/* Pour utiliser une configuration manuelle, décommenter la configuration ci dessous et commenter la configuration automatique en fin de fichier
+//la chaîne nom-arbitraire-SACoche-monserveur est un entityID qui doit être le même que le fichier simplesaml/config/authsources.cfg du fournisseur de service (sacoche)
+
+$metadata['nom-arbitraire-monserveur-sacoche'] = array(
+	'AssertionConsumerService' => 'https://www.mon-serveur-acoche.fr/SACoche/simplesaml/module.php/saml/sp/saml2-acs.php/distant-gepi-saml',
+	'SingleLogoutService' => 'http://www.mon-serveur-acoche.fr/SACoche/simplesaml/module.php/saml/sp/saml2-logout.php/distant-gepi-saml',
+);*/
+
+/* configuration automatique */
+//on va charger l'adresse SACoche configurée en admin gepi si elle est précisée
+$path = dirname(dirname(dirname(dirname(__FILE__))));
+require_once("$path/secure/connect.inc.php");
+// Database connection
+require_once("$path/lib/mysql.inc");
+require_once("$path/lib/settings.inc");
+// Load settings
+if (!loadSettings()) {
+    die("Erreur chargement settings");
+}
+if (getSettingValue('sacocheUrl') != null) {
+	$sacocheUrl = getSettingValue('sacocheUrl');
+	if (substr($sacocheUrl,strlen($sacocheUrl)-1,1) != '/') {$sacocheUrl .= '/';} //on rajout un / a� la fin
+	$firstEntityID = 'sacoche-sp';
+	$firstEntityArray = array();
+	$firstEntityArray['AssertionConsumerService'] = $sacocheUrl.'simplesaml/module.php/saml/sp/saml2-acs.php/distant-gepi-saml';
+	$firstEntityArray['SingleLogoutService'] = $sacocheUrl.'simplesaml/module.php/saml/sp/saml2-logout.php/distant-gepi-saml';
+	$metadata[$firstEntityID]= $firstEntityArray;
+}
+/* fin de configuration automatique */
+
+?>
