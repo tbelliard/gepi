400ac97140ed1a3272f0ee80ed20775f12d7158e
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2011-07-21 11:47:30 +0000

    Nettoyage de la page.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@7470 e073496e-d129-0410-8f7b-841bfd467322

--- bulletin/export_modele_pdf.php
+++ bulletin/export_modele_pdf.php
@@ -2,7 +2,7 @@
 /*
 * $Id$
 *
-* Copyright 2001-2007 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun, Christian Chapel
+* Copyright 2001-2011 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun, Christian Chapel
 *
 * This file is part of GEPI.
 *
@@ -36,36 +36,13 @@ if ($resultat_session == 'c') {
 } else if ($resultat_session == '0') {
 	header("Location: ../logout.php?auto=1");
 	die();
-};
+}
 
 if (!checkAccess()) {
 	header("Location: ../logout.php?auto=1");
 	die();
 }
-/*
-if (empty($_GET['selection']) and empty($_POST['selection'])) {
-	$selection = '';
-}
-else {
-	if (isset($_GET['selection'])) {
-		$selection = $_GET['selection'];
-	}
-	if (isset($_POST['selection'])) {
-		$selection = $_POST['selection'];
-	}
-}
-if (empty($_GET['id_model_bulletin']) and empty($_POST['id_model_bulletin'])) {
-	$modele = '';
-}
-else {
-	if (isset($_GET['id_model_bulletin'])) {
-		$modele = $_GET['id_model_bulletin'];
-	}
-	if (isset($_POST['id_model_bulletin'])) {
-		$modele = $_POST['id_model_bulletin'];
-	}
-}
-*/
+
 //$id_model_bulletin=isset($_POST['id_model_bulletin']) ? $_POST['id_model_bulletin'] : NULL;
 $selection=isset($_POST['selection']) ? $_POST['selection'] : NULL;
 
@@ -120,57 +97,4 @@ if ( $action === 'export' ) {
 	}
 
 	echo $fd;
-	/*
-	//requête des modèle sélectionné
-	if ( !empty($modele[0]) ) {
-		$o=0;
-		$prepa_requete = "";
-		$passage = 'non';
-		while( !empty($modele[$o]) )
-		{
-			if ( !empty($selection[$o]) and $selection[$o] === '1' ) {
-				if($passage === 'non') { $prepa_requete = 'id_model_bulletin = "'.$modele[$o].'"'; $passage = 'oui'; }
-				if($passage != 'non') { $prepa_requete = $prepa_requete.' OR id_model_bulletin = "'.$modele[$o].'" '; }
-			}
-			$o = $o + 1;
-		}
-	}
-
-
-	if ($modele != '') {
-		$requete = 'SELECT * FROM '.$prefix_base.'model_bulletin WHERE ('.$prepa_requete.') ORDER BY nom_model_bulletin ASC';
-		//echo "$requete<br />";
-	}
-
-	// liste le champs de la table model_bulletin
-	$result = mysql_query("SHOW COLUMNS FROM ".$prefix_base."model_bulletin");
-	if (!$result) {
-	echo 'Impossible d\'exécuter la requête : ' . mysql_error();
-	exit;
-	}
-	if (mysql_num_rows($result) > 0) {
-	while ($row = mysql_fetch_assoc($result)) {
-		echo $row['Field'].';';
-		$var[] = $row['Field'];
-	}
-	}
-	echo "\r\n";
-
-$executer = mysql_query($requete) or die('Erreur SQL !'.$requete.'<br>'.mysql_error());
-while ( $donner = mysql_fetch_array( $executer ) )
-{
-	$o = '0';
-	while ( !empty($var[$o]) ) {
-	$var_select = $var[$o];
-		if ( $var_select === 'id_model_bulletin' ) {
-			echo ';';
-		} else {
-				echo $donner[$var_select].';';
-			}
-	$o = $o + 1;
-	}
-	echo "\r\n";
-}
-	*/
-}
 ?>
