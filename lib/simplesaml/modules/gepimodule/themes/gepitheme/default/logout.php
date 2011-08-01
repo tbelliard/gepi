6c12d64b6cb783f07a7602c36931f24beb0d6c2e
mleygnac <mleygnac@e073496e-d129-0410-8f7b-841bfd467322>
2011-08-01 10:15:44 +0000

    Modifications du thème GEPI de simplesaml
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@7550 e073496e-d129-0410-8f7b-841bfd467322

---
+++ lib/simplesaml/modules/gepimodule/themes/gepitheme/default/logout.php
@@ -0,0 +1,23 @@
+<?php
+
+$this->data['header'] = $this->t('{logout:title}');
+
+$this->includeAtTemplateBase('includes/header.php');
+
+
+
+echo('<h2>' . $this->data['header'] . '</h2>');
+echo('<p>' . $this->t('{logout:logged_out_text}') . '</p>');
+
+if($this->getTag($this->data['text']) !== NULL) {
+	$this->data['text'] = $this->t($this->data['text']);
+}
+echo('<p>[ <a href="' . htmlspecialchars($this->data['link']) . '">' .
+	htmlspecialchars($this->data['text']) . '</a> ]</p>');
+
+$this->includeAtTemplateBase('includes/footer.php');
+
+?>
+<script type="text/javascript">
+ document.getElementById("wrap").style.width = "27em";
+</script>
