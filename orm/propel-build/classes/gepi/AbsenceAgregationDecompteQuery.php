58a64a3dea16ad8c192c383530a1466ced7dd577
dblanqui <dblanqui@e073496e-d129-0410-8f7b-841bfd467322>
2011-08-06 16:16:07 +0000

    [orm]ajout de deux methodes pour les requêtes sur la table d'agrégation
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@7584 e073496e-d129-0410-8f7b-841bfd467322

--- orm/propel-build/classes/gepi/AbsenceAgregationDecompteQuery.php
+++ orm/propel-build/classes/gepi/AbsenceAgregationDecompteQuery.php
@@ -14,5 +14,46 @@
  * @package    propel.generator.gepi
  */
 class AbsenceAgregationDecompteQuery extends BaseAbsenceAgregationDecompteQuery {
-
+    
+    /**
+     * Filtre la requete sur les dates de début et de fin. En cas de date nulle, 
+     * le premier jour ou le dernier de l'année scolaire est utilisé
+     * 
+     * @param  DateTime $date_debut, $date_fin Dates de début et de fin de l'extraction des demi journées
+     * @return    AbsenceAgregationDecompteQuery The current query, for fluid interface
+     */
+    public function filterByDateIntervalle($date_debut=Null,  $date_fin=Null) {
+        
+        if (is_null($date_debut) || is_null($date_fin)) {
+            require_once("helpers/EdtHelper.php");
+            if (is_null($date_debut)) {
+                $date_debut = EdtHelper::getPremierJourAnneeScolaire();
+            }
+            if (is_null($date_fin)) {
+                $date_fin = EdtHelper::getDernierJourAnneeScolaire();
+            }
+        }
+        $this->filterByDateDemiJounee($date_debut, Criteria::GREATER_EQUAL)
+             ->filterByDateDemiJounee($date_fin, Criteria::LESS_EQUAL);
+        return $this;
+    }
+    
+    /**
+     * Retourne le nombre de retards (somme des demi journées de la requête)     * 
+     * 
+     * @return    int $nbreRetards Nbre de retards 
+     */
+    public function countRetards() {
+        
+        $absAgregationCol=$this->find();
+        $nbreRetards=0;
+        if ($absAgregationCol->isEmpty()){
+            return $nbreRetards;
+        }else{
+            foreach($absAgregationCol as $demiJournee){
+                $nbreRetards=$nbreRetards+$demiJournee->getNbRetards();
+            }
+            return($nbreRetards);
+        }        
+    }
 } // AbsenceAgregationDecompteQuery
