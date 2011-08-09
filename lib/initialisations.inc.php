a36f1a8a8b32807f9fc177c8d158ea5bd6e43dbb
jjacquard <jjacquard@e073496e-d129-0410-8f7b-841bfd467322>
2011-08-09 12:51:05 +0000

    retrait de code intutile pour la sélection de source multiauth saml
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@7651 e073496e-d129-0410-8f7b-841bfd467322

--- lib/initialisations.inc.php
+++ lib/initialisations.inc.php
@@ -1,182 +1,359 @@
 <?php
-/**
- * Appel des fichiers de configuration
- * 
- * @package Initialisation
- * @subpackage initialisation
- */
 
-/**
- * Niveau de la page dans l'arborescence de GEPI
- *
- * @global int $GLOBALS['niveau_arbo']
- * @name $niveau_arbo
- */
-$GLOBALS['niveau_arbo']=$niveau_arbo;
-
-/**
- * Chemin de la racine de GEPI
- * 
- * initialisée dans secure/connect.inc.php
- * 
- * On la retrouve en tant que global dans share.inc.php
- *
- * @global text $GLOBALS['gepiPath']
- * @name $gepiPath
- */
-$GLOBALS['gepiPath']=$gepiPath;
-
-/**
- * Chemin relatif vers la racine de GEPI
- *
- * @global text $GLOBALS['relatif_gepi']
- * @name $chemin_relatif_gepi
- */
-$GLOBALS['relatif_gepi']='';
-
-
+//======================================
+/*
+$utiliser_mb="y";
+if($utiliser_mb=="y") {
+	mb_internal_encoding("iso-8859-15");
+}
+*/
+//======================================
 
 // Initialisation de variables utilisées si javascript activé
 $tabdiv_infobulle=array();
 $tabid_infobulle=array();
-
-// Initialisation des chemins relatifs
-// $chemin_relatif_gepi2 utilisé avec secure/connect.inc.php
-// $chemin_relatif_gepi pour les autres fichiers
-
-
-$chemin_relatif_gepi = $chemin_relatif_gepi2 = dirname(dirname(__FILE__));
-
-if (isset($niveau_arbo) and ($niveau_arbo == "public")) {
-  $chemin_relatif_gepi2 = './';
-}
-
+	
 $is_lcs_plugin="no";
-
-if (file_exists($chemin_relatif_gepi."secure/config_lcs.inc.php")) {
-    $is_lcs_plugin="yes";
+// Pour les scripts situés à la racine de GEPI
+if (isset($niveau_arbo) and ($niveau_arbo == "0")) {
+	if (file_exists("./secure/config_lcs.inc.php")) {
+		$is_lcs_plugin="yes";
+	}
+}
+// Pour les scripts situés dans un sous-répertoire à l'intérieur d'une sous-répertoire de GEPI
+else if (isset($niveau_arbo) and ($niveau_arbo == "2")) {
+	if (file_exists("../../secure/config_lcs.inc.php")) {
+		$is_lcs_plugin="yes";
+	}
+}
+// Pour les scripts situés dans un sous-sous-répertoire à l'intérieur d'une sous-répertoire de GEPI
+else if (isset($niveau_arbo) and ($niveau_arbo == "3")) {
+	if (file_exists("../../../secure/config_lcs.inc.php")) {
+		$is_lcs_plugin="yes";
+	}
+}
+else if (isset($niveau_arbo) and ($niveau_arbo == "4")) {
+	if (file_exists("../../../../secure/config_lcs.inc.php")) {
+		$is_lcs_plugin="yes";
+	}
+}
+else if (isset($niveau_arbo) and ($niveau_arbo == "5")) {
+	if (file_exists("../../../../../secure/config_lcs.inc.php")) {
+		$is_lcs_plugin="yes";
+	}
+}
+else {
+	if (file_exists("../secure/config_lcs.inc.php")) {
+		$is_lcs_plugin="yes";
+	}
 }
 
 if($is_lcs_plugin=='yes') {
-  /**
-   * authentification lcs
-   */
-  require_once ('/usr/share/lcs/Plugins/Gepi/secure/config_lcs.inc.php');
-  /**
-   * authentification lcs page auth
-   */
-  include LCS_PAGE_AUTH_INC_PHP;
-  /**
-   * authentification lcs page LDAP
-   */
-  include LCS_PAGE_LDAP_INC_PHP;
-  list ($idpers,$login) = isauth();
+	//authentification lcs
+	//require_once ('/usr/share/lcs/Plugins/Gepi/secure/config_lcs.cfg');
+	require_once ('/usr/share/lcs/Plugins/Gepi/secure/config_lcs.inc.php');
+	include LCS_PAGE_AUTH_INC_PHP;
+	include LCS_PAGE_LDAP_INC_PHP;
+	list ($idpers,$login) = isauth();
+	/*
+	//on se reconnecte à la base gepi
+	if (empty($db_nopersist))
+		$db_c = mysql_pconnect($dbHost, $dbUser, $dbPass);
+	else
+		$db_c = mysql_connect($dbHost, $dbUser, $dbPass);
+
+	if (!$db_c || !mysql_select_db ($dbDb))
+	{
+		echo "\n<p>Erreur grave: Echec de la connexion à la base de données";
+		exit;
+	}
+
+	// Ce n'est plus utile. On realise cette operation plus loin lors de l'include connect.inc.php
+	*/
 }
 
 $version = substr(phpversion(), 0, 1);
 if ($version == 4) {
-  $ldap_class = "/lib/LDAPServer.php4.class.php";
-  $session_class = "/lib/Session.php4.class.php";
+  $ldap_class = "lib/LDAPServer.php4.class.php";
+  $session_class = "lib/Session.php4.class.php";
 } else {
-  $ldap_class = "/lib/LDAPServer.class.php";
-  $session_class = "/lib/Session.class.php";
+  $ldap_class = "lib/LDAPServer.class.php";
+  $session_class = "lib/Session.class.php";
 }
 
 // Pour le multisite
 if (isset($_GET["rne"])) {
 	setcookie('RNE', $_GET["rne"], null, '/');
 }
-// Pour le choix de la préférence de source d'authentification pour l'authentification multiauth
-if (isset($_REQUEST["source"])) {
-	setcookie('source', $_REQUEST["source"], null, '/');
-}
 
-/**
- * Données de connexion à la base
- */
-   require_once($chemin_relatif_gepi2."/secure/connect.inc.php");
-/**
- * Connection à la base
- */
-   require_once($chemin_relatif_gepi."/lib/mysql.inc");
- /**
-  * Ajout pour utiliser ou pas les fonctions mb_
-  */
-   require_once($chemin_relatif_gepi."/lib/mb_ou_pas.php");
- /**
-  * Fichier de configuration générale
-  */
-   require_once($chemin_relatif_gepi."/lib/global.inc");
- /**
-  * Filtrage html
-  */
-   require_once($chemin_relatif_gepi."/lib/filtrage_html.inc.php");
+
+// Pour les scripts situés à la racine de GEPI
+if (isset($niveau_arbo) and ($niveau_arbo == "0")) {
+   // Database configuration file
+   require_once("./secure/connect.inc.php");
+   // Database connection
+   require_once("./lib/mysql.inc");
+   // Ajout pour utiliser ou pas les fonctions mb_*
+   require_once("./lib/mb_ou_pas.php");
+   // Global configuration file
+   require_once("./lib/global.inc");
+   // Traitement des données
+   require_once("./lib/filtrage_html.inc.php");
 	if($filtrage_html=="htmlpurifier") {
- /**
-  * Utilisation de HTMLPurifier.standalone pour filtrer les saisies
-  */
-		require_once($chemin_relatif_gepi."/lib/HTMLPurifier.standalone.php");
+		require_once("./lib/HTMLPurifier.standalone.php");
 	}
 	elseif($filtrage_html=="inputfilter") {
- /**
-  * Utilisation de class.inputfilter_clean.php pour filtrer les saisies
-  */
-		require_once($chemin_relatif_gepi."/lib/class.inputfilter_clean.php");
+		require_once("./lib/class.inputfilter_clean.php");
 	}
 
- /**
-  * Traitement des données
-  */
-   require_once($chemin_relatif_gepi."/lib/traitement_data.inc.php");
- /**
-  * Librairies
-  */
-   include $chemin_relatif_gepi."/lib/share.inc.php";
- /**
-  * Fonctions relatives aux groupes
-  */
-    include $chemin_relatif_gepi."/lib/groupes.inc.php";
- /**
-  * classes
-  */
-    include $chemin_relatif_gepi."/lib/classes.inc.php";
- /**
-  * Fonctions de manipulation de la table settings
-  * 
-  * @see loadSettings()
-  */
-   require_once($chemin_relatif_gepi."/lib/settings.inc");
+   require_once("./lib/traitement_data.inc.php");
+   // Libraries
+   include "./lib/share.inc.php";
+   // Fonctions relatives aux groupes
+    include "./lib/groupes.inc.php";
+   // Fonctions relatives aux groupes
+    include "./lib/classes.inc.php";
+   // Settings
+   require_once("./lib/settings.inc");
    // Load settings
    if (!loadSettings()) {
      die("Erreur chargement settings");
    }
-   /**
-    * Fonctions relatives à l'identification via LDAP
-    */
-   require_once($chemin_relatif_gepi.$ldap_class);
-   /**
-    * Fonctions relatives à la session
-    * 
-    * @see class_exists()
-    * @see get_include_path()
-    */
-   require_once($chemin_relatif_gepi.$session_class);
+   // Session related functions
+   require_once("./".$ldap_class);
+   require_once("./".$session_class);
 
-// Modif pour la longueur des logins par $longmax_login du global.inc
-// Si le champ de setting existe alors il faut l'utiliser car il est réglé par la page param_gen.php
-if(isset($gepiSettings['longmax_login'])){
-    $longmax_login = $gepiSettings['longmax_login'];
+// Pour les scripts situés dans un sous-répertoire à l'intérieur d'une sous-répertoire de GEPI
+} else if (isset($niveau_arbo) and ($niveau_arbo == "2")) {
+   // Database configuration file
+   require_once("../../secure/connect.inc.php");
+   // Database connection
+   require_once("../../lib/mysql.inc");
+   // Ajout pour utiliser ou pas les fonctions mb_*
+   require_once("../../lib/mb_ou_pas.php");
+   // Global configuration file
+   require_once("../../lib/global.inc");
+   // Traitement des données
+   require_once("../../lib/filtrage_html.inc.php");
+	if($filtrage_html=="htmlpurifier") {
+		require_once("../../lib/HTMLPurifier.standalone.php");
+	}
+	elseif($filtrage_html=="inputfilter") {
+		require_once("../../lib/class.inputfilter_clean.php");
+	}
+   require_once("../../lib/traitement_data.inc.php");
+   // Libraries
+   include "../../lib/share.inc.php";
+   // Fonctions relatives aux groupes
+   include "../../lib/groupes.inc.php";
+   // Fonctions relatives aux groupes
+   include "../../lib/classes.inc.php";
+   // Settings
+   require_once("../../lib/settings.inc");
+   // Load settings
+   if (!loadSettings()) {
+       die("Erreur chargement settings");
+   }
+   // Session related functions
+   require_once("../../".$ldap_class);
+   require_once("../../".$session_class);
+
+// Pour les scripts situés dans un sous-sous-répertoire à l'intérieur d'une sous-répertoire de GEPI
+} else if (isset($niveau_arbo) and ($niveau_arbo == "3")) {
+   // Database configuration file
+   require_once("../../../secure/connect.inc.php");
+   // Database connection
+   require_once("../../../lib/mysql.inc");
+   // Ajout pour utiliser ou pas les fonctions mb_*
+   require_once("../../../lib/mb_ou_pas.php");
+   // Global configuration file
+   require_once("../../../lib/global.inc");
+   // Traitement des données
+   require_once("../../../lib/filtrage_html.inc.php");
+	if($filtrage_html=="htmlpurifier") {
+		require_once("../../../lib/HTMLPurifier.standalone.php");
+	}
+	elseif($filtrage_html=="inputfilter") {
+		require_once("../../../lib/class.inputfilter_clean.php");
+	}
+   require_once("../../../lib/traitement_data.inc.php");
+   // Libraries
+   include "../../../lib/share.inc.php";
+   // Fonctions relatives aux groupes
+   include "../../../lib/groupes.inc.php";
+   // Fonctions relatives aux groupes
+   include "../../../lib/classes.inc.php";
+   // Settings
+   require_once("../../../lib/settings.inc");
+   // Load settings
+   if (!loadSettings()) {
+       die("Erreur chargement settings");
+   }
+   // Session related functions
+   require_once("../../../".$ldap_class);
+   require_once("../../../".$session_class);
+
+
+
+
+} else if (isset($niveau_arbo) and ($niveau_arbo == "4")) {
+   // Database configuration file
+   require_once("../../../../secure/connect.inc.php");
+   // Database connection
+   require_once("../../../../lib/mysql.inc");
+   // Ajout pour utiliser ou pas les fonctions mb_*
+   require_once("../../../../lib/mb_ou_pas.php");
+   // Global configuration file
+   require_once("../../../../lib/global.inc");
+   // Traitement des données
+   require_once("../../../../lib/filtrage_html.inc.php");
+	if($filtrage_html=="htmlpurifier") {
+		require_once("../../../../lib/HTMLPurifier.standalone.php");
+	}
+	elseif($filtrage_html=="inputfilter") {
+		require_once("../../../../lib/class.inputfilter_clean.php");
+	}
+   require_once("../../../../lib/traitement_data.inc.php");
+   // Libraries
+   include "../../../../lib/share.inc.php";
+   // Fonctions relatives aux groupes
+   include "../../../../lib/groupes.inc.php";
+   // Fonctions relatives aux groupes
+   include "../../../../lib/classes.inc.php";
+   // Settings
+   require_once("../../../../lib/settings.inc");
+   // Load settings
+   if (!loadSettings()) {
+       die("Erreur chargement settings");
+   }
+   // Session related functions
+   require_once("../../../../".$ldap_class);
+   require_once("../../../../".$session_class);
+
+
+} else if (isset($niveau_arbo) and ($niveau_arbo == "5")) {
+   // Database configuration file
+   require_once("../../../../../secure/connect.inc.php");
+   // Database connection
+   require_once("../../../../../lib/mysql.inc");
+   // Ajout pour utiliser ou pas les fonctions mb_*
+   require_once("../../../../../lib/mb_ou_pas.php");
+   // Global configuration file
+   require_once("../../../../../lib/global.inc");
+   // Traitement des données
+   require_once("../../../../../lib/filtrage_html.inc.php");
+	if($filtrage_html=="htmlpurifier") {
+		require_once("../../../../../lib/HTMLPurifier.standalone.php");
+	}
+	elseif($filtrage_html=="inputfilter") {
+		require_once("../../../../../lib/class.inputfilter_clean.php");
+	}
+   require_once("../../../../../lib/traitement_data.inc.php");
+   // Libraries
+   include "../../../../../lib/share.inc.php";
+   // Fonctions relatives aux groupes
+   include "../../../../../lib/groupes.inc.php";
+   // Fonctions relatives aux groupes
+   include "../../../../../lib/classes.inc.php";
+   // Settings
+   require_once("../../../../../lib/settings.inc");
+   // Load settings
+   if (!loadSettings()) {
+       die("Erreur chargement settings");
+   }
+   // Session related functions
+   require_once("../../../../../".$ldap_class);
+   require_once("../../../../../".$session_class);
+
+
+
+// Pour les scripts situés dans le sous-répertoire "public"
+// Ces scripts font appel au fichier /public/secure/connect.inc et non pas /secure/connect.inc
+} else if (isset($niveau_arbo) and ($niveau_arbo == "public")) {
+   // Database configuration file
+    require_once("./secure/connect.inc.php");
+    // Database
+    require_once("../lib/mysql.inc");
+   // Ajout pour utiliser ou pas les fonctions mb_*
+   require_once("../lib/mb_ou_pas.php");
+    // Global configuration file
+    require_once("../lib/global.inc");
+    // Traitement des données
+   require_once("../lib/filtrage_html.inc.php");
+	if($filtrage_html=="htmlpurifier") {
+		require_once("../lib/HTMLPurifier.standalone.php");
+	}
+	elseif($filtrage_html=="inputfilter") {
+		require_once("../lib/class.inputfilter_clean.php");
+	}
+    require_once("../lib/traitement_data.inc.php");
+    // Libraries
+    include "../lib/share.inc.php";
+    // Fonctions relatives aux groupes
+    include "../lib/groupes.inc.php";
+    // Settings
+    require_once("../lib/settings.inc");
+    // Load settings
+    if (!loadSettings()) {
+        die("Erreur chargement settings");
+    }
+   // Session related functions
+   require_once("../".$ldap_class);
+   require_once("../".$session_class);
+
+// Pour les scripts situés dans un sous-répertoire GEPI
+} else {
+   // Database configuration file
+   require_once("../secure/connect.inc.php");
+   // Database connection
+   require_once("../lib/mysql.inc");
+   // Ajout pour utiliser ou pas les fonctions mb_*
+   require_once("../lib/mb_ou_pas.php");
+   // Global configuration file
+   require_once("../lib/global.inc");
+   // Traitement des données
+   require_once("../lib/filtrage_html.inc.php");
+	if($filtrage_html=="htmlpurifier") {
+		require_once("../lib/HTMLPurifier.standalone.php");
+	}
+	elseif($filtrage_html=="inputfilter") {
+		require_once("../lib/class.inputfilter_clean.php");
+	}
+   require_once("../lib/traitement_data.inc.php");
+   // Libraries
+   include "../lib/share.inc.php";
+    // Fonctions relatives aux groupes
+    include "../lib/groupes.inc.php";
+     // Fonctions relatives aux groupes
+    include "../lib/classes.inc.php";
+   // Settings
+   require_once("../lib/settings.inc");
+   // Load settings
+   if (!loadSettings()) {
+       die("Erreur chargement settings");
+   }
+   // Session related functions
+   require_once("../".$ldap_class);
+   require_once("../".$session_class);
 }
+
+	// Modif pour la longueur des logins par $longmax_login du global.inc
+	// Si le champ de setting existe alors il faut l'utiliser car il est réglé par la page param_gen.php
+	if(isset($gepiSettings['longmax_login'])){
+		$longmax_login = $gepiSettings['longmax_login'];
+	}
 
 if (!isset($mode_debug)) {
     $mode_debug = false;
 }
 
 
-// Initialisation de la session Gepi :
+// Initialisaton de la session Gepi :
 if (!isset($prevent_session_init)) {
   $session_gepi = new Session();
 }
+
+//echo 'on est ici';die;
 
 if (!class_exists('Propel')
 	|| !strstr(get_include_path(), '/orm/propel-build/classes')) {
