69e6a69351202ba4bfa45c824cf1acb566ee48ff
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2011-08-10 09:09:52 +0000

    Ajout d'une barre scolarité.
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@7675 e073496e-d129-0410-8f7b-841bfd467322

--- templates/origine/bandeau_template.php
+++ templates/origine/bandeau_template.php
@@ -264,6 +264,20 @@
 </div>
 <?php endif ?>
 
+<!-- menu scolarité -->
+<?php if (count($tbs_menu_scol)) : ?>
+<div id="menu_barre">
+	<div class="menu_barre_bottom"></div>
+	<div class="menu_barre_container">
+		<ul class="niveau1">
+			<?php foreach ($tbs_menu_scol as $value) { if ("$value[li]"!="") { ?>
+			<?php echo $value['li']; ?>
+			<?php }} unset($value); ?>
+		</ul>
+	</div>
+</div>
+<?php endif ?>
+
 
 <!-- fil d'ariane -->
 <?php
