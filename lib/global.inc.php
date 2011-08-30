af983bea65ae59b15d252074b089ca006a170b39
robot <robot@e073496e-d129-0410-8f7b-841bfd467322>
2011-08-30 22:30:04 +0000

    Ajout automatique du numero de revision.
    git-svn-id: http://svn.sylogix.org/gepiphp/branches/release-1.5.5@8062 e073496e-d129-0410-8f7b-841bfd467322

--- lib/global.inc.php
+++ lib/global.inc.php
@@ -175,7 +175,7 @@ $gepiBetaVersion = "";
 // automatique du site web de Gepi. Elle n'est vraiment utile que pour les
 // branches stables, pas pour le trunk, qui correspond de toute façon à la
 // version la plus récente.
-$gepiSvnRev="8058";
+$gepiSvnRev="8061";
 
 
 // Forcer l'utilisation du module de gestion des mise à jour de GEPI
