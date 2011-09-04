32601a7ceda1380b0bb2c27ada4a4726ecacfb56
dblanqui <dblanqui@e073496e-d129-0410-8f7b-841bfd467322>
2011-09-04 15:16:17 +0000

    [abs2]correction orthographe
    git-svn-id: http://svn.sylogix.org/gepiphp/branches/release-1.5.5@8126 e073496e-d129-0410-8f7b-841bfd467322

--- mod_abs2/admin/admin_types_absences.php
+++ mod_abs2/admin/admin_types_absences.php
@@ -151,7 +151,7 @@ echo "</p>";
     <p>
         <span style="font-weight: bold;">Attention :</span> Associer un type à une saisie implique la création d'un traitement de cette saisie.<br />
         Pour éviter une multiplicité des traitements pour une saisie, il peut être intéressant de limiter les statuts pouvant saisir un type.<br />
-        Par exemple si l'on souhaite que seul la vie scolaire crée le traitement absence ou retard on n'affectera pas ces types au statut professeur.        
+        Par exemple si l'on souhaite que seule la vie scolaire crée le traitement absence ou retard on n'affectera pas ces types au statut professeur.        
     </p>
 <?php if ($action == "ajouter" OR $action == "modifier" OR $action == "supprimer_statut") { ?>
 <div style="text-align:center">
