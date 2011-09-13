e60a938a459bf4266dc0b9fb0f045e1c509faf27
dblanqui <dblanqui@e073496e-d129-0410-8f7b-841bfd467322>
2011-09-13 21:25:49 +0000

    [abs2]correction erreur html qui empechait le select de fonctionner sous iE8
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@8213 e073496e-d129-0410-8f7b-841bfd467322

--- mod_abs2/tableau_des_appels.php
+++ mod_abs2/tableau_des_appels.php
@@ -138,7 +138,7 @@ include('menu_bilans.inc.php');
 	}
 ?>
 	</select>
-<br />
+  </p>
 <?php
 $creneau_col = EdtCreneauPeer::retrieveAllEdtCreneauxOrderByTime();
 foreach ($creneau_col as $creneau) {
