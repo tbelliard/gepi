b41aeef12992852a38c3f64a928daec080df6a97
Josselin Jacquard <josselin.jacquard@gmail.com>
2011-10-15 22:05:38 +0200

    [orm] Correction de bug sur AbsenceEleveTraitementQuery et ajout de tests

--- test/testsuite/orm/base/AbsenceEleveTraitementTest.php
+++ test/testsuite/orm/base/AbsenceEleveTraitementTest.php
@@ -57,4 +57,43 @@ class AbsenceEleveTraitementTest extends GepiEmptyTestBase
 		saveSetting('abs2_saisie_par_defaut_sous_responsabilite_etab','n');
 	    
 	}
+
+	public function testQuery()
+	{
+        $florence_eleve = EleveQuery::create()->findOneByLogin('Florence Michu');
+        
+        $traitements = AbsenceEleveTraitementQuery::create()->useJTraitementSaisieEleveQuery()->useAbsenceEleveSaisieQuery()
+                                                    	    ->filterByEleve($florence_eleve)
+                                                    	    ->endUse()->endUse()->find();
+        $this->assertEquals(16,$traitements->count(),'le total des traitements de Florence est 16');
+                                    	    
+        saveSetting('abs2_saisie_par_defaut_sans_manquement','n');
+        $traitements = AbsenceEleveTraitementQuery::create()->filterByManquementObligationPresence(true)
+                                                    	    ->useJTraitementSaisieEleveQuery()->useAbsenceEleveSaisieQuery()
+                                                    	    ->filterByEleve($florence_eleve)
+                                                    	    ->endUse()->endUse()->find();
+        $this->assertEquals(9,$traitements->count());
+        $traitements = AbsenceEleveTraitementQuery::create()->filterByManquementObligationPresence(false)
+                                                    	    ->useJTraitementSaisieEleveQuery()->useAbsenceEleveSaisieQuery()
+                                                    	    ->filterByEleve($florence_eleve)
+                                                    	    ->endUse()->endUse()->find();
+//        foreach ($traitements as $traitement) {
+//            echo "\n".$traitement->getDescription();
+//        }
+	    $this->assertEquals(7,$traitements->count());
+        
+        saveSetting('abs2_saisie_par_defaut_sans_manquement','y');
+        $traitements = AbsenceEleveTraitementQuery::create()->filterByManquementObligationPresence(true)
+                                                    	    ->useJTraitementSaisieEleveQuery()->useAbsenceEleveSaisieQuery()
+                                                    	    ->filterByEleve($florence_eleve)
+                                                    	    ->endUse()->endUse()->find();
+        $this->assertEquals(8,$traitements->count());
+        $traitements = AbsenceEleveTraitementQuery::create()->filterByManquementObligationPresence(false)
+                                                    	    ->useJTraitementSaisieEleveQuery()->useAbsenceEleveSaisieQuery()
+                                                    	    ->filterByEleve($florence_eleve)
+                                                    	    ->endUse()->endUse()->find();
+        $this->assertEquals(8,$traitements->count());
+        
+       saveSetting('abs2_saisie_par_defaut_sans_manquement','n');
+	}
 }
