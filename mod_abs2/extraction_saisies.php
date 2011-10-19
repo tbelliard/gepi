337741ccf56b68bbe20a1d43441a6a5182b4e023
Josselin Jacquard <josselin.jacquard@gmail.com>
2011-10-19 17:21:20 +0200

    [utf8] correction d'un bug sur une configuration de opentbs

--- mod_abs2/extraction_saisies.php
+++ mod_abs2/extraction_saisies.php
@@ -238,7 +238,7 @@ if ($affichage == 'html') {
 
     // Load the template
 	$extraction_saisies=repertoire_modeles('absence_extraction_saisies.ods');
-    $TBS->LoadTemplate($extraction_saisies);
+    $TBS->LoadTemplate($extraction_saisies, OPENTBS_ALREADY_UTF8);
 
     $titre = 'Extrait des absences du '.$dt_date_absence_eleve_debut->format('d/m/Y').' au '.$dt_date_absence_eleve_fin->format('d/m/Y');
     $classe = null;
