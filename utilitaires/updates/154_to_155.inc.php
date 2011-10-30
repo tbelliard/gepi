450e0e59bc91e32598687ea3bf2c7cd49b13c14b
crob <crob@e073496e-d129-0410-8f7b-841bfd467322>
2011-10-30 10:54:03 +0000

    Ajout de tests sur tempo_utilisateurs
    git-svn-id: http://svn.sylogix.org/gepiphp/trunk@8579 e073496e-d129-0410-8f7b-841bfd467322

--- utilitaires/updates/154_to_155.inc.php
+++ utilitaires/updates/154_to_155.inc.php
@@ -974,6 +974,29 @@ if ($test_tempo_utilisateurs == -1) {
 
 $test_tempo_utilisateurs = sql_query1("SHOW TABLES LIKE 'tempo_utilisateurs';");
 if ($test_tempo_utilisateurs!=-1) {
+
+	$test_champ=mysql_num_rows(mysql_query("SHOW COLUMNS FROM tempo_utilisateurs LIKE 'pers_id';"));
+	if ($test_champ!=0) {
+		$result .= "<br />&nbsp;->Changement du champ 'pers_id' de la table 'tempo_utilisateurs' en 'identifiant1'<br />";
+		$query = mysql_query("ALTER TABLE tempo_utilisateurs CHANGE pers_id identifiant1 VARCHAR( 10 ) NOT NULL;");
+		if ($query) {
+			$result .= msj_present("Le champ 'pers_id' de la table 'tempo_utilisateurs' a été changé en 'identifiant1'");
+		} else {
+			$result .= msj_erreur(": Erreur lors du changement du champ 'pers_id' de la table 'tempo_utilisateurs' en 'identifiant1'");
+		}
+	}
+
+	$test_champ=mysql_num_rows(mysql_query("SHOW COLUMNS FROM tempo_utilisateurs LIKE 'identifiant2';"));
+	if ($test_champ==0) {
+		$result .= "<br />&nbsp;->Ajout d'un champ 'identifiant2' à la table 'tempo_utilisateurs'<br />";
+		$query = mysql_query("ALTER TABLE tempo_utilisateurs ADD identifiant2 VARCHAR( 50 ) NOT NULL AFTER identifiant1;");
+		if ($query) {
+			$result .= msj_present("Le champ 'identifiant2' de la table 'tempo_utilisateurs' a été ajouté");
+		} else {
+			$result .= msj_erreur(": Le champ 'identifiant2' de la table 'tempo_utilisateurs' n'a pas été ajouté");
+		}
+	}
+
 	$test = sql_query1("SHOW TABLES LIKE 'tempo_utilisateurs_resp';");
 	if ($test != -1) {
 
