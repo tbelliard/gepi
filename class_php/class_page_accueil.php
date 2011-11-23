9ff03564ac79b7f19f7a13203d3b2f717f64b347
Stephane Boireau <crob2607@free.fr>
2011-11-23 21:29:21 +0100

    Correctif sur une section if()

--- class_php/class_page_accueil.php
+++ class_php/class_page_accueil.php
@@ -448,7 +448,7 @@ class class_page_accueil {
 			  "Bulletin : saisie des moyennes et des appréciations par matière",
 			  "Cet outil permet de saisir directement, sans passer par le carnet de notes, les moyennes et les appréciations du bulletin");
 
-	if($afficher_correction_validation=="y")
+	if($afficher_correction_validation=="y") {
 		$texte_item="Cet outil vous permet de valider les corrections d'appréciations proposées par des professeurs après la clôture d'une période.";
 		if($_SESSION['statut']=='scolarite') {
 			$sql="SELECT 1=1 FROM matieres_app_corrections map, j_scol_classes jsc, j_groupes_classes jgc where jsc.login='".$_SESSION['login']."' AND jsc.id_classe=jgc.id_classe AND jgc.id_groupe=map.id_groupe;";
@@ -463,6 +463,7 @@ class class_page_accueil {
 		$this->creeNouveauItem("/saisie/validation_corrections.php",
 			  "Correction des bulletins",
 			  $texte_item);
+	}
 
 	if ((($this->test_prof_suivi != "0") and (getSettingValue("GepiRubConseilProf")=='yes'))
 			or (($this->statutUtilisateur!='professeur') and (getSettingValue("GepiRubConseilScol")=='yes') )
