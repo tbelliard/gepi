f81b3195d5334d854345ef03947bb88e877f0422
regis85 <regis.bouguin@laposte.net>
2011-12-07 22:58:45 +0100

    Réencoder les CSV en ISO

--- lib/share.inc.php
+++ lib/share.inc.php
@@ -5045,10 +5045,8 @@ function getDateDescription($date_debut,$date_fin) {
  * @return string La chaine encodée 
  */
 function echo_csv_encoded($texte_csv) {
-	// D'après http://www.oxeron.com/2008/09/15/probleme-daccent-dans-un-export-csv-en-php
-
-	//$retour=$texte_csv;
-	$retour=chr(255).chr(254).mb_convert_encoding($texte_csv, 'UTF-16LE', 'UTF-8');
+	
+	$retour=mb_convert_encoding($texte_csv, 'ISO-8859-15', 'UTF-8');
 
 	return $retour;
 }
