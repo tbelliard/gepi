3c2e6fad67d7e3925e1d42f9e1688e187f54b714
Stephane Boireau <crob2607@free.fr>
2012-03-04 17:23:30 +0100

    Accès vers l'affichage semaine.

--- cahier_texte_2/see_all.php
+++ cahier_texte_2/see_all.php
@@ -1,7 +1,7 @@
 <?php
 /*
 *
-* Copyright 2001, 2011 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun, Gabriel Fischer
+* Copyright 2001, 2012 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun, Gabriel Fischer
 *
 * This file is part of GEPI.
 *
@@ -161,7 +161,9 @@ if ($current_imprime=='y') echo "<div id='container'>\n";
 //debug_var();
 
 // Création d'un espace entre le bandeau et le reste 
-echo "<p></p>\n";
+//echo "<p></p>\n";
+//echo "<div style='float:right; width:10em;z-index:10000;'><a href='consultation2.php'>Affichage semaine</a></div>\n";
+echo "<p><a href='consultation2.php'>Affichage semaine</a></p>\n";
 
 //On vérifie si le module est activé
 if (getSettingValue("active_cahiers_texte")!='y') {
