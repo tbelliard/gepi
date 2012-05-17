a94ab84bc4945ca85199e1285413628418877ee8
Stephane Boireau <crob2607@free.fr>
2012-05-17 20:48:14 +0200

    Accès à l'affichage semaine dans le cas où l'accès cdt est public.

--- cahier_texte_2/ajax_affichages_liste_notices.php
+++ cahier_texte_2/ajax_affichages_liste_notices.php
@@ -112,7 +112,9 @@ echo "<div id=\"div_chaine_liste_notices\" style=\"display:inline;\"><img id=\"c
 
 echo "<p style='font-size:9pt'>";
 if(getSettingValue('cahier_texte_acces_public')!='no'){
-	echo "<a href='../public/see_all.php?id_groupe=" . $current_group->getId() ."' target='_blank'>Visualiser l'accès public</a>\n<br>";
+	echo "<a href='../public/see_all.php?id_groupe=" . $current_group->getId() ."' target='_blank'>Visualiser l'accès public</a>\n";
+	echo " | <a href='consultation2.php?mode=professeur' target='_blank'>Aff.semaine</a>";
+	echo "<br>";
 } else {
 	//$classes_du_groupe = $current_group->getClasses();
 	//echo "<a href='./see_all.php?year=". date("Y") ."&month=". date("m") ."&day=". date("d") ."&id_classe=" . $classes_du_groupe[0]->getId() ."&id_groupe=" . $current_group->getId() ."'>Visualiser les cahiers de textes</a>\n<br>";
