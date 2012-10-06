7b219be85db045dca9235eed9fe7cc58527220b3
Stephane Boireau <crob2607@free.fr>
2012-10-06 18:00:15 +0200

    Affichage des effectifs.

--- groupes/repartition_ele_grp.php
+++ groupes/repartition_ele_grp.php
@@ -675,7 +675,6 @@ if(!isset($_POST['recopie_select'])) {
 			}
 		}
 	}
-
 </script>\n";
 
 	echo "<form action='".$_SERVER['PHP_SELF']."' name='form1' method='post'>\n";
@@ -809,7 +808,16 @@ if(!isset($_POST['recopie_select'])) {
 	echo "</td>\n";
 	echo "</tr>\n";
 	*/
-	
+
+	echo "<tr id='tr_effectifs' style='display:none;'>\n";
+	echo "<th>Effectifs</th>\n";
+	echo "<th id='effectif_total'></th>\n";
+	for($i=0;$i<count($id_groupe);$i++) {
+		echo "<th id='effectif_colonne_$i'></th>\n";
+	}
+	echo "<th></th>\n";
+	echo "</tr>\n";
+
 	// LISTE FOIREUSE UNE FOIS QU'ON A VALIDE UNE FOIS
 	//for($j=0;$j<count($group["eleves"]["all"]["list"]);$j++) {
 	$cpt=0;
@@ -861,8 +869,8 @@ if(!isset($_POST['recopie_select'])) {
 			echo "</td>\n";
 			*/
 
-			$ligne_si_desinscription_possible.="<td onclick=\"document.getElementById('grp_eleve_".$i."_".$cpt."').checked=true;changement();\">\n";
-			$ligne_si_desinscription_possible.="<input type='radio' name='grp_eleve[$cpt]' id='grp_eleve_".$i."_".$cpt."' value='".$id_groupe[$i]."' onchange='changement()' title=\"$tab_eleve[$j] -&gt; ".$group[$i]['name']." de ".$group[$i]['classlist_string']."\" ";
+			$ligne_si_desinscription_possible.="<td onclick=\"document.getElementById('grp_eleve_".$i."_".$cpt."').checked=true;calcule_effectifs();changement();\">\n";
+			$ligne_si_desinscription_possible.="<input type='radio' name='grp_eleve[$cpt]' id='grp_eleve_".$i."_".$cpt."' value='".$id_groupe[$i]."' onchange='calcule_effectifs();changement()' title=\"$tab_eleve[$j] -&gt; ".$group[$i]['name']." de ".$group[$i]['classlist_string']."\" ";
 			if(in_array($login_ele,$group[$i]["eleves"][$num_periode]["list"])) {
 				$ligne_si_desinscription_possible.="checked ";
 				if($nb_grp_ele>0) {$info_plusieurs_grp_ele.=", ";}
@@ -992,6 +1000,20 @@ if(!isset($_POST['recopie_select'])) {
 		}
 	}
 
+	function calcule_effectifs() {
+		for (var i=0;i<".count($id_groupe).";i++) {
+			total=0;
+			for (var ki=0;ki<$cpt;ki++) {
+				if(document.getElementById('grp_eleve_'+i+'_'+ki)) {
+					if(document.getElementById('grp_eleve_'+i+'_'+ki).checked==true) {total++;}
+				}
+			}
+			document.getElementById('effectif_colonne_'+i).innerHTML=total;
+		}
+		document.getElementById('effectif_total').innerHTML=$cpt;
+	}
+	calcule_effectifs();
+	document.getElementById('tr_effectifs').style.display='';
 </script>\n";
 
 	//====================================
