bcc820c2425688aa5056e69bac6034ea7ac0a34e
Stephane Boireau <crob2607@free.fr>
2012-11-28 15:26:58 +0100

    Modif possible via ajax des valeurs dans la page (moins lourd que de soumettre tout le formulaire).

--- groupes/check_enseignements.php
+++ groupes/check_enseignements.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * saisie des Notes
+ * Verifications sur les catégories, coef, visibilité des groupes,...
 *
 * Copyright 2001, 2012 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun
 *
@@ -74,7 +74,7 @@ if(isset($_GET['modif_visu_cn'])) {
 	}
 
 	$id_groupe=isset($_GET['id_groupe']) ? $_GET['id_groupe'] : NULL;
-	if(isset($id_groupe)) {
+	if((isset($id_groupe))&&(is_numeric($id_groupe))) {
 		$sql="SELECT visible FROM j_groupes_visibilite WHERE id_groupe='$id_groupe' AND domaine='cahier_notes';";
 		$test=mysql_query($sql);
 		if(mysql_num_rows($test)==0) {
@@ -98,7 +98,7 @@ if(isset($_GET['modif_visu_bull'])) {
 	}
 
 	$id_groupe=isset($_GET['id_groupe']) ? $_GET['id_groupe'] : NULL;
-	if(isset($id_groupe)) {
+	if((isset($id_groupe))&&(is_numeric($id_groupe))) {
 		$sql="SELECT visible FROM j_groupes_visibilite WHERE id_groupe='$id_groupe' AND domaine='bulletins';";
 		$test=mysql_query($sql);
 		if(mysql_num_rows($test)==0) {
@@ -122,7 +122,7 @@ if(isset($_GET['modif_visu_cdt'])) {
 	}
 
 	$id_groupe=isset($_GET['id_groupe']) ? $_GET['id_groupe'] : NULL;
-	if(isset($id_groupe)) {
+	if((isset($id_groupe))&&(is_numeric($id_groupe))) {
 		$sql="SELECT visible FROM j_groupes_visibilite WHERE id_groupe='$id_groupe' AND domaine='cahier_texte';";
 		$test=mysql_query($sql);
 		if(mysql_num_rows($test)==0) {
@@ -143,17 +143,71 @@ if(isset($_GET['modif_coef'])) {
 
 	$id_groupe=isset($_GET['id_groupe']) ? $_GET['id_groupe'] : NULL;
 	$id_classe=isset($_GET['id_classe']) ? $_GET['id_classe'] : NULL;
-	if((isset($id_groupe))&&(isset($id_classe))) {
+	if((isset($id_groupe))&&(isset($id_classe))&&(is_numeric($id_groupe))&&(is_numeric($id_classe))) {
 		$sql="SELECT coef FROM j_groupes_classes WHERE id_groupe='$id_groupe' AND id_classe='$id_classe';";
 		$test=mysql_query($sql);
 		if(mysql_num_rows($test)==0) {
 			$msg="Anomalie&nbsp;: Le groupe n'existe pas dans j_groupes_classes???<br />";
+
+			if(isset($_GET['modif_ajax'])) {
+				echo "<span style='color:red' title=\"Anomalie : Le groupe n'existe pas dans j_groupes_classes ???\n$sql\">Anomalie</span>";
+				die();
+			}
 		}
 		else {
 			$sql="UPDATE j_groupes_classes SET coef='$coef' WHERE id_groupe='$id_groupe' AND id_classe='$id_classe';";
 			$update=mysql_query($sql);
+
+			if(($update)&&(isset($_GET['modif_ajax']))) {
+				echo $coef;
+				die();
+			}
 		}
 	}
+}
+
+if((isset($_GET['modif_ajax']))&&(isset($_GET['domaine']))&&(isset($_GET['id_groupe']))&&(isset($_GET['passer_a']))) {
+	check_token();
+
+	$domaine=$_GET['domaine'];
+	$passer_a=$_GET['passer_a'];
+	if(!in_array($domaine, $tab_domaines)) {
+		echo "<span style='color:red' title=\"Erreur : Le domaine choisi est inconnu : $domaine\">Erreur</span>";
+	}
+	elseif(($passer_a!='y')&&($passer_a!='n')) {
+		echo "<span style='color:red' title=\"Le mode choisi est inconnu : $passer_a\">Erreur</span>";
+	}
+	else {
+		$id_groupe=isset($_GET['id_groupe']) ? $_GET['id_groupe'] : NULL;
+		if((isset($id_groupe))&&(is_numeric($id_groupe))) {
+			$sql="SELECT visible FROM j_groupes_visibilite WHERE id_groupe='$id_groupe' AND domaine='$domaine';";
+			$test=mysql_query($sql);
+			if(mysql_num_rows($test)==0) {
+				$sql="INSERT INTO j_groupes_visibilite SET id_groupe='$id_groupe', domaine='$domaine', visible='$passer_a';";
+				$insert_ou_update=mysql_query($sql);
+			}
+			else {
+				$sql="UPDATE j_groupes_visibilite SET visible='$passer_a' WHERE id_groupe='$id_groupe' AND domaine='$domaine';";
+				$insert_ou_update=mysql_query($sql);
+			}
+
+			if($insert_ou_update) {
+				if($passer_a=='y') {
+					echo "<span style='color:blue'>OUI</span>";
+				}
+				else {
+					echo "<span style='color:red'>NON</span>";
+				}
+			}
+			else {
+				echo "<span style='color:red' title=\"Erreur lors de $sql\">Erreur</span>";
+			}
+		}
+		else {
+			echo "<span style='color:red' title=\"Le groupe n'est pas choisi ou non valable.\">Erreur</span>";
+		}
+	}
+	die();
 }
 
 $themessage  = 'Des paramètres ont été modifiés. Voulez-vous vraiment quitter sans enregistrer ?';
@@ -190,6 +244,81 @@ if(mysql_num_rows($res_classe)==0) {
 	die();
 }
 
+echo "<script type='text/javascript'>
+var griser_cn='$griser_cn';
+var griser_bull='$griser_bull';
+var griser_cdt='$griser_cdt';
+var masquer_cn='$masquer_cn';
+var masquer_bull='$masquer_bull';
+var masquer_cdt='$masquer_cdt';
+
+function modif_coef(id, delta, id_groupe, id_classe) {
+	if(document.getElementById(id)) {
+		coef_actuel=document.getElementById(id).innerHTML;
+		coef_modifie=eval(eval(coef_actuel)+delta);
+		//alert('coef_actuel='+coef_actuel);
+
+		if(coef_modifie>=0) {
+			//alert(eval(eval(coef_actuel)+delta));
+
+			new Ajax.Updater($(id),'".$_SERVER['PHP_SELF']."?modif_ajax=y&modif_coef='+coef_modifie+'&id_groupe='+id_groupe+'&id_classe='+id_classe+'".add_token_in_url(false)."',{method: 'get'});
+		}
+	}
+}
+
+function modif_visibilite(domaine, id, id_groupe) {
+	if(document.getElementById(id)) {
+		contenu_actuel=document.getElementById(id).innerHTML;
+		passer_a='';
+		/*
+		alert(contenu_actuel)
+		alert(contenu_actuel.search('/OUI/'))
+		if(contenu_actuel.search('/OUI/')!=-1) {
+			passer_a='n';
+		}
+		else {
+			if(contenu_actuel.search('/NON/')!=-1) {
+				passer_a='y';
+			}
+		}
+		*/
+		var reg1=new RegExp(\"OUI\",\"g\");
+		if (contenu_actuel.match(reg1)) {
+			passer_a='n';
+		}
+		else {
+			var reg2=new RegExp(\"NON\",\"g\");
+			if(contenu_actuel.match(reg2)) {
+				passer_a='y';
+			}
+		}
+
+		//alert(passer_a);
+		if((passer_a=='y')||(passer_a=='n')) {
+
+			new Ajax.Updater($(id),'".$_SERVER['PHP_SELF']."?modif_ajax=y&domaine='+domaine+'&passer_a='+passer_a+'&id_groupe='+id_groupe+'".add_token_in_url(false)."',{method: 'get'});
+
+			// On ne fait que griser même si on a demandé à masquer: précaution pour voir une éventuelle erreur de la requête ajax
+			if(((griser_cn=='y')&&(domaine=='cahier_notes'))||
+				((griser_bull=='y')&&(domaine=='bulletins'))||
+				((griser_cdt=='y')&&(domaine=='cahier_texte'))||
+				((masquer_cn=='y')&&(domaine=='cahier_notes'))||
+				((masquer_bull=='y')&&(domaine=='bulletins'))||
+				((masquer_cdt=='y')&&(domaine=='cahier_texte'))
+				) {
+
+				if(passer_a=='y') {
+					document.getElementById('tr_'+id_groupe).style.backgroundColor='';
+				}
+				else {
+					document.getElementById('tr_'+id_groupe).style.backgroundColor='grey';
+				}
+			}
+		}
+	}
+}
+</script>\n";
+
 while($lig_classe=mysql_fetch_object($res_classe)) {
 	echo "<a name='classe_".$lig_classe->classe."'></a>\n";
 	echo "<p class='bold'>Classe de $lig_classe->classe</p>\n";
@@ -219,18 +348,19 @@ while($lig_classe=mysql_fetch_object($res_classe)) {
 			if(($masquer_cdt=="y")&&(isset($current_group["visibilite"]["cahier_texte"]))&&($current_group["visibilite"]["cahier_texte"]=="n")) {$afficher_ligne="n";}
 
 			if($afficher_ligne=="y") {
+				$alt=$alt*(-1);
 				if(($griser_cn=='y')&&(isset($current_group["visibilite"]["cahier_notes"]))&&($current_group["visibilite"]["cahier_notes"]=="n")) {
-					echo "<tr class='white_hover' style='background-color: grey;'>\n";
+					echo "<tr id='tr_".$current_group["id"]."' class='lig$alt white_hover' style='background-color: grey;'>\n";
 				}
 				elseif(($griser_bull=='y')&&(isset($current_group["visibilite"]["bulletins"]))&&($current_group["visibilite"]["bulletins"]=="n")) {
-					echo "<tr class='white_hover' style='background-color: grey;'>\n";
+					echo "<tr id='tr_".$current_group["id"]."' class='lig$alt white_hover' style='background-color: grey;'>\n";
 				}
 				elseif(($griser_cdt=='y')&&(isset($current_group["visibilite"]["cahier_texte"]))&&($current_group["visibilite"]["cahier_texte"]=="n")) {
-					echo "<tr class='white_hover' style='background-color: grey;'>\n";
+					echo "<tr id='tr_".$current_group["id"]."' class='lig$alt white_hover' style='background-color: grey;'>\n";
 				}
 				else {
-					$alt=$alt*(-1);
-					echo "<tr class='lig$alt white_hover'>\n";
+					//$alt=$alt*(-1);
+					echo "<tr id='tr_".$current_group["id"]."' class='lig$alt white_hover'>\n";
 				}
 				echo "<td>".$current_group['name']."</td>\n";
 				echo "<td>".$current_group['description']."</td>\n";
@@ -248,49 +378,63 @@ while($lig_classe=mysql_fetch_object($res_classe)) {
 				$coef=$current_group["classes"]["classes"][$lig_classe->id]['coef'];
 				$coef_up=$current_group["classes"]["classes"][$lig_classe->id]['coef']+1;
 				$coef_down=max($current_group["classes"]["classes"][$lig_classe->id]['coef']-1,0);
-				echo "<a href='".$_SERVER['PHP_SELF']."?modif_coef=$coef_up&amp;id_groupe=".$current_group["id"]."&amp;id_classe=$lig_classe->id".$option_grisage.$option_masquage.add_token_in_url()."#classe_$lig_classe->classe'>";
+				echo "<a href='".$_SERVER['PHP_SELF']."?modif_coef=$coef_up&amp;id_groupe=".$current_group["id"]."&amp;id_classe=$lig_classe->id".$option_grisage.$option_masquage.add_token_in_url()."#classe_$lig_classe->classe' onclick=\"modif_coef('coef_".$current_group["id"]."', 1, ".$current_group["id"].", ".$lig_classe->id.");return false;\">";
 				echo "<img src='../images/icons/add.png' /> ";
 				echo "</a>";
+				echo "<span id='coef_".$current_group["id"]."'>";
 				echo $coef;
-				echo " <a href='".$_SERVER['PHP_SELF']."?modif_coef=$coef_down&amp;id_groupe=".$current_group["id"]."&amp;id_classe=$lig_classe->id".$option_grisage.$option_masquage.add_token_in_url()."#classe_$lig_classe->classe'>";
+				echo "</span>";
+				echo " <a href='".$_SERVER['PHP_SELF']."?modif_coef=$coef_down&amp;id_groupe=".$current_group["id"]."&amp;id_classe=$lig_classe->id".$option_grisage.$option_masquage.add_token_in_url()."#classe_$lig_classe->classe' onclick=\"modif_coef('coef_".$current_group["id"]."', -1, ".$current_group["id"].", ".$lig_classe->id.");return false;\">";
 				echo "<img src='../images/icons/remove.png' />";
 				echo "</a>";
 				echo "</td>\n";
 	
 				echo "<td>";
 				if((!isset($current_group["visibilite"]["cahier_notes"]))||($current_group["visibilite"]["cahier_notes"]=="y")) {
-					echo "<a href='".$_SERVER['PHP_SELF']."?modif_visu_cn=cacher&amp;id_groupe=".$current_group["id"].$option_grisage.$option_masquage.add_token_in_url()."#classe_$lig_classe->classe'>";
+					echo "<a href='".$_SERVER['PHP_SELF']."?modif_visu_cn=cacher&amp;id_groupe=".$current_group["id"].$option_grisage.$option_masquage.add_token_in_url()."#classe_$lig_classe->classe' onclick=\"modif_visibilite('cahier_notes', 'visibilite_cn_".$current_group["id"]."', ".$current_group["id"].");return false;\">";
+					echo "<span id='visibilite_cn_".$current_group["id"]."'>";
 					echo "<span style='color:blue;'>OUI</span>";
+					echo "</span>";
 					echo "</a>";
 				}
 				else {
-					echo "<a href='".$_SERVER['PHP_SELF']."?modif_visu_cn=afficher&amp;id_groupe=".$current_group["id"].$option_grisage.$option_masquage.add_token_in_url()."#classe_$lig_classe->classe'>";
+					echo "<a href='".$_SERVER['PHP_SELF']."?modif_visu_cn=afficher&amp;id_groupe=".$current_group["id"].$option_grisage.$option_masquage.add_token_in_url()."#classe_$lig_classe->classe' onclick=\"modif_visibilite('cahier_notes', 'visibilite_cn_".$current_group["id"]."', ".$current_group["id"].");return false;\">";
+					echo "<span id='visibilite_cn_".$current_group["id"]."'>";
 					echo "<span style='color:red;'>NON</span>";
+					echo "</span>";
 					echo "</a>";
 				}
 				echo "</td>\n";
 				echo "<td>";
 				if((!isset($current_group["visibilite"]["bulletins"]))||($current_group["visibilite"]["bulletins"]=="y")) {
-					echo "<a href='".$_SERVER['PHP_SELF']."?modif_visu_bull=cacher&amp;id_groupe=".$current_group["id"].$option_grisage.$option_masquage.add_token_in_url()."#classe_$lig_classe->classe'>";
+					echo "<a href='".$_SERVER['PHP_SELF']."?modif_visu_bull=cacher&amp;id_groupe=".$current_group["id"].$option_grisage.$option_masquage.add_token_in_url()."#classe_$lig_classe->classe' onclick=\"modif_visibilite('bulletins', 'visibilite_bull_".$current_group["id"]."', ".$current_group["id"].");return false;\">";
+					echo "<span id='visibilite_bull_".$current_group["id"]."'>";
 					echo "<span style='color:blue;'>OUI</span>";
+					echo "</span>";
 					echo "</a>";
 				}
 				else {
-					echo "<a href='".$_SERVER['PHP_SELF']."?modif_visu_bull=afficher&amp;id_groupe=".$current_group["id"].$option_grisage.$option_masquage.add_token_in_url()."#classe_$lig_classe->classe'>";
+					echo "<a href='".$_SERVER['PHP_SELF']."?modif_visu_bull=afficher&amp;id_groupe=".$current_group["id"].$option_grisage.$option_masquage.add_token_in_url()."#classe_$lig_classe->classe' onclick=\"modif_visibilite('bulletins', 'visibilite_bull_".$current_group["id"]."', ".$current_group["id"].");return false;\">";
+					echo "<span id='visibilite_bull_".$current_group["id"]."'>";
 					echo "<span style='color:red;'>NON</span>";
+					echo "</span>";
 					echo "</a>";
 				}
 				echo "</td>\n";
 
 				echo "<td>";
 				if((!isset($current_group["visibilite"]["cahier_texte"]))||($current_group["visibilite"]["cahier_texte"]=="y")) {
-					echo "<a href='".$_SERVER['PHP_SELF']."?modif_visu_cdt=cacher&amp;id_groupe=".$current_group["id"].$option_grisage.$option_masquage.add_token_in_url()."#classe_$lig_classe->classe'>";
+					echo "<a href='".$_SERVER['PHP_SELF']."?modif_visu_cdt=cacher&amp;id_groupe=".$current_group["id"].$option_grisage.$option_masquage.add_token_in_url()."#classe_$lig_classe->classe' onclick=\"modif_visibilite('cahier_texte', 'visibilite_cdt_".$current_group["id"]."', ".$current_group["id"].");return false;\">";
+					echo "<span id='visibilite_cdt_".$current_group["id"]."'>";
 					echo "<span style='color:blue;'>OUI</span>";
+					echo "</span>";
 					echo "</a>";
 				}
 				else {
-					echo "<a href='".$_SERVER['PHP_SELF']."?modif_visu_cdt=afficher&amp;id_groupe=".$current_group["id"].$option_grisage.$option_masquage.add_token_in_url()."#classe_$lig_classe->classe'>";
+					echo "<a href='".$_SERVER['PHP_SELF']."?modif_visu_cdt=afficher&amp;id_groupe=".$current_group["id"].$option_grisage.$option_masquage.add_token_in_url()."#classe_$lig_classe->classe' onclick=\"modif_visibilite('cahier_texte', 'visibilite_cdt_".$current_group["id"]."', ".$current_group["id"].");return false;\">";
+					echo "<span id='visibilite_cdt_".$current_group["id"]."'>";
 					echo "<span style='color:red;'>NON</span>";
+					echo "</span>";
 					echo "</a>";
 				}
 				echo "</td>\n";
