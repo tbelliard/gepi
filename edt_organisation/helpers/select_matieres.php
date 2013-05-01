bdaedbe0aef148c45690353ecaebadfe9ce4ddb1
Stephane Boireau <crob2607@free.fr>
2013-05-01 11:12:08 +0200

    Amélioration de la détection d'associations. Modifications cosmétiques.

--- edt_organisation/helpers/select_matieres.php
+++ edt_organisation/helpers/select_matieres.php
@@ -28,18 +28,33 @@ $nbre = mysql_num_rows($query);
 
 $warning = ' style="background-color: orange;"'; // si il ne trouve pas de correspondance, on modifie le fond
 
+$matiere_selected_nettoyee=preg_replace("/[^A-Za-z0-9\-]/", "", $matiere_selected);
+
 for($i = 0; $i < $nbre; $i++) {
     $matiere[$i] = mysql_result($query, $i, "matiere");
     $nom[$i] = mysql_result($query, $i, "nom_complet");
-    if (my_strtoupper(trim(remplace_accents($nom[$i], 'all_nospace'))) == $matiere_selected OR my_strtoupper(trim(remplace_accents($matiere[$i], 'all_nospace'))) == $matiere_selected) {
+
+	$matiere_nettoyee[$i]=my_strtoupper(trim(remplace_accents($matiere[$i], 'all_nospace')));
+	$nom_nettoye[$i]=my_strtoupper(trim(remplace_accents($nom[$i], 'all_nospace')));
+
+    if ($matiere[$i] == $matiere_selected OR 
+    $matiere_nettoyee[$i] == $matiere_selected OR 
+    $nom[$i] == $matiere_selected OR 
+    $nom_nettoye[$i] == $matiere_selected OR
+    $matiere[$i] == $matiere_selected_nettoyee OR 
+    $matiere_nettoyee[$i] == $matiere_selected_nettoyee OR 
+    $nom[$i] == $matiere_selected_nettoyee OR 
+    $nom_nettoye[$i] == $matiere_selected_nettoyee
+    ) {
         $selected = ' selected="selected"';
         $warning = ''; // il a trouvé une correspondance, donc on enlève le fond.
-    } else {
+    }
+    else {
         $selected = '';
     }
 
     $options .= '
-		<option value="' . $matiere[$i] . '"' . $selected . '>' . $nom[$i] . '</option>';
+		<option value="' . $matiere[$i] . '"' . $selected . ' title="'.$matiere[$i].'">' . $nom[$i] . '</option>';
 }
 echo '
 	<select name ="' . $increment . '"'.$warning .' onmouseover="if(document.getElementById(\'texte_nomGepi'.$l.'\')) {document.getElementById(\'texte_nomGepi'.$l.'\').style.backgroundColor=\'yellow\'}" onmouseout="if(document.getElementById(\'texte_nomGepi'.$l.'\')) {document.getElementById(\'texte_nomGepi'.$l.'\').style.backgroundColor=\'\'}">
