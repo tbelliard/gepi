46f9ae84b0c3b8f1c375fcc35444a996c583fa49
Stephane Boireau <crob2607@free.fr>
2013-08-29 10:53:25 +0200

    Correctif ENT Eure.

--- init_xml2/index.php
+++ init_xml2/index.php
@@ -45,7 +45,9 @@ require_once("../lib/header.inc.php");
 //**************** FIN EN-TETE *****************
 ?>
 <p class='bold'><a href="../gestion/index.php#init_xml2"><img src='../images/icons/back.png' alt='Retour' class='back_link'/> Retour</a></p>
-<?php if (getSettingValue('use_ent') == 'y') {
+<?php
+// A MODIFIER : Pouvoir gérer use_ent et NetCollege ITOP hors 27:
+if ((getSettingValue("use_ent") == 'y')&&(!preg_match("/^027/", getSettingValue('gepiSchoolRne')))) {
 	echo '<p>Avant de commencer, vous devez récupérer les logins de vos utilisateurs dans l\'ENT : <a href="../mod_ent/index.php">RECUPERER</a></p>';
 }
 ?>
