74729e10db2d56fd4ea8679ffca24552201d70d4
Stephane Boireau <crob2607@free.fr>
2013-10-01 17:06:29 +0200

    Possibilité de donner le droit aux comptes scolarité/cpe de modifier la composition des groupes.

--- accueil.php
+++ accueil.php
@@ -380,6 +380,25 @@ Veuillez vérifier que le répertoire /temp de Gepi est accessible en écriture 
 	}
 }
 
+// Cas particulier CPE et SCOL:
+if((($_SESSION['statut']=='cpe')&&(getSettingAOui('CpeEditElevesGroupes'))&&(acces('/groupes/edit_eleves.php', 'cpe')))||
+(($_SESSION['statut']=='scolarite')&&(getSettingAOui('ScolEditElevesGroupes'))&&(acces('/groupes/edit_eleves.php', 'scolarite')))) {
+
+	$sql="SELECT DISTINCT id_groupe, declarant FROM j_signalement WHERE nature='erreur_affect';";
+	$res_sign=mysql_query($sql);
+	if(mysql_num_rows($res_sign)>0) {
+		$tbs_signalement="<p class='tbs_signalement'>Une ou des erreurs d'affectation d'élèves ont été signalées dans le ou les enseignements suivants&nbsp;:<br />\n";
+		while($lig_sign=mysql_fetch_object($res_sign)) {
+			$tmp_tab_champ=array('classes');
+			$current_group_sign=get_group($lig_sign->id_groupe,$tmp_tab_champ);
+			$current_group_sign['description']=str_replace  ( "&"  , "&amp;"  , $current_group_sign['description'] );
+			$tbs_signalement.="<a href='groupes/edit_eleves.php?id_groupe=".$lig_sign->id_groupe."&amp;id_classe=".$current_group_sign['classes']['list'][0]."'>".$current_group_sign['name']." (<em>".$current_group_sign['description']." ".$current_group_sign['classlist_string']."</em>)</a> signalé par ".affiche_utilisateur($lig_sign->declarant,$current_group_sign['classes']['list'][0])."<br />\n";
+		}
+		$tbs_signalement.="</p>\n";
+		$afficheAccueil->signalement=$tbs_signalement;
+	}
+}
+
 // ----- interface graphique prof -----
 if($_SESSION['statut']=="professeur"){
 	$tbs_interface_graphique[]=array("classe"=>"bold","lien"=>"accueil_simpl_prof.php","titre"=>"Interface graphique");
