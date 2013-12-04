13f5c18dd4fb1ac60f694ddf8cea2efcae030e3f
Stephane Boireau <crob2607@free.fr>
2013-12-04 12:29:19 +0100

    Ajout d'un fichier oublié.

---
+++ mod_discipline/mod_discipline.lib.php
@@ -0,0 +1,8 @@
+<?php
+// Initialisations diverses pour le module Discipline
+$mod_disc_terme_incident=getSettingValue('mod_disc_terme_incident');
+if($mod_disc_terme_incident=="") {$mod_disc_terme_incident="incident";}
+
+$mod_disc_terme_sanction=getSettingValue('mod_disc_terme_sanction');
+if($mod_disc_terme_sanction=="") {$mod_disc_terme_sanction="sanction";}
+?>
