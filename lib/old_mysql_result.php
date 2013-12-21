6d877cf8be5726e874b557a5cb21695f00a2fbbb
Stephane Boireau <crob2607@free.fr>
2013-12-21 17:28:40 +0100

    Modification pour repérer les pb avec la fonction et envoyer un mail à l'admin

--- lib/old_mysql_result.php
+++ lib/old_mysql_result.php
@@ -3,6 +3,26 @@
 # mysql to mysqli migration library
 # by dotpointer
 	function old_mysql_result ($result , $row , $field = 0) {
+		if (!(strpos($field,".")===false)) {
+			if(getSettingValue('debug_old_mysql_result')!="no") {
+				$ajout_header="";
+				$email_destinataires=getSettingValue('gepiAdminAdress');
+				if(check_mail($email_destinataires)) {
+					$sujet_mail="[Gepi]: Probleme old_mysql_result";
+					$texte_mail="Bonjour,
+
+Un problème a été détecté dans ".$_SERVER['PHP_SELF']." avec la fonction old_mysql_result().
+Merci d'en informer la liste de diffusion officielle Gepi pour aider à corriger ce problème.
+
+Cordialement.
+-- 
+Equipe de developpement Gepi.";
+					$envoi = envoi_mail($sujet_mail, $texte_mail, $email_destinataires, $ajout_header);
+				}
+			}
+			die ("<br /><h1>Pb old_mysql_result dans ".$_SERVER['PHP_SELF']."</h1>");
+		}
+
 		if (mysqli_data_seek($result, $row) === false) return false;
 		if (is_int($field)) $line=mysqli_fetch_array($result); else $line=mysqli_fetch_assoc($result);
 		if (!isset($line[$field])) return false;
