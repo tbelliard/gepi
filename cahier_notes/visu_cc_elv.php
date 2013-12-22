7f0de58305ab9b20d90140b50d644f5b2372bc8e
Stephane Boireau <crob2607@free.fr>
2013-12-22 18:07:20 +0100

    Correctif sur le chemin des images.

--- cahier_notes/visu_cc_elv.php
+++ cahier_notes/visu_cc_elv.php
@@ -778,7 +778,7 @@ if (!suivi_ariane($_SERVER['PHP_SELF'],$nom_cc))
 							height:200px;"> &nbsp;</div>
 <div id="alert_entete" style="z-index:2000;
 								display:none;
-								position:absolute;"><img   src="./images/alerte_entete.png" alt="alerte" /></div>
+								position:absolute;"><img   src="../images/alerte_entete.png" alt="alerte" /></div>
 <div id="alert_popup" style="z-index:2000;
 								text-align:justify;
 								width:600px;
@@ -790,12 +790,12 @@ if (!suivi_ariane($_SERVER['PHP_SELF'],$nom_cc))
 								padding-right:20px;
 								display:none;
 								position:absolute;
-								background-image:url('./images/degrade_noir.png');
+								background-image:url('../images/degrade_noir.png');
 								background-repeat:repeat-x;
 								background-position: left bottom;">
 	<div id="alert_message"></div>
 	<div id="alert_button" style="margin:5px auto;width:90px;">
-		<div id="alert_bouton_ok" style="float:left;"><img src="./images/bouton_continue.png" alt="ok" /></div>
+		<div id="alert_bouton_ok" style="float:left;"><img src="../images/bouton_continue.png" alt="ok" /></div>
 	</div>
 </div>
 
