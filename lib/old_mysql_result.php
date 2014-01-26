29bf25146a9adc177ff21862077baf0f65e4b429
Marc Leygnac <marc.leygnac@ac-poitiers.fr>
2014-01-26 11:24:05 +0100

    Code plus lisible (?)

--- lib/old_mysql_result.php
+++ lib/old_mysql_result.php
@@ -11,21 +11,23 @@
  */
 function old_mysql_result($result,$row,$field=0) {
 	if ($result===false) return;
-	if (mysqli_data_seek($result,$row) === false) return false;
-	$pos=strpos($field,".");
-	if (!($pos===false)) {
+	if ($row>mysqli_num_rows($result)) return false;
+	if (is_string($field) && !(strpos($field,".")===false)) {
 		// si $field est de la forme table.field ou alias.field
 		// on convertit $field en indice numérique
 		$t_field=explode(".",$field);
 		$field=-1;
 		$t_fields=mysqli_fetch_fields($result);
 		for ($id=0;$id<mysqli_num_fields($result);$id++) {
-			if ($t_fields[$id]->table==$t_field[0] && $t_fields[$id]->name==$t_field[1]) $field=$id;
+			if ($t_fields[$id]->table==$t_field[0] && $t_fields[$id]->name==$t_field[1]) {
+				$field=$id;
+				break;
 			}
+		}
 		if ($field==-1) return false;
 	}
+	mysqli_data_seek($result,$row);
 	$line=mysqli_fetch_array($result);
-	if (!isset($line[$field])) return false;
-	return $line[$field];
+	return isset($line[$field])?$line[$field]:false;
 }
 ?>
