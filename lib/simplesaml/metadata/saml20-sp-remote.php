a01b3afee9453c3cc6a8be8ec7518e2f69b66fb6
Stephane Boireau <crob2607@free.fr>
2014-05-11 10:30:10 +0200

    Rétablissement du fonctionnement simpleSAML

--- lib/simplesaml/metadata/saml20-sp-remote.php
+++ lib/simplesaml/metadata/saml20-sp-remote.php
@@ -5,23 +5,35 @@
  * See: http://simplesamlphp.org/docs/trunk/simplesamlphp-reference-sp-remote
  */
 
-/*
- * Example simpleSAMLphp SAML 2.0 SP
- */
-$metadata['https://saml2sp.example.org'] = array(
-	'AssertionConsumerService' => 'https://saml2sp.example.org/simplesaml/module.php/saml/sp/saml2-acs.php/default-sp',
-	'SingleLogoutService' => 'https://saml2sp.example.org/simplesaml/module.php/saml/sp/saml2-logout.php/default-sp',
-);
+/* configuration automatique */
+if (getSettingValue('gepiEnableIdpSaml20') == 'yes') {
+	//on va charger l'adresse SACoche configurÃ©e en admin gepi si elle est prÃ©cisÃ©e
+	$path = dirname(dirname(dirname(dirname(__FILE__))));
+	require_once("$path/secure/connect.inc.php");
+	// Database connection
+	require_once("$path/lib/mysql.inc");
+	require_once("$path/lib/settings.inc");
+	// Load settings
+	if (!loadSettings()) {
+	    die("Erreur chargement settings");
+	}
+	
+	if (getSettingValue('sacocheUrl') != null) {
+		$sacocheUrl = getSettingValue('sacocheUrl');
+		if (substr($sacocheUrl,strlen($sacocheUrl)-1,1) != '/') {$sacocheUrl .= '/';} //on rajout un / a  la fin
+		$firstEntityID = 'sacoche-sp';
+		$firstEntityArray = array();
+		$firstEntityArray['AssertionConsumerService'] = $sacocheUrl.'_lib/SimpleSAMLphp/www/module.php/saml/sp/saml2-acs.php/distant-gepi-saml';
+		$firstEntityArray['SingleLogoutService'] = $sacocheUrl.'_lib/SimpleSAMLphp/www/module.php/saml/sp/saml2-logout.php/distant-gepi-saml';
+		$metadata[$firstEntityID]= $firstEntityArray;
+	}
 
-/*
- * This example shows an example config that works with Google Apps for education.
- * What is important is that you have an attribute in your IdP that maps to the local part of the email address
- * at Google Apps. In example, if your google account is foo.com, and you have a user that has an email john@foo.com, then you
- * must set the simplesaml.nameidattribute to be the name of an attribute that for this user has the value of 'john'.
- */
-$metadata['google.com'] = array(
-	'AssertionConsumerService' => 'https://www.google.com/a/g.feide.no/acs',
-	'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
-	'simplesaml.nameidattribute' => 'uid',
-	'simplesaml.attributes' => FALSE,
-);
+	/*configuration pour un gepi distant qui va venir s'identifier sur nous à décommenter sur le serveur maitre
+	$metadata['gepi-esclave-sp'] = array(
+		'AssertionConsumerService' => 'https://www.mon-serveur-esclave-gepi.fr/gepi/_lib/simplesaml/www/module.php/saml/sp/saml2-acs.php/distant-gepi-saml',
+		'SingleLogoutService' => 'http://www.mon-serveur-esclave-gepi.fr/gepi/_lib/simplesaml/www/module.php/saml/sp/saml2-logout.php/distant-gepi-saml',
+	);*/
+	
+}
+/* fin de configuration automatique */
+?>
