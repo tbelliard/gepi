2e5085597b21e57b81990622eb9fbe02a7007914
Stephane Boireau <crob2607@free.fr>
2014-05-28 16:25:08 +0200

    Affichage des accès exceptionnels en cours.

--- bulletin/verif_bulletins.php
+++ bulletin/verif_bulletins.php
@@ -1190,11 +1190,53 @@ Les saisies/modifications sont possibles.";
 <ul>";
 				if($acces_autorisation_exceptionnelle_modif_cn) {
 					echo "
-	<li><a href='../cahier_notes/autorisation_exceptionnelle_saisie.php?id_classe=$id_classe".$ajout."' target='_blank'>la saisie/modification de notes du carnet de notes,</a></li>";
+	<li><a href='../cahier_notes/autorisation_exceptionnelle_saisie.php?id_classe=$id_classe".$ajout."' target='_blank'>la saisie/modification de notes du carnet de notes,</a>";
+
+				foreach($tab_prof['groupe'] as $group_id => $tab_group) {
+					$sql="SELECT * FROM acces_cn WHERE id_groupe='$group_id' AND periode='$per' AND date_limite>'".strftime("%Y-%m-%d %H:%M:%S")."' ORDER BY date_limite ASC;";
+					//echo "$sql<br />";
+					$test = mysqli_query($mysqli, $sql);
+					if($test->num_rows > 0) {
+						while($lig_acces=mysqli_fetch_object($test)) {
+							echo "<br />
+		".$tab_group['info']."&nbsp;: Accès (<em>à la saisie de notes dans le Carnet de notes</em>) ouvert jusqu'au ".formate_date($lig_acces->date_limite, "y");
+						}
+					}
+				}
+
+				echo "</li>";
 				}
 				echo "
-	<li><a href='autorisation_exceptionnelle_saisie_note.php?id_classe=$id_classe".$ajout."' target='_blank'>la saisie/modification de notes du bulletin,</a></li>
-	<li><a href='autorisation_exceptionnelle_saisie_app.php?id_classe=$id_classe".$ajout."' target='_blank'>la proposition de saisie d'appréciation(s) sur les bulletins.</a></li>
+	<li><a href='autorisation_exceptionnelle_saisie_note.php?id_classe=$id_classe".$ajout."' target='_blank'>la saisie/modification de notes du bulletin,</a>";
+
+				foreach($tab_prof['groupe'] as $group_id => $tab_group) {
+					$sql="SELECT * FROM acces_exceptionnel_matieres_notes WHERE  id_groupe='$group_id' AND periode='$per' AND date_limite>'".strftime("%Y-%m-%d %H:%M:%S")."' ORDER BY date_limite ASC;";
+					//echo "$sql<br />";
+					$test = mysqli_query($mysqli, $sql);
+					if($test->num_rows > 0) {
+						while($lig_acces=mysqli_fetch_object($test)) {
+							echo "<br />
+		".$tab_group['info']."&nbsp;: Accès (<em>à la saisie de notes dans les Bulletins</em>) ouvert jusqu'au ".formate_date($lig_acces->date_limite, "y");
+						}
+					}
+				}
+
+				echo "</li>
+	<li><a href='autorisation_exceptionnelle_saisie_app.php?id_classe=$id_classe".$ajout."' target='_blank'>la proposition de saisie d'appréciation(s) sur les bulletins.</a>";
+
+				foreach($tab_prof['groupe'] as $group_id => $tab_group) {
+					$sql="SELECT * FROM matieres_app_delais WHERE id_groupe='$group_id' AND periode='$per' AND date_limite>'".strftime("%Y-%m-%d %H:%M:%S")."' ORDER BY date_limite ASC;";
+					//echo "$sql<br />";
+					$test = mysqli_query($mysqli, $sql);
+					if($test->num_rows > 0) {
+						while($lig_acces=mysqli_fetch_object($test)) {
+							echo "<br />
+		".$tab_group['info']."&nbsp;: Accès (<em>Appréciations des bulletins&nbsp;: ".$lig_acces->mode."</em>) ouvert jusqu'au ".formate_date($lig_acces->date_limite, "y");
+						}
+					}
+				}
+
+				echo "</li>
 </ul>\n";
 				echo "</td>\n";
 			}
