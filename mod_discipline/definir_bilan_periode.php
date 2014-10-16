a2876497576fb796c53776984e3bc2ff9b4dd99f
Stephane Boireau <crob2607@free.fr>
2014-10-16 10:54:53 +0200

    Correctif sur une initialisation de variable.

--- mod_discipline/definir_bilan_periode.php
+++ mod_discipline/definir_bilan_periode.php
@@ -99,16 +99,18 @@ if ((isset($avertissement_nom_court))&&($avertissement_nom_court!='')&&(isset($a
 	$tab_type_avertissement_fin_periode=get_tab_type_avertissement();
 
 	$temoin_deja=0;
-	for($loop=0;$loop<count($tab_type_avertissement_fin_periode['cpt']);$loop++) {
-		if($tab_type_avertissement_fin_periode['cpt'][$loop]['nom_court']==$avertissement_nom_court) {
-			$msg.="Le nom court de  l'".$mod_disc_terme_avertissement_fin_periode." proposé '$avertissement_nom_court' existe déjà.<br />\n";
-			$temoin_deja++;
-			break;
-		}
-		elseif($tab_type_avertissement_fin_periode['cpt'][$loop]['nom_complet']==$avertissement_nom_complet) {
-			$msg.="Le nom complet de  l'".$mod_disc_terme_avertissement_fin_periode." proposé '$avertissement_nom_complet' existe déjà.<br />\n";
-			$temoin_deja++;
-			break;
+	if(isset($tab_type_avertissement_fin_periode['cpt'])) {
+		for($loop=0;$loop<count($tab_type_avertissement_fin_periode['cpt']);$loop++) {
+			if($tab_type_avertissement_fin_periode['cpt'][$loop]['nom_court']==$avertissement_nom_court) {
+				$msg.="Le nom court de l'".$mod_disc_terme_avertissement_fin_periode." proposé '$avertissement_nom_court' existe déjà.<br />\n";
+				$temoin_deja++;
+				break;
+			}
+			elseif($tab_type_avertissement_fin_periode['cpt'][$loop]['nom_complet']==$avertissement_nom_complet) {
+				$msg.="Le nom complet de l'".$mod_disc_terme_avertissement_fin_periode." proposé '$avertissement_nom_complet' existe déjà.<br />\n";
+				$temoin_deja++;
+				break;
+			}
 		}
 	}
 
