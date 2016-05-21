0d81966b82ecfff3fece46aa63b9a80312671bf8
Stephane Boireau <crob2607@free.fr>
2016-05-21 09:30:53 +0200

    Correctif sur la récupération de notices connecté en élève/parent

--- cahier_texte/see_all.php
+++ cahier_texte/see_all.php
@@ -291,7 +291,7 @@ echo "<h2 class='grande_ligne couleur_bord_tableau_notice'>\n<strong>CAHIER DE T
 if(($_SESSION['statut']=='eleve')||($_SESSION['statut']=='responsable')) {
 	if((isset($selected_eleve_login))&&($selected_eleve_login!="")) {
 		$req_notices =
-			"select 'c' type, contenu, date_ct, id_ct
+			"select DISTINCT 'c' type, contenu, date_ct, id_ct
 			from ct_entry ce, j_eleves_groupes jeg
 			where (contenu != ''
 			and ce.id_groupe='".$id_groupe."'
