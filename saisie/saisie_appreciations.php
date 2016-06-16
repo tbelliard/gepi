28d68557c478906ace9707117f92c2fd3c80409d
Stephane Boireau <crob2607@free.fr>
2016-06-16 08:26:57 +0200

    Rétablissement de l'ancre utilisée après correction d'appréciation.

--- saisie/saisie_appreciations.php
+++ saisie/saisie_appreciations.php
@@ -1664,7 +1664,7 @@ foreach ($liste_eleves as $eleve_login) {
 			echo "<h3>Classe de " . $eleve_classe . "</h3>\n";
 		}
 		$prev_classe = $eleve_classe;
-                //echo "<a name='saisie_app_$eleve_login'></a>";
+		echo "<a name='saisie_app_$eleve_login'></a>";
 ?>
     <table style ="width: 750px; border-collapse: separate; border-spacing: 1px; " class='boireaus'>
         <tr>
