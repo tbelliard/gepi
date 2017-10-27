c031cbd6d5015fb7ebb7cb1fd858241af36d3460
Stephane Boireau <stephane.boireau@free.fr>
2017-10-27 17:38:13 +0200

    Possibilité de modifier les dates de vacances après import ICS.

--- edt/import_vacances_ics.php
+++ edt/import_vacances_ics.php
@@ -45,6 +45,7 @@ if (!checkAccess()) {
 $zone=isset($_POST['zone']) ? $_POST['zone'] : (isset($_GET['zone']) ? $_GET['zone'] : NULL);
 $mode=isset($_POST['mode']) ? $_POST['mode'] : (isset($_GET['mode']) ? $_GET['mode'] : NULL);
 $id_classe=isset($_POST['id_classe']) ? $_POST['id_classe'] : NULL;
+$corriger_manuellement=isset($_POST['corriger_manuellement']) ? $_POST['corriger_manuellement'] : (isset($_GET['corriger_manuellement']) ? $_GET['corriger_manuellement'] : NULL);
 
 $sql="CREATE TABLE IF NOT EXISTS calendrier_vacances (
 id int(11) NOT NULL auto_increment,
@@ -68,6 +69,7 @@ if(isset($_POST['enregistrer_vacances'])) {
 	//echo "$sql<br />";
 	$menage=mysqli_query($GLOBALS["mysqli"], $sql);
 
+	// Récupération des vacances actuellement enregistrées pour les classes
 	$edt_vacances=array();
 	$edt_vacances2=array();
 	$sql="SELECT * FROM edt_calendrier WHERE etabvacances_calendrier='1';";
@@ -97,6 +99,8 @@ if(isset($_POST['enregistrer_vacances'])) {
 	$nb_reg=0;
 	$date_vacances=isset($_POST['date_vacances']) ? $_POST['date_vacances'] : array();
 
+	// On importe des vacances depuis un ICS.
+	// Suppression d'un éventuel message en page d'accueil
 	if(count($date_vacances)>0) {
 		$sql="SELECT * FROM infos_actions WHERE titre='Dates des vacances et jours fériés';";
 		$res=mysqli_query($GLOBALS["mysqli"], $sql);
@@ -133,6 +137,7 @@ if(isset($_POST['enregistrer_vacances'])) {
 			//echo "<p style='margin-top:1em;'>Du ".strftime("%A %d/%m/%Y à %H:%M:%S", $debut_calendrier_ts)."";
 
 			$tab_date=explode("/", $tab[2]);
+			// On ramène une minute avant pour passer du jour de reprise, au jour de fin des vacances à 23:59:59
 			$fin_calendrier_ts=mktime(0,0,0,$tab_date[1], $tab_date[0], $tab_date[2])-60;
 			$jourfin_calendrier=strftime("%Y-%m-%d", $fin_calendrier_ts);
 			$heurefin_calendrier=strftime("%H:%M:%S", $fin_calendrier_ts);
@@ -172,6 +177,312 @@ if(isset($_POST['enregistrer_vacances'])) {
 
 	$msg.=$nb_reg." enregistrements effectués.<br />";
 }
+elseif(isset($_GET['imposer_pour_toutes_les_classes'])) {
+	check_token();
+
+	$msg="";
+
+	/*
+	$sql="TRUNCATE calendrier_vacances;";
+	//echo "$sql<br />";
+	$menage=mysqli_query($GLOBALS["mysqli"], $sql);
+	*/
+
+	// Récupération des vacances actuellement enregistrées pour les classes
+	$edt_vacances=array();
+	$edt_vacances2=array();
+	$sql="SELECT * FROM edt_calendrier WHERE etabvacances_calendrier='1';";
+	$res=mysqli_query($GLOBALS["mysqli"], $sql);
+	if(mysqli_num_rows($res)>0) {
+		$cpt=0;
+		while($lig=mysqli_fetch_assoc($res)) {
+			$edt_vacances[$cpt]=$lig;
+			$nom_corrige=remplace_accents(mb_strtolower(stripslashes($lig['nom_calendrier'])), "all");
+			$edt_vacances2[$nom_corrige]=$cpt;
+			$cpt++;
+		}
+	}
+
+	$sql="SELECT id FROM classes ORDER BY classe;";
+	$res=mysqli_query($GLOBALS["mysqli"], $sql);
+	if(mysqli_num_rows($res)>0) {
+		$chaine_id_classe="";
+		while($lig=mysqli_fetch_object($res)) {
+			if($chaine_id_classe!="") {
+				$chaine_id_classe.=";";
+			}
+			$chaine_id_classe.=$lig->id;
+		}
+	}
+
+	$nb_reg=0;
+	/*
+	$date_vacances=isset($_POST['date_vacances']) ? $_POST['date_vacances'] : array();
+
+	if(count($date_vacances)>0) {
+		$sql="SELECT * FROM infos_actions WHERE titre='Dates des vacances et jours fériés';";
+		$res=mysqli_query($GLOBALS["mysqli"], $sql);
+		if(mysqli_num_rows($res)>0) {
+			while($lig=mysqli_fetch_object($res)) {
+				del_info_action($lig->id);
+			}
+		}
+	}
+	*/
+
+	//preg_replace('/"/', '', $event['DESCRIPTION'])."|".strftime("%d/%m/%Y", $ts_debut)."|".strftime("%d/%m/%Y", $ts_fin)
+	$date_vacances=array();
+
+	// Récupération des dates de vacances enregistrées dans 'calendrier_vacances'
+	$sql="SELECT * FROM calendrier_vacances ORDER BY debut_calendrier_ts;";
+	$res=mysqli_query($GLOBALS["mysqli"], $sql);
+	if(mysqli_num_rows($res)>0) {
+		while($lig=mysqli_fetch_object($res)) {
+			$date_vacances[]=$lig->nom_calendrier."|".strftime("%d/%m/%Y", $lig->debut_calendrier_ts)."|".strftime("%d/%m/%Y", $lig->fin_calendrier_ts);
+		}
+	}
+
+	for($loop=0;$loop<count($date_vacances);$loop++) {
+		$tab=explode("|", $date_vacances[$loop]);
+		if(!isset($tab[2])) {
+			$msg.="Date n°$loop invalide&nbsp;: ".$date_vacances[$loop]."<br />";
+		}
+		else {
+			$nom_corrige=remplace_accents(mb_strtolower(stripslashes($tab[0])), "all");
+			if(array_key_exists($nom_corrige, $edt_vacances2)) {
+				//echo "<p>".$date_vacances[$loop]." trouvé&nbsp;:</p>";
+				/*
+				echo "<pre>";
+				print_r($edt_vacances[$edt_vacances2[$nom_corrige]]);
+				echo "</pre>";
+				*/
+				$sql="DELETE FROM edt_calendrier WHERE id_calendrier='".$edt_vacances[$edt_vacances2[$nom_corrige]]['id_calendrier']."';";
+				//echo "$sql<br />";
+				$res=mysqli_query($GLOBALS["mysqli"], $sql);
+			}
+
+			$tab_date=explode("/", $tab[1]);
+			$debut_calendrier_ts=mktime(0,0,0,$tab_date[1], $tab_date[0], $tab_date[2]);
+			$jourdebut_calendrier=strftime("%Y-%m-%d", $debut_calendrier_ts);
+			$heuredebut_calendrier=strftime("%H:%M:%S", $debut_calendrier_ts);
+			//echo "<p style='margin-top:1em;'>Du ".strftime("%A %d/%m/%Y à %H:%M:%S", $debut_calendrier_ts)."";
+
+			$tab_date=explode("/", $tab[2]);
+			// On ramène une minute avant pour passer du jour de reprise, au jour de fin des vacances à 23:59:59
+			$fin_calendrier_ts=mktime(0,0,0,$tab_date[1], $tab_date[0], $tab_date[2])-60;
+			$jourfin_calendrier=strftime("%Y-%m-%d", $fin_calendrier_ts);
+			$heurefin_calendrier=strftime("%H:%M:%S", $fin_calendrier_ts);
+			//echo " au ".strftime("%A %d/%m/%Y à %H:%M:%S", $fin_calendrier_ts)."<br />";
+
+			$sql="INSERT INTO edt_calendrier SET classe_concerne_calendrier='".$chaine_id_classe."', 
+									nom_calendrier='".mysqli_real_escape_string($mysqli, $tab[0])."',
+									debut_calendrier_ts='".$debut_calendrier_ts."',
+									fin_calendrier_ts='".$fin_calendrier_ts."',
+									jourdebut_calendrier='".$jourdebut_calendrier."',
+									heuredebut_calendrier='".$heuredebut_calendrier."',
+									jourfin_calendrier='".$jourfin_calendrier."',
+									heurefin_calendrier='".$heurefin_calendrier."',
+									numero_periode='0',
+									etabferme_calendrier='2',
+									etabvacances_calendrier='1';";
+			//echo "$sql<br />";
+			$res=mysqli_query($GLOBALS["mysqli"], $sql);
+			if($res) {
+				$nb_reg++;
+				/*
+				$sql="INSERT INTO calendrier_vacances SET nom_calendrier='".$tab[0]."',
+									debut_calendrier_ts='".$debut_calendrier_ts."',
+									fin_calendrier_ts='".$fin_calendrier_ts."',
+									jourdebut_calendrier='".$jourdebut_calendrier."',
+									heuredebut_calendrier='".$heuredebut_calendrier."',
+									jourfin_calendrier='".$jourfin_calendrier."',
+									heurefin_calendrier='".$heurefin_calendrier."';";
+				echo "$sql<br />";
+				$res=mysqli_query($GLOBALS["mysqli"], $sql);
+				*/
+			}
+			else {
+				$msg.="Erreur lors de l'enregistrement pour ".$tab[0]."<br />";
+			}
+		}
+	}
+
+	$msg.=$nb_reg." enregistrements effectués.<br />";
+}
+elseif(isset($_POST['enregistrer_correction_manuelle'])) {
+	check_token();
+
+	$msg="";
+
+	$nb_reg=0;
+	//$date_vacances=isset($_POST['date_vacances']) ? $_POST['date_vacances'] : array();
+	$id_calendrier=isset($_POST['id_calendrier']) ? $_POST['id_calendrier'] : array();
+
+	// Récupérer le tableau des vacances déjà enregistrées
+	$date_vacances=array();
+	$sql="SELECT * FROM calendrier_vacances ORDER BY debut_calendrier_ts;";
+	$res=mysqli_query($GLOBALS["mysqli"], $sql);
+	if(mysqli_num_rows($res)>0) {
+		while($lig=mysqli_fetch_assoc($res)) {
+			$date_vacances[$lig['id']]=$lig;
+		}
+	}
+
+	// Parcourir les éventuelles modifs
+	for($loop=0;$loop<count($id_calendrier);$loop++) {
+		if(preg_match("/^[0-9]{1,}$/", $id_calendrier[$loop])) {
+			$date_debut=isset($_POST['date_debut_'.$id_calendrier[$loop]]) ? $_POST['date_debut_'.$id_calendrier[$loop]] : NULL;
+			$date_fin=isset($_POST['date_fin_'.$id_calendrier[$loop]]) ? $_POST['date_fin_'.$id_calendrier[$loop]] : NULL;
+			if((!isset($date_debut))||
+			(!isset($date_fin))||
+			(!preg_match("#^[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}$#", $date_debut))||
+			(!preg_match("#^[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}$#", $date_fin))) {
+				$msg.="Une (au moins) des dates proposées pour les vacances n°".$id_calendrier[$loop]." n'est pas valide.<br />";
+				// Pour revenir à la page de correction manuelle:
+				$corriger_manuellement="y";
+			}
+			else {
+				// Reste à contrôler la validité des dates
+				$tab_debut=explode("/", $date_debut);
+				$tab_fin=explode("/", $date_fin);
+
+				$poursuivre=true;
+				if(!checkdate($tab_debut[1], $tab_debut[0], $tab_debut[2])) {
+					$msg.="La date de début des vacances n°".$id_calendrier[$loop]." n'est pas valide.<br />";
+					$poursuivre=false;
+					// Pour revenir à la page de correction manuelle:
+					$corriger_manuellement="y";
+				}
+
+				if(!checkdate($tab_fin[1], $tab_fin[0], $tab_fin[2])) {
+					$msg.="La date de fin des vacances n°".$id_calendrier[$loop]." n'est pas valide.<br />";
+					$poursuivre=false;
+					// Pour revenir à la page de correction manuelle:
+					$corriger_manuellement="y";
+				}
+
+				if($poursuivre) {
+					// Comparer $date_vacances[$id_calendrier[$loop]]['jourdebut_calendrier'] au format mysql avec $tab_debut[2]-$tab_debut[1]-$tab_debut[0];
+					// Mois et jour à formater sur deux chiffres?
+
+					$sql_ajout="";
+					$mysql_date_debut=$tab_debut[2]."-".$tab_debut[1]."-".$tab_debut[0];
+					if($mysql_date_debut!=$date_vacances[$id_calendrier[$loop]]['jourdebut_calendrier']) {
+						$debut_calendrier_ts=mktime(0,0,0,$tab_debut[1], $tab_debut[0], $tab_debut[2]);
+
+						$sql_ajout.=" jourdebut_calendrier='".$mysql_date_debut."', heuredebut_calendrier='00:00:00', debut_calendrier_ts='".$debut_calendrier_ts."'";
+
+
+						$mysql_date_fin=$tab_fin[2]."-".$tab_fin[1]."-".$tab_fin[0];
+						if($mysql_date_fin!=$date_vacances[$id_calendrier[$loop]]['jourfin_calendrier']) {
+							$fin_calendrier_ts=mktime(23,59,0,$tab_fin[1], $tab_fin[0], $tab_fin[2]);
+
+							$sql_ajout.=", jourfin_calendrier='".$mysql_date_fin."', heurefin_calendrier='23:59:00', fin_calendrier_ts='".$fin_calendrier_ts."'";
+						}
+					}
+					else {
+						$mysql_date_fin=$tab_fin[2]."-".$tab_fin[1]."-".$tab_fin[0];
+						if($mysql_date_fin!=$date_vacances[$id_calendrier[$loop]]['jourfin_calendrier']) {
+							$fin_calendrier_ts=mktime(23,59,0,$tab_fin[1], $tab_fin[0], $tab_fin[2]);
+
+							$sql_ajout.=" jourfin_calendrier='".$mysql_date_fin."', heurefin_calendrier='23:59:00', fin_calendrier_ts='".$fin_calendrier_ts."'";
+						}
+					}
+
+					if($sql_ajout!='') {
+						$sql="UPDATE calendrier_vacances SET ".$sql_ajout." WHERE id='".$id_calendrier[$loop]."';";
+						//echo "$sql<br />";
+						$res=mysqli_query($GLOBALS["mysqli"], $sql);
+						if($res) {
+							$nb_reg++;
+						}
+						else {
+							$msg.="Erreur sur<br />$sql<br />";
+							// Pour revenir à la page de correction manuelle:
+							$corriger_manuellement="y";
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// Ajout d'une période de vacances
+	if((isset($_POST['ajout_date_vacances']))&&
+	($_POST['ajout_date_vacances']!="")&&
+	(isset($_POST['date_debut_ajout']))&&
+	(preg_match("#^[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}$#", $_POST['date_debut_ajout']))&&
+	(isset($_POST['date_fin_ajout']))&&
+	(preg_match("#^[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}$#", $_POST['date_fin_ajout']))) {
+
+		$tab_debut=explode("/", $_POST['date_debut_ajout']);
+		$tab_fin=explode("/", $_POST['date_fin_ajout']);
+
+		$poursuivre=true;
+		if(!checkdate($tab_debut[1], $tab_debut[0], $tab_debut[2])) {
+			$msg.="La date de début des vacances pour la période ajoutée n'est pas valide.<br />";
+			$poursuivre=false;
+			// Pour revenir à la page de correction manuelle:
+			$corriger_manuellement="y";
+		}
+
+		if(!checkdate($tab_fin[1], $tab_fin[0], $tab_fin[2])) {
+			$msg.="La date de fin des vacances pour la période ajoutée n'est pas valide.<br />";
+			$poursuivre=false;
+			// Pour revenir à la page de correction manuelle:
+			$corriger_manuellement="y";
+		}
+
+		if($poursuivre) {
+
+			$mysql_date_debut=$tab_debut[2]."-".$tab_debut[1]."-".$tab_debut[0];
+			$debut_calendrier_ts=mktime(0,0,0,$tab_debut[1], $tab_debut[0], $tab_debut[2]);
+			$mysql_date_fin=$tab_fin[2]."-".$tab_fin[1]."-".$tab_fin[0];
+			$fin_calendrier_ts=mktime(23,59,0,$tab_fin[1], $tab_fin[0], $tab_fin[2]);
+
+			$sql="INSERT INTO calendrier_vacances SET nom_calendrier='".mysqli_real_escape_string($mysqli, stripslashes($_POST['ajout_date_vacances']))."', 
+										jourdebut_calendrier='".$mysql_date_debut."', 
+										heuredebut_calendrier='00:00:00', 
+										debut_calendrier_ts='".$debut_calendrier_ts."', 
+										jourfin_calendrier='".$mysql_date_fin."', 
+										heurefin_calendrier='23:59:00', 
+										fin_calendrier_ts='".$fin_calendrier_ts."';";
+			//echo "$sql<br />";
+			$res=mysqli_query($GLOBALS["mysqli"], $sql);
+			if($res) {
+				$nb_reg++;
+			}
+			else {
+				$msg.="Erreur lors de l'ajout d'une période de vacances.<br />";
+			}
+		}
+	}
+
+	// Suppression d'une période de vacances
+	$nb_suppr=0;
+	$suppr=isset($_POST['suppr']) ? $_POST['suppr'] : array();
+	for($loop=0;$loop<count($suppr);$loop++) {
+		$sql="DELETE FROM calendrier_vacances WHERE id='".$suppr[$loop]."';";
+		$del=mysqli_query($GLOBALS["mysqli"], $sql);
+		if(!$del) {
+			
+		}
+		else {
+			$nb_suppr++;
+		}
+	}
+
+	$msg.=$nb_reg." enregistrements effectués.<br />";
+	if($nb_suppr>0) {
+		$msg.=$nb_suppr." enregistrements supprimés.<br />";
+	}
+}
+
+// Configuration du calendrier
+$style_specifique[] = "lib/DHTMLcalendar/calendarstyle";
+$javascript_specifique[] = "lib/DHTMLcalendar/calendar";
+$javascript_specifique[] = "lib/DHTMLcalendar/lang/calendar-fr";
+$javascript_specifique[] = "lib/DHTMLcalendar/calendar-setup";
 
 //**************** EN-TETE *****************
 $titre_page = "Import vacances";
@@ -195,6 +506,85 @@ if(($begin_bookings=="")||($end_bookings=="")) {
 	die();
 }
 
+if(isset($corriger_manuellement)) {
+	echo "
+	 | <a href='".$_SERVER['PHP_SELF']."'>Retour à la page d'accueil de l'import des vacances et jours fériés</a>
+</p>
+<h2>Correction manuelle</h2>
+<p>Vous pouvez corriger manuellement ici le contenu de la table 'calendrier_vacances'.</p>
+<form action='".$_SERVER['PHP_SELF']."' method='post'>
+	<fieldset class='fieldset_opacite50'>
+		".add_token_field()."
+		<input type='hidden' name='enregistrer_correction_manuelle' value='y' />";
+
+	$sql="SELECT * FROM calendrier_vacances ORDER BY debut_calendrier_ts;";
+	$res=mysqli_query($GLOBALS["mysqli"], $sql);
+	if(mysqli_num_rows($res)>0) {
+		echo "<table class='boireaus boireaus_alt' summary='Vacances scolaires'>
+	<tr>
+		<th>Titre</th>
+		<th colspan='2'>Du premier jour des vacances</th>
+		<th colspan='2'>Au dernier jour des vacances inclus</th>
+		<th>Supprimer</th>
+	</tr>";
+		while($lig=mysqli_fetch_object($res)) {
+			echo "
+	<tr>
+		<th>".$lig->nom_calendrier."<input type='hidden' name='id_calendrier[]' value='".$lig->id."' /></th>
+		<td>".strftime("%a %d/%m/%Y", $lig->debut_calendrier_ts)."</td>
+		<td><input type='text' name='date_debut_".$lig->id."' id='date_debut_".$lig->id."' value='".strftime("%d/%m/%Y", $lig->debut_calendrier_ts)."'  onKeyDown=\"clavier_date(this.id,event);\" AutoComplete=\"off\" />".img_calendrier_js("date_debut_".$lig->id, "img_bouton_date_debut_".$lig->id)."</td>
+		<td>".strftime("%a %d/%m/%Y", $lig->fin_calendrier_ts)."</td>
+		<td><input type='text' name='date_fin_".$lig->id."' id='date_fin_".$lig->id."' value='".strftime("%d/%m/%Y", $lig->fin_calendrier_ts)."'  onKeyDown=\"clavier_date(this.id,event);\" AutoComplete=\"off\" />".img_calendrier_js("date_fin_".$lig->id, "img_bouton_date_fin_".$lig->id)."</td>
+		<td>
+			<input type='checkbox' name='suppr[]' value='".$lig->id."' />
+		</td>
+	</tr>";
+		}
+		echo "</table>";
+	}
+
+	// Ajouter une date manuellement:
+	echo "
+	<p style='margin-top:2em;'>Ajouter une période de vacances ou un jour férié&nbsp;:<br />
+	<em>(laisser un des champs ci-dessous vide, pour ne pas ajouter de période de vacances)</em></p>
+	<table>
+		<tr>
+			<th>
+				Désignation&nbsp;: 
+			</th>
+			<td>
+				<input type='text' name='ajout_date_vacances' value='' />
+			</td>
+		</tr>
+		<tr>
+			<th>
+				Du 
+			</th>
+			<td>
+				<input type='text' name='date_debut_ajout' id='date_debut_ajout' value=''  onKeyDown=\"clavier_date(this.id,event);\" AutoComplete=\"off\" />".img_calendrier_js("date_debut_ajout", "img_bouton_date_debut_ajout")."
+			</td>
+		</tr>
+		<tr>
+			<th>
+				Au 
+			</th>
+			<td>
+				<input type='text' name='date_fin_ajout' id='date_fin_ajout' value=''  onKeyDown=\"clavier_date(this.id,event);\" AutoComplete=\"off\" />".img_calendrier_js("date_fin_ajout", "img_bouton_date_fin_ajout")."
+			</td>
+		</tr>
+	</table>";
+
+	echo "
+		<p align='center' style='margin-top:1em;'><input type='submit' value='Valider' /></p>";
+
+
+	echo "
+	</fieldset>
+</form>";
+	require("../lib/footer.inc.php");
+	die();
+}
+
 if(!isset($zone)) {
 	echo "
 </p>
@@ -205,6 +595,28 @@ if(!isset($zone)) {
 	<li><p><a href='".$_SERVER['PHP_SELF']."?zone=B&amp;mode=telech'>Zone B</a>&nbsp;: Aix-Marseille, Amiens, Caen, Lille, Nancy-Metz, Nantes, Nice, Orléans-Tours, Reims, Rennes, Rouen, Strasbourg</p></li>
 	<li><p><a href='".$_SERVER['PHP_SELF']."?zone=C&amp;mode=telech'>Zone C</a>&nbsp;: Créteil, Montpellier, Paris, Toulouse, Versailles</p></li>
 </ul>";
+
+
+	$html_tab_vacances=affiche_tableau_vacances("", "n", "y");
+	if($html_tab_vacances!="") {
+		echo "<div align='center' style='margin-top:1em;'>
+		<p class='bold'>Les vacances et jours fériés enregistrées dans la table 'calendrier_vacances' sont les suivantes&nbsp;:</p>
+		".$html_tab_vacances."
+		<p>Vous pouvez importer fichier de zone plus haut dans la page,<br />
+		ou <a href='".$_SERVER["PHP_SELF"]."?corriger_manuellement=y'>corriger manuellement</a><br />
+		ou <a href='".$_SERVER["PHP_SELF"]."?imposer_pour_toutes_les_classes=y".add_token_in_url()."'>imposer les valeurs ci-dessus pour toutes les classes</a>.</p>
+	</div>";
+	}
+
+	echo "<p style='text-indent:-4em;margin-left:4em;margin-top:1em;'><em>NOTES&nbsp;:</em></p>
+	<ul>
+		<li><p>Les dates de vacances et jours fériés sont enregistrés ici dans une table 'edt_vacances'.<br />
+		Les dates de cette table sont utilisées dans les calendriers affichés pour les personnels de l'établissement.<br />
+		Les élèves ont pour leur part des calendriers par classe <em>(table 'edt_calendrier')</em>.</p></li>
+		<li><p>La table 'edt_vacances' sert de modèle pour remplir le calendrier par classe, mais pour chaque classe, vous pouvez modifier ,via <strong>Emplois du temps/Gestion/Gestion du calendrier</strong>, les dates de vacances et jours fériés pour telle ou telle classe.<br />
+	En principe cependant, toutes les classes devraient tout de même avoir les mêmes vacances et dans ce cas, en important les dates depuis un fichier ICS officiel, vous imposerez les dates de vacances et jours fériés par défaut à toutes les classes.<br />
+	Libre à vous d'ajuster ensuite si nécessaire pour telle ou telle classe, par exemple pour ajouter une semaine de stage durant laquelle les élèves ne seront pas considérés comme absents dans le module Absences 2.</p></li>
+	</ul>";
 
 }
 elseif($mode=="telech") {
