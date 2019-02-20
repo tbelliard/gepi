034dd1978be6e72a30892c09e5b9829b401383fa
Stephane Boireau <stephane.boireau@free.fr>
2019-02-20 18:57:19 +0100

    Correction du placement du lien Actualiser.

--- cahier_texte_2/ajax_edition_compte_rendu.php
+++ cahier_texte_2/ajax_edition_compte_rendu.php
@@ -251,23 +251,6 @@ echo "<br />";
 //debug_var();
 // **********************************************
 
-echo "<div style='float:right; width:16px;'><a href=\"#\" style=\"font-size: 11pt;\" title=\"Recharger la page. Cela peut être utile si le champ de saisie ne s'affiche pas.\" onclick=\"javascript:
-				id_groupe = '".$id_groupe."';
-				getWinDernieresNotices().hide();
-				getWinListeNotices();
-				new Ajax.Updater('affichage_liste_notice', './ajax_affichages_liste_notices.php?id_groupe=".$id_groupe."', {encoding: 'utf-8'});
-				getWinEditionNotice().setAjaxContent('./ajax_edition_compte_rendu.php?id_groupe=".$id_groupe."&today='+getCalendarUnixDate(), { 
-				    		encoding: 'utf-8',
-				    		onComplete : 
-				    		function() {
-				    			initWysiwyg();
-							}
-						}
-				);
-				return false;
-			\"><img src='../images/icons/actualiser.png' class='icone16' /></a></div>\n";
-
-
 // Affichage des différents groupes du professeur
 //\$A($('id_groupe_colonne_gauche').options).find(function(option) { return option.selected; }).value is a javascript trick to get selected value.
 echo "<div id=\"div_chaine_edition_notice\" style=\"display:inline;\"><img id=\"chaine_edition_notice\" onLoad=\"updateChaineIcones()\" HEIGHT=\"16\" WIDTH=\"16\" style=\"border: 0px; vertical-align : middle\" src=\"../images/blank.gif\"  alt=\"Lier\" title=\"Lier la liste avec la liste des notices\" /></div>&nbsp;\n";
@@ -347,6 +330,23 @@ echo " <a href=\"#\" onclick=\"fen=window.open('../groupes/popup.php?id_groupe="
 if((isset($ctCompteRendu))&&($ctCompteRendu->getIdCt()!=null)) {
 	echo " <a href='affiche_notice.php?id_ct=".$ctCompteRendu->getIdCt()."&type_notice=c' target='_blank' title=\"Afficher la notice dans un nouvel onglet.\"><img src='../images/icons/chercher.png' class='icone16' alt='Afficher' /></a>";
 }
+
+echo " <a href=\"#\" style=\"font-size: 11pt;\" title=\"Recharger la page. Cela peut être utile si le champ de saisie ne s'affiche pas.\" onclick=\"javascript:
+				id_groupe = '".$id_groupe."';
+				getWinDernieresNotices().hide();
+				getWinListeNotices();
+				new Ajax.Updater('affichage_liste_notice', './ajax_affichages_liste_notices.php?id_groupe=".$id_groupe."', {encoding: 'utf-8'});
+				getWinEditionNotice().setAjaxContent('./ajax_edition_compte_rendu.php?id_groupe=".$id_groupe."&today='+getCalendarUnixDate(), { 
+				    		encoding: 'utf-8',
+				    		onComplete : 
+				    		function() {
+				    			initWysiwyg();
+							}
+						}
+				);
+				return false;
+			\"><img src='../images/icons/actualiser.png' class='icone16' /></a>";
+
 
 if(getSettingAOui('cdt_afficher_volume_docs_joints')) {
 	$volume_cdt_groupe=volume_docs_joints($groupe->getId());
