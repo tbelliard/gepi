81029fe7c4bd7789e8ae6c283122e04739f81887
Stephane Boireau <stephane.boireau@free.fr>
2019-06-28 18:43:51 +0200

    Initialisation d'un tableau.

--- mod_genese_classes/liste_options.php
+++ mod_genese_classes/liste_options.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* Copyright 2001, 2018 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun, Stephane Boireau
+* Copyright 2001, 2019 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun, Stephane Boireau
 *
 * This file is part of GEPI.
 *
@@ -469,6 +469,7 @@ if(isset($lien_fichier_ods)) {
 	die();
 }
 
+$tab_id_classe=array();
 $sql="SELECT id_classe FROM gc_divisions WHERE projet='$projet' AND statut='actuelle';";
 $res=mysqli_query($GLOBALS["mysqli"], $sql);
 while($lig=mysqli_fetch_object($res)) {
@@ -509,7 +510,7 @@ if(!isset($_POST['choix_param'])) {
 		
 			echo "<input type='checkbox' name='id_classe[]' id='id_classe_$cpt_i' value='$lig_clas->id' ";
 			echo "onchange=\"checkbox_champ_change('id_classe_$cpt_i'); changement();\" ";
-			if(in_array($lig_clas->id,$tab_id_classe)) {echo "checked ";$temp_style=" style='font-weight:bold;'";} else {$temp_style="";}
+			if(in_array($lig_clas->id, $tab_id_classe)) {echo "checked ";$temp_style=" style='font-weight:bold;'";} else {$temp_style="";}
 			echo "/><label for='id_classe_$cpt_i'><span id='texte_id_classe_$cpt_i'$temp_style>$lig_clas->classe</span></label>\n";
 			echo "<input type='hidden' name='classe[$lig_clas->id]' value='$lig_clas->classe' />\n";
 			echo "<br />\n";
