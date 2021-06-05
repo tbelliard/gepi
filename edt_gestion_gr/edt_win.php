e43ee61328f68dae8a9cc780342cb304ce492fa7
Romain <contact@romain-neil.fr>
2021-06-05 17:17:11 +0200

    edt_win.php: uniformisation

--- edt_gestion_gr/edt_win.php
+++ edt_gestion_gr/edt_win.php
@@ -1,9 +1,6 @@
 <?php
 /**
- *
- *
- *
- * Copyright 2001, 2002 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun, Julien Jocal
+ * Copyright 2001, 2002, 2021 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun, Julien Jocal, Romain Neil
  *
  * This file is part of GEPI.
  *
@@ -30,17 +27,17 @@ require_once("../lib/initialisations.inc.php");
 // Resume session
 $resultat_session = $session_gepi->security_check();
 if ($resultat_session == 'c') {
-   header("Location:../utilisateurs/mon_compte.php?change_mdp=yes&retour=accueil#changemdp");
-   die();
+	header("Location:../utilisateurs/mon_compte.php?change_mdp=yes&retour=accueil#changemdp");
+	die();
 } else if ($resultat_session == '0') {
-    header("Location: ../logout.php?auto=1");
-    die();
+	header("Location: ../logout.php?auto=1");
+	die();
 }
 
 // Sécurité
 if (!checkAccess()) {
-    header("Location: ./logout.php?auto=2");
-    die();
+	header("Location: ./logout.php?auto=2");
+	die();
 }
 
 // ===================== fin de l'initialisation ================
@@ -53,33 +50,31 @@ $action = isset($_POST["action"]) ? $_POST["action"] : NULL;
 $aff_modif = NULL;
 
 // On récupère toutes les données du groupe
-if (isset($var) AND is_numeric($var)) {
+if (isset($var) and is_numeric($var)) {
 	// On y va pour récupérer les données
-	$query_d = mysqli_query($GLOBALS["mysqli"], "SELECT nom, nom_long, subdivision_type, subdivision FROM edt_gr_nom WHERE id = '".$var."'");
+	$query_d = mysqli_query($GLOBALS["mysqli"], "SELECT nom, nom_long, subdivision_type, subdivision FROM edt_gr_nom WHERE id = '" . $var . "'");
 	$rep_d = mysqli_fetch_array($query_d);
 }
-
 
 if ($var2 == "changer_nom") {
 	//echo '<p>Vous voulez changer de nom ?</p>';
 	// On récupère toutes les données du groupe
-	if (isset($var) AND is_numeric($var)) {
-
-				$aff_modif .= '
+	if (isset($var) and is_numeric($var)) {
+		$aff_modif .= '
 		<fieldset id="ajoutGr2">
 		<legend>&nbsp;Modifier un groupe d\'&eacute;l&egrave;ves pour l\'EdT&nbsp;</legend>
 
 		<form name="ajout" action="edt_win.php" method="post">
 			<input type="hidden" name="action" value="modifier_gr" />
-			<input type="hidden" name="var" value="'.$var.'" />
+			<input type="hidden" name="var" value="' . $var . '" />
 
 			<p style="text-align: right; margin-right: 10px;">
 			<label for="nomGr" title="Tel qu\'il doit apparaitre dans l\'EdT">Nom</label>
-			<input type="text" id="nomGr" name="nom_gr" value="'.$rep_d["nom"].'" />
+			<input type="text" id="nomGr" name="nom_gr" value="' . $rep_d["nom"] . '" />
 			</p>
 			<p style="text-align: right; margin-right: 10px;">
 			<label for="nomLongGr" title="Si n&eacute;cessaire !">Autre nom</label>
-			<input type="text" id="nomLongGr" name="nom_long_gr" value="'.$rep_d["nom_long"].'" />
+			<input type="text" id="nomLongGr" name="nom_long_gr" value="' . $rep_d["nom_long"] . '" />
 			</p>
 
 			<input type="submit" name="enregistrer" value="Enregistrer les modifications" />
@@ -87,13 +82,10 @@ if ($var2 == "changer_nom") {
 		</form>
 	</fieldset>
 	';
-
-	}else{
+	} else {
 		$aff_modif = '<p>Impossible de récupérer les données de ce groupe.</p>';
 	}
-
-}elseif($var2 == "liste_e"){
-
+} elseif ($var2 == "liste_e") {
 	// On vérifie si ce gr ne correspond pas à une classe et à laquelle précisément
 	//$query_verif = mysql_fetch_array(mysql_query("SELECT subdivision_type, subdivision FROM edt_gr_nom WHERE id ='".$var."'"));
 
@@ -102,58 +94,51 @@ if ($var2 == "changer_nom") {
 		// On récupère donc la liste des élèves de cette classe
 		$sql_e = "SELECT DISTINCT e.nom, e.prenom, e.login FROM eleves e, j_eleves_classes jec
 											WHERE jec.login = e.login
-											AND	jec.id_classe = '".$rep_d["subdivision"]."'
+											AND	jec.id_classe = '" . $rep_d["subdivision"] . "'
 											ORDER BY nom, prenom";
 
-	}else{
+	} else {
 		// Permet d'afficher la liste des élèves
 		$sql_e = "SELECT DISTINCT e.nom, e.prenom, e.login FROM eleves e, edt_gr_eleves ege
 											WHERE ege.id_eleve = e.id_eleve
-											AND	ege.id_gr_nom = '".$var."'
+											AND	ege.id_gr_nom = '" . $var . "'
 											ORDER BY nom, prenom";
 	}
 
 	// On met en place l'affichage
-	$aff_modif .= '<p style="text-align: right;"><a href="edt_liste_eleves.php?id_gr='.$var.'" target="_blank">Modifier cette liste</a></p>';
+	$aff_modif .= '<p style="text-align: right;"><a href="edt_liste_eleves.php?id_gr=' . $var . '" target="_blank">Modifier cette liste</a></p>';
 
-	$query_e = mysqli_query($GLOBALS["mysqli"], $sql_e) OR trigger_error('Impossible de récupérer la liste des élèves', E_USER_ERROR);
+	$query_e = mysqli_query($GLOBALS["mysqli"], $sql_e) or trigger_error('Impossible de récupérer la liste des élèves', E_USER_ERROR);
 
-	while($rep = mysqli_fetch_array($query_e)){
-
+	while ($rep = mysqli_fetch_array($query_e)) {
 		// On récupère alors la classe
 		$query_c = mysqli_query($GLOBALS["mysqli"], "SELECT classe FROM j_eleves_classes jec, classes c
-										WHERE jec.login = '".$rep["login"]."'
+										WHERE jec.login = '" . $rep["login"] . "'
 										AND jec.id_classe = c.id");
-		$classe = old_mysql_result($query_c, 0,"classe");
+		$classe = old_mysql_result($query_c, 0, "classe");
 
-		$aff_modif .= $rep["nom"].'&nbsp;'.$rep["prenom"].' ('.$classe.').<br />';
-
+		$aff_modif .= $rep["nom"] . '&nbsp;' . $rep["prenom"] . ' (' . $classe . ').<br />';
 	}
-
-
-}elseif($var2 == "liste_p"){
+} elseif ($var2 == "liste_p") {
 	// On travaille sur la liste des professeurs
 
 	$sql_p = "SELECT login, nom, prenom FROM edt_gr_profs egp, utilisateurs u
 										WHERE egp.id_utilisateurs = u.login
-										AND id_gr_nom = '".$var."'
+										AND id_gr_nom = '" . $var . "'
 										ORDER BY nom, prenom";
-	$query_p = mysqli_query($GLOBALS["mysqli"], $sql_p) OR trigger_error("Impossible de récupérer la liste des professeurs de ce groupe : ", E_USER_ERROR);
+	$query_p = mysqli_query($GLOBALS["mysqli"], $sql_p) or trigger_error("Impossible de récupérer la liste des professeurs de ce groupe : ", E_USER_ERROR);
 
-	$aff_modif .= '<p style="text-align: right;"><a href="./edt_liste_profs.php?id_gr='.$var.'" target="_blank">Modifier cette liste</a></p>';
+	$aff_modif .= '<p style="text-align: right;"><a href="./edt_liste_profs.php?id_gr=' . $var . '" target="_blank">Modifier cette liste</a></p>';
 
-	while($rep = mysqli_fetch_array($query_p)){
-
-		$aff_modif .= '<br />'.$rep["nom"].' '.$rep["prenom"];
-
+	while ($rep = mysqli_fetch_array($query_p)) {
+		$aff_modif .= '<br />' . $rep["nom"] . ' ' . $rep["prenom"];
 	}
 }
 
-
 // On traite la modification si elle est demandée (liste d'élèves : obsolète)
 if ($action == "modifier_gr") {
-	$sql_m = "UPDATE edt_gr_nom SET nom = '".$nom_gr."', nom_long = '".$nom_long_gr."' WHERE id = '".$var."'";
-	$query_m = mysqli_query($GLOBALS["mysqli"], $sql_m) OR trigger_error('Impossible de mettre à jour ce groupe '.mysqli_error($GLOBALS["mysqli"]), E_USER_ERROR);
+	$sql_m = "UPDATE edt_gr_nom SET nom = '" . $nom_gr . "', nom_long = '" . $nom_long_gr . "' WHERE id = '" . $var . "'";
+	$query_m = mysqli_query($GLOBALS["mysqli"], $sql_m) or trigger_error('Impossible de mettre à jour ce groupe ' . mysqli_error($GLOBALS["mysqli"]), E_USER_ERROR);
 	if ($query_m) {
 		// On ferme la fenêtre
 		echo '<html><body><p>La modification a bien été enregistrée, vous pouvez fermer cette fenêtre et rafraichir votre navigateur.</p></body></html>';
@@ -166,10 +151,10 @@ if ($action == "modifier_gr") {
 <html lang="fr">
 	<head><title>&nbsp;-&nbsp;</title></head>
 	<body>
-	<div style="border: 2px solid red;">
+		<div style="border: 2px solid red;">
 
-	<?php echo $aff_modif; ?>
+			<?php echo $aff_modif; ?>
 
-	</div>
+		</div>
 	</body>
 </html>
